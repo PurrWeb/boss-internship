537ee8d613ffe4a3ebfec882a2a82b403d33acea
William Billington <william.billington@econsultancy.com>
2015-11-12 18:46:14 +0000

    Add backup upload service

---
+++ app/services/upload_backup.rb
@@ -0,0 +1,31 @@
+module Services
+  class UploadBackup
+    include ActionView::Helpers::NumberHelper
+
+    def call
+      uri  = URI.parse(ENV["DATABASE_URL"])
+      name = uri.path.sub(%r{^/}, '')
+
+      Dir.mktmpdir do |dir|
+        cmd = []
+        cmd << "mysqldump"
+        cmd << "-u #{uri.user}"
+        cmd << "-p#{uri.password}" if uri.password
+        cmd << name
+        cmd << "|"
+        cmd << "gzip -9"
+
+        path = "#{dir}/#{name}-#{Time.now.strftime("%Y.%m.%d-%H.%M.%S")}.sql.gz"
+
+        out = `#{cmd.join(" ")} 2>&1 >#{path}`
+        unless $?.success?
+          raise "mysqldump failed: #{out}"
+        end
+
+        backup = Backup.create! dump: File.open(path), size: File.size(path)
+
+        "Created DB dump of #{name} (#{number_to_human_size backup.size})"
+      end
+    end
+  end
+end
--- config/application.rb
+++ config/application.rb
@@ -20,6 +20,8 @@ module Boss
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
 
+    config.autoload_paths += Dir["#{config.root}/app/services/**/"]
+
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
   end
