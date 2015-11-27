ede676da0f46d6602fce73066939adbe5508a3db
William Billington <william.billington@econsultancy.com>
2015-11-27 11:34:18 +0000

    Only use fog in production environments by defalt

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,11 +1,17 @@
 CarrierWave.configure do |config|
-  config.fog_credentials = {
-    provider:              "AWS",
-    aws_access_key_id:     ENV.fetch("AWS_ACCESS_KEY_ID"),
-    aws_secret_access_key: ENV.fetch("AWS_SECRET_ACCESS_KEY"),
-    region:                ENV.fetch("S3_BACKUP_REGION")
-  }
+  if Rails.env.development? || Rails.env.test?
+    config.storage = :file
+  else
+    config.storage = :fog
 
-  config.fog_directory = ENV.fetch("S3_BACKUP_BUCKET")
-  config.fog_public    = false
+    config.fog_credentials = {
+      provider:              "AWS",
+      aws_access_key_id:     ENV.fetch("AWS_ACCESS_KEY_ID"),
+      aws_secret_access_key: ENV.fetch("AWS_SECRET_ACCESS_KEY"),
+      region:                ENV.fetch("S3_BACKUP_REGION")
+    }
+
+    config.fog_directory = ENV.fetch("S3_BACKUP_BUCKET")
+    config.fog_public    = false
+  end
 end
