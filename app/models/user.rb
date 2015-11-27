b17a6905500a97fd324bccf956b0e07c8ae3adfa
William Billington <william.billington@econsultancy.com>
2015-11-27 16:36:29 +0000

    Require user to confirm accounts via email before logging in

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
-  # :confirmable, :timeoutable and :omniauthable
+  # :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable, :lockable
+         :recoverable, :rememberable, :trackable,
+         :validatable, :lockable, :confirmable
 end
