4426705b2f5d3482a0e2a35e488d8f3edbf09404
William Billington <william.billington@econsultancy.com>
2015-12-23 18:18:17 +0100

    Set mailer host in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,6 +77,8 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
+  config.action_mailer.default_url_options = { host: "boss.jsmbars.co.uk" }
+
   ActionMailer::Base.smtp_settings = {
     :user_name => ENV.fetch("SMTP_USERNAME"),
     :password => ENV.fetch("SMTP_PASSWORD"),
