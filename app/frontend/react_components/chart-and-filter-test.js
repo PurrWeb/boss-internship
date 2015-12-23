6d2c176f15f3a2024a297e7c657a607f7a4126eb
Matt Zeunert <matt@mostlystatic.com>
2015-12-23 06:45:58 +0000

    Test shifts starting or ending at 8am.

--- app/frontend/react_components/chart-and-filter-test.js
+++ app/frontend/react_components/chart-and-filter-test.js
@@ -7,6 +7,11 @@ describe('ChartAndFilter', function() {
     function makeDate(hours, minutes){
         return new Date(2015, 10, 10, hours, minutes)
     }
+    function expectBoundaries(shifts, start, end){
+        var boundaries = ChartAndFilter.calculateChartBoundaries(shifts);
+        expect(boundaries.start).toBe(start);
+        expect(boundaries.end).toBe(end);
+    }
 
     it("Determines the part of the day to show based on the shifts", function(){
         var shifts = [
@@ -20,8 +25,28 @@ describe('ChartAndFilter', function() {
             }
         ];
 
-        var boundaries = ChartAndFilter.calculateChartBoundaries(shifts);
-        expect(boundaries.start).toBe(13);
-        expect(boundaries.end).toBe(21);
+        expectBoundaries(shifts, 13, 21);
+    });
+
+    it("Can handle shifts starting at 8am", function(){
+        var shifts = [
+            {
+                starts_at: makeDate(8, 0),
+                ends_at: makeDate(12, 0)
+            }
+        ];
+
+        expectBoundaries(shifts, 8, 13);
+    });
+
+    it("Can handle shifts ending at 8am", function(){
+        var shifts = [
+            {
+                starts_at: makeDate(14, 0),
+                ends_at: new Date(2015, 10, 11, 8)
+            }
+        ];
+
+        expectBoundaries(shifts, 13, 8);
     });
 });
