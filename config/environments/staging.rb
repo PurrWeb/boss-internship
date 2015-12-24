b00ef1606cbb4c1e892077198d8f519a3e8f177b
William Billington <william.billington@econsultancy.com>
2015-12-24 14:18:10 +0100

    Send mail with sendgrid in staging

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -76,4 +76,14 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :user_name => '539115ce1447638c6',
+    :password => '75313dfadc6231',
+    :address => 'mailtrap.io',
+    :domain => 'mailtrap.io',
+    :port => '2525',
+    :authentication => :cram_md5
+  }
 end
