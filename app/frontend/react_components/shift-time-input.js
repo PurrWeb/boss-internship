a45785eb17f6b1b2899fc4ba1deafd60ad9b38e0
Matt Zeunert <matt@mostlystatic.com>
2015-12-29 09:44:23 +0100

    Move state out of ShiftTimeInput (into shiftTimeSelector)

--- app/frontend/react_components/shift-time-input.js
+++ app/frontend/react_components/shift-time-input.js
@@ -9,78 +9,45 @@ var SHIFT_TIME_TYPES = {
 }
 
 export default class ShiftTimeInput extends Component {
-    constructor(props){
-        super(props);
-        this.state = {
-            shiftTimeType: null,
-            defaultDate: null,
-            time: null
-        };
-    }
-    componentWillMount(){
-        this.handlePotentialPropChange(this.props);
-    }
-    componentWillReceiveProps(nextProps){
-        this.handlePotentialPropChange(nextProps)
-    }
-    /**
-     * If the date being edited has changed we want to use that instead,
-     * otherwise just keep the existing state.
-     */
-    handlePotentialPropChange(props){
-        var defaultDate;
-        var shiftTimeType;
-        if (props.defaultStartsAt) {
-            shiftTimeType = SHIFT_TIME_TYPES.START;
-            defaultDate = props.defaultStartsAt;
-        } else {
-            shiftTimeType = SHIFT_TIME_TYPES.END;
-            defaultDate = props.defaultEndsAt;
+    getShiftTimeType(){
+        if (this.props.startsAt !== undefined){
+            return SHIFT_TIME_TYPES.START;
         }
-
-        var hasDefaultDate = this.state.defaultDate !== null;
-        if (hasDefaultDate) {
-            var defaultDateHasChanged = defaultDate.valueOf() !== this.state.defaultDate.valueOf();
-            if (!defaultDateHasChanged) {
-                return;
-            }
+        if (this.props.endsAt !== undefined) {
+            return SHIFT_TIME_TYPES.END;
         }
-
-        this.setState({
-            time: moment(defaultDate).format("HH:mm"),
-            defaultDate: defaultDate,
-            shiftTimeType: shiftTimeType
-        })
+    }
+    getDateFromProps(){
+        var shiftTimeType = this.getShiftTimeType();
+        if (shiftTimeType === SHIFT_TIME_TYPES.START) {
+            return this.props.startsAt;
+        }
+        if (shiftTimeType === SHIFT_TIME_TYPES.END) {
+            return this.props.endsAt;
+        }
     }
     render(){
-        if (!this.state) {
-            return null;
-        }
-
         var className = "";
-        if (!utils.dateIsValid(this.getDateFromTime(this.state.time))){
+        if (!utils.dateIsValid(this.getDateFromProps())){
             className += "shift-time-input--invalid";
         }
 
         return <input
             type="time"
-            value={this.state.time}
+            value={  moment(this.getDateFromProps()).format("HH:mm")}
             className={className}
             onChange={(e) => this.updateTime(e.target.value)} />
     }
     getDateFromTime(timeString){
-        var rotaDate = new RotaDate(this.state.defaultDate);
-        if (this.state.shiftTimeType === SHIFT_TIME_TYPES.START) {
+        var rotaDate = this.props.rotaDate;
+        if (this.getShiftTimeType() === SHIFT_TIME_TYPES.START) {
             return rotaDate.getDateFromShiftStartTimeString(timeString);
         } else {
             return rotaDate.getDateFromShiftEndTimeString(timeString);
         }
     }
     updateTime(newValue){
-        this.setState({time: newValue});
-
         var newDate = this.getDateFromTime(newValue);
-
         this.props.onChange(newDate, utils.dateIsValid(newDate));
     }
 }
