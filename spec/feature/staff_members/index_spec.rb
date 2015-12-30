b40785ae576a54176ae5c335f9391fa8442f03fc
William Billington <william.billington@econsultancy.com>
2015-12-30 21:42:06 +0100

    Format staff members index spec

--- spec/feature/staff_members/index_spec.rb
+++ spec/feature/staff_members/index_spec.rb
@@ -14,7 +14,6 @@ RSpec.feature 'Staff members section index page' do
     staff_members_index_page.navigation.ensure_only_sections_highlighted(:staff_members)
   end
 
-
   scenario 'clicking add new staff member button takes you to the add staff members page' do
     staff_members_index_page.surf_to
     staff_members_index_page.click_add_staff_member_button
