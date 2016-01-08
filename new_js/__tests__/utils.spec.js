b4fd59b813710ec7286422b0e2e62bd6e2c78899
Matt Zeunert <matt@mostlystatic.com>
2016-01-08 12:58:41 +0000

    Remove old front-end code.

--- new_js/__tests__/utils.spec.js
+++
@@ -1,8 +0,0 @@
-jest.dontMock("../utils");
-
-describe("utils", function() {
-    it("Knows that 'hello' starts with 'h'", function() {
-        var utils = require("../utils").default;
-        expect(utils.stringStartsWith("hello", "h")).toBe(true);
-    });
-});
