c80f99da3a196760dd8622d0c69ca8d36ef99c9d
William Billington <william.billington@econsultancy.com>
2016-01-13 17:27:10 +0100

    Add links to user from staff member show page

--- spec/feature/staff_members/show_spec.rb
+++ spec/feature/staff_members/show_spec.rb
@@ -18,4 +18,24 @@ RSpec.feature 'Viewing a staff member' do
     show_page.surf_to
     show_page.ensure_avatar_image_displayed(image_url: staff_member.avatar.medium.url)
   end
+
+  scenario 'page should have message explaining that no staff member has no associated user' do
+    show_page.surf_to
+    show_page.ensure_no_associated_user_message_displayed
+  end
+
+  context 'staff member has an associated user' do
+    let(:user) { FactoryGirl.create(:user) }
+    let(:user_show_page) { PageObject::UserShowPage.new(staff_member.user) }
+
+    before do
+      staff_member.update_attributes!(user: user)
+    end
+
+    scenario 'clicking on the view user link should take you to the user show page' do
+      show_page.surf_to
+      show_page.click_view_user_button
+      user_show_page.assert_on_correct_page
+    end
+  end
 end
