4e9c7f1cce2760c3b95ba6844a43ff0806e77563
William Billington <william.billington@gmail.com>
2016-01-22 19:41:12 +0000

    New validations

--- app/models/rota_shift.rb
+++ app/models/rota_shift.rb
@@ -8,4 +8,38 @@ class RotaShift < ActiveRecord::Base
   validates :rota, presence: true
   validates :starts_at, presence: true
   validates :ends_at, presence: true
+  validate :times_in_correct_order
+  validate :times_on_correct_day
+  validate :times_in_fifteen_minute_increments
+
+  private
+  # validation
+  def times_in_correct_order
+    if starts_at.present? && ends_at.present?
+      errors.add(:base, 'starts time must be after end time') if starts_at >= ends_at
+    end
+  end
+
+  # validation
+  def times_on_correct_day
+    [:starts_at, :ends_at].each do |field|
+      time = public_send(field)
+      if time.present? && (time.to_date != rota.date.to_date)
+        raise 'time suppiled on incorrect day'
+      end
+    end
+  end
+
+  # validation
+  def times_in_fifteen_minute_increments
+    [:starts_at, :ends_at].each do |field|
+      time = public_send(field)
+      if time.present?
+        minute = Integer(time.strftime('%M'))
+        if ![0, 15, 30 ,45].include?(minute)
+          errors.add(field, 'must be 15 minute intervals')
+        end
+      end
+    end
+  end
 end
