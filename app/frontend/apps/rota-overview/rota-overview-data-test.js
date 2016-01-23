edb5ab4992741fb7edb25d3875f8814b41fa9626
Matt Zeunert <matt@mostlystatic.com>
2016-01-23 18:27:06 +0000

    Implement getStaffTypeBreakDownByTime.

--- app/frontend/apps/rota-overview/rota-overview-data-test.js
+++ app/frontend/apps/rota-overview/rota-overview-data-test.js
@@ -1,10 +1,78 @@
 import expect from "expect"
+import { getStaffTypeBreakDownByTime, _getSamplingTimeOffsetsForDay } from "./rota-overview-data"
+import RotaDate from "~lib/rota-date"
+import _ from "underscore"
+
+describe("_getSamplingTimeOffsetsForDay", function(){
+    it("determines the sampling times to use based on the sampling granularity", function(){
+        var expectedOffsetsInMinutes = [
+            0, 4, 8, 12, 16, 20, 24
+        ].map((x) => x * 60);
+        expect(_getSamplingTimeOffsetsForDay(4 * 60)).toEqual(expectedOffsetsInMinutes);
+    })
+})
 
 describe("getStaffTypeBreakDownByTime", function() {
-    expect(false).toBe(true)
-    // it("Shows how many staff members ")
-    // it("Can generate a date object based on a time string", function(){
-    //     expect(date.getDateFromShiftEndTimeString("16:00")).toEqual(new Date(2015, 5, 12, 16, 0));
-    //     expect(date.getDateFromShiftStartTimeString("8:00")).toEqual(new Date(2015, 5, 12, 8, 0));
-    // });
+        var rotaDate = new RotaDate(new Date(2015,10, 1, 14, 0, 0));
+
+        var shifts = [
+            {
+                staff_id: 1,
+                starts_at: rotaDate.getDateFromShiftStartTime(10, 0),
+                ends_at: rotaDate.getDateFromShiftEndTime(22, 0)
+            },
+            {
+                staff_id: 2,
+                starts_at: rotaDate.getDateFromShiftStartTime(16, 0),
+                ends_at: rotaDate.getDateFromShiftEndTime(2, 0)
+            },
+            {
+                staff_id: 3,
+                starts_at: rotaDate.getDateFromShiftStartTime(14, 0),
+                ends_at: rotaDate.getDateFromShiftEndTime(8, 0)
+            }
+    ];
+    var staff = [
+        {
+            id: 1,
+            staff_type: "bar_back"
+        },
+        {
+            id: 2,
+            staff_type: "bar_back"
+        },
+
+        {
+            id: 3,
+            staff_type: "kitchen"
+        }
+    ];
+    var expectedResult = {
+        0: { // 8am
+            bar_back: 0,
+            kitchen: 0
+        },
+        [6 * 60]: { // 2pm
+            bar_back: 1,
+            kitchen: 1
+        },
+        [12 * 60]: { // 8pm
+            bar_back: 2,
+            kitchen: 1
+        },
+        [18 * 60]: { // 2am
+            bar_back: 1,
+            kitchen: 1
+        },
+        [24 * 60]: { // 8am
+            bar_back: 0,
+            kitchen: 1
+        }
+    }
+
+    var staffTypes = {kitchen: {}, bar_back: {}};
+
+    it("Determines the staff count for each staff type at different times during the day", function(){
+        expect(getStaffTypeBreakDownByTime(shifts, staff, 60 * 6, staffTypes)).toEqual(expectedResult)
+    })
 });
--- app/frontend/apps/rota-overview/rota-overview-data.js
+++ app/frontend/apps/rota-overview/rota-overview-data.js
@@ -1,21 +1,74 @@
-export getStaffTypeBreakdownByTime = function(shifts, staff, granularityInMinutes){
-    
+import _ from "underscore"
+import RotaDate from "~lib/rota-date"
+
+const MINUTES_PER_DAY = 24 * 60;
+
+export function _getSamplingTimeOffsetsForDay(granularityInMinutes){
+    var samplingTimes = [];
+
+    var lastSamplingTimeInMinutes = 0;
+    while (lastSamplingTimeInMinutes <= MINUTES_PER_DAY) {
+        samplingTimes.push(lastSamplingTimeInMinutes);
+
+        lastSamplingTimeInMinutes += granularityInMinutes;
+    }
+
+    return samplingTimes;
 }
-//     getStaffTypeBreakdownAt(hours, minutes) {
 
-//     }
-//     getStaffTypeBreakdownByTime(shifts, staff, granularityInMinutes){
-//         for (start,end in intervals) {
+export function getStaffTypeBreakDownByTime(shifts, staff, granularityInMinutes, staffTypes){
+    function getStaffTypeFromShift(shift) {
+        return _(staff).find({id: shift.staff_id}).staff_type;
+    }
+    function getInitialBreakdownAtSamplingPoint(){
+        var obj = {};
+        for (var staffType in staffTypes) {
+            obj[staffType] = 0;
+        }
+        return obj;
+    }
 
-//         }
+    var rotaDate = new RotaDate(shifts[0].starts_at)
 
-//         arr[intervalId] = 0;
+    var samplingTimeOffsets = _getSamplingTimeOffsetsForDay(granularityInMinutes)
+    var breakDown = {};
 
-//         for shift(){
-//             if (wotking)
-//             for each interval(){
-//                 ++arr[intervalId]
-//             }
-//         }
-//     }
-// }
+    var initialBreakdownAtSamplingPoint = getInitialBreakdownAtSamplingPoint();
+    samplingTimeOffsets.forEach(function(offset){
+        breakDown[offset] = _.clone(initialBreakdownAtSamplingPoint);
+    })
+
+    var samplingOffsetsAndDates = samplingTimeOffsets.map(function(offset){
+        var startTimeClone = new Date(rotaDate.startTime);
+        var date = new Date(startTimeClone.setMinutes(rotaDate.startTime.getMinutes() + offset));
+        return {
+            offset, date
+        };
+    });
+
+    shifts.forEach(function(shift, i){
+        var staffType = getStaffTypeFromShift(shift);
+        samplingOffsetsAndDates.forEach(function(samplingPoint){
+            var shiftCoversSamplingPoint = samplingPoint.date >= shift.starts_at && samplingPoint.date <= shift.ends_at;
+            console.log("---", shiftCoversSamplingPoint, samplingPoint, shift)
+            breakDown[samplingPoint.offset][staffType] += shiftCoversSamplingPoint ? 1 : 0;
+        })
+    });
+
+    console.log("breakDown", JSON.stringify(breakDown, null, 4));
+    return breakDown;
+    //         for (start,end in intervals) {
+
+    //         }
+
+    //         arr[intervalId] = 0;
+
+    //         for shift(){
+    //             if (wotking)
+    //             for each interval(){
+    //                 ++arr[intervalId]
+    //             }
+    //         }
+    //     }
+
+}
