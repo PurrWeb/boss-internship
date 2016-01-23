85b547cf5e09592abbd5e5a4f3b8959045d15089
Matt Zeunert <matt@mostlystatic.com>
2016-01-23 16:40:27 +0000

    Add empty rota overview data module.

---
+++ app/frontend/apps/rota-overview/rota-overview-data-test.js
@@ -0,0 +1,10 @@
+import expect from "expect"
+
+describe("getStaffTypeBreakDownByTime", function() {
+    expect(false).toBe(true)
+    // it("Shows how many staff members ")
+    // it("Can generate a date object based on a time string", function(){
+    //     expect(date.getDateFromShiftEndTimeString("16:00")).toEqual(new Date(2015, 5, 12, 16, 0));
+    //     expect(date.getDateFromShiftStartTimeString("8:00")).toEqual(new Date(2015, 5, 12, 8, 0));
+    // });
+});
---
+++ app/frontend/apps/rota-overview/rota-overview-data.js
@@ -0,0 +1,21 @@
+export getStaffTypeBreakdownByTime = function(shifts, staff, granularityInMinutes){
+    
+}
+//     getStaffTypeBreakdownAt(hours, minutes) {
+
+//     }
+//     getStaffTypeBreakdownByTime(shifts, staff, granularityInMinutes){
+//         for (start,end in intervals) {
+
+//         }
+
+//         arr[intervalId] = 0;
+
+//         for shift(){
+//             if (wotking)
+//             for each interval(){
+//                 ++arr[intervalId]
+//             }
+//         }
+//     }
+// }
