1e3f528c15b582bbafc0e2e3b85d89ca54084358
Matt Zeunert <matt@mostlystatic.com>
2016-01-31 07:51:11 +0000

    Add code to load react component on add staff page.

---
+++ app/frontend/apps/staff-member-form/staff-member-form-avatar-image.js
@@ -0,0 +1,8 @@
+import React from "react"
+import StaffImageInput from "~components/staff-image-input"
+
+export default class StaffMemberFormAvatarImage extends React.Component {
+    render(){
+        return <StaffImageInput></StaffImageInput>
+    }
+}
---
+++ app/frontend/components/staff-image-input.js
@@ -0,0 +1,7 @@
+import React from "react"
+
+export default class StaffImageInput extends React.Component {
+    render(){
+        return <div>Staff Image Input</div>
+    }
+}
