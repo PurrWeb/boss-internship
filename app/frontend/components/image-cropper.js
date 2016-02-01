6cbd3bfa9632fd2b11de82d2cecc69d012ba0111
Matt Zeunert <matt@mostlystatic.com>
2016-02-01 18:12:59 +0000

    Allow zooming in staff image cropper.

--- app/frontend/components/image-cropper.js
+++ app/frontend/components/image-cropper.js
@@ -25,17 +25,27 @@ export default class ImageCropper extends React.Component {
     }
     updateCropper(){
         if (!this.props.sourceImage) {
+            if (this.cropper) {
+                this.cropper.destroy();
+                this.cropper = null;
+            }
             return;
         }
+        var callOnChange = () => this.callOnChange(this.cropper);
+        callOnChange = _.throttle(callOnChange, 500); // prevent calling too often while zooming
         this.cropper = new Cropper(this.img, {
             aspectRatio: 1,
-            zoomable: false,
+            zoomable: true,
             viewMode: 2,
             autoCropArea: 1,
             toggleDragModeOnDblClick: false,
-            cropend: () => this.callOnChange(this.cropper),
-            built: () => this.callOnChange(this.cropper)
+            cropend: callOnChange,
+            built: callOnChange,
+            zoom: callOnChange
         });
+    }
+    componentWillUnmount() {
+        this.cropper.destroy();
     }
     callOnChange(cropper){
         var dataUrl = cropper.getCroppedCanvas().toDataURL("image/jpeg");
