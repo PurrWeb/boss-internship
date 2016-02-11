899010991989f33b3064852b9b3ee55b6c99e8eb
Matt Zeunert <matt@mostlystatic.com>
2016-02-11 09:01:41 +0000

    Add test checking that shift updates show a confirm dialog on published rotas.

---
+++ app/frontend/redux/rota-actions-test.js
@@ -0,0 +1,32 @@
+import expect from "expect"
+import {addRotaShift, updateRotaShift, deleteRotaShift} from "./actions"
+
+describe("Rota Actions", function(){
+    beforeEach(function(){
+        expect.spyOn(window, "confirm");
+    });
+
+    afterEach(function(){
+        expect.restoreSpies();
+    });    
+
+    it("Shows a confirmation dialog before adding a shift to a published rota", function(){
+        var storeState = {
+            pageOptions: {
+                displayedRota: 99
+            },
+            rotas: {
+                99: {
+                    status:"published"
+                }
+            }
+        };
+
+        var dispatch = function(){};
+        var getState = function(){return storeState}
+        addRotaShift({})(dispatch, getState);
+        updateRotaShift({})(dispatch, getState);
+        deleteRotaShift({})(dispatch, getState);
+        expect(window.confirm.calls.length).toBe(3);
+    });
+});
