112840593fd6a9fa9ed90e22063cd41ae0cf6b8f
William Billington <william.billington@gmail.com>
2016-02-14 22:56:27 +0000

    Stop printing uglify warnings

--- webpack/production.config.js
+++ webpack/production.config.js
@@ -1,3 +1,12 @@
 const config = require('./../webpack.config');
+var webpack = require('webpack');
+
+config.plugins.push(
+    new webpack.optimize.UglifyJsPlugin({
+      compress: {
+        warnings: false
+      }
+    })
+);
 
 module.exports = config;
