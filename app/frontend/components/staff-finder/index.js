6ecc109c1d7639da45fff277ea0fb9f6e0ef07ed
Matt Zeunert <matt@mostlystatic.com>
2016-02-18 22:28:26 +0000

    Only show staff types with staff members in staff finder.

--- app/frontend/components/staff-finder/index.js
+++ app/frontend/components/staff-finder/index.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react"
 import StaffFilter from "~components/staff-finder/staff-filter"
 import FilterableStaffList from "~components/staff-finder/filterable-staff-list"
+import { selectStaffTypesWithStaffMembers } from "~redux/selectors"
 import _ from "underscore"
 
 export default class StaffFinder extends Component {
@@ -18,7 +19,7 @@ export default class StaffFinder extends Component {
     render() {
         return <div>
             <StaffFilter
-                staffTypes={this.props.staffTypes}
+                staffTypes={this.getStaffTypesWithStaffMembers()}
                 onChange={(arg) => this.onFilterChange(arg)} />
 
             <FilterableStaffList
@@ -26,6 +27,9 @@ export default class StaffFinder extends Component {
                 staffItemComponent={this.props.staffItemComponent}
                 filterSettings={this.state.staffFilterSettings} />
         </div>
+    }
+    getStaffTypesWithStaffMembers(){
+        return selectStaffTypesWithStaffMembers(this.props.staffTypes, this.props.staff);
     }
     onFilterChange(filterSettings) {
         this.setState({staffFilterSettings: filterSettings});
