dae4b6a9ad2e5849ad66b95d754ec8761a5d13ef
Matt Zeunert <matt@mostlystatic.com>
2016-02-18 13:20:30 +0000

    Fix rotas reducer test.

--- app/frontend/redux/rotas-reducer-test.js
+++ app/frontend/redux/rotas-reducer-test.js
@@ -2,7 +2,7 @@ import rotasReducer from "./rotas-reducer"
 import expect from "expect"
 
 describe("rotasReducer", function(){
-    it("Can update the state of a rota", function(){
+    it("Can update the status of a rota", function(){
         var initalState = {
             8: {
                 id: 8,
@@ -12,8 +12,7 @@ describe("rotasReducer", function(){
 
         var action = {
             type: "UPDATE_ROTA_STATUS_SUCCESS",
-            venueId: 8,
-            date: "02-02-2014",
+            rotaId: 8,
             status: "finished"
         }
 
