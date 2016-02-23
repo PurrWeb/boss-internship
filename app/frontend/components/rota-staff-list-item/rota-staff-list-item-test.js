f1d310fb9ab049baa98188dea728cdf16896a2c8
Matt Zeunert <matt@mostlystatic.com>
2016-02-23 15:26:13 +0000

    Fix test for rota staff list item.

--- app/frontend/components/rota-staff-list-item/rota-staff-list-item-test.js
+++ app/frontend/components/rota-staff-list-item/rota-staff-list-item-test.js
@@ -2,8 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom"
 import TestUtils from "react-addons-test-utils"
 import expect from "expect"
-import StaffListItem from "./staff-list-item"
+import StaffListItem from "./index"
 import {ContextProvider, NoOpComponent} from "~lib/test-helpers"
+import {createStore} from "redux"
 
 StaffListItem.__Rewire__('StaffTypeBadge', NoOpComponent);
 StaffListItem.__Rewire__('AddStaffToShiftButton', NoOpComponent);
@@ -17,11 +18,13 @@ describe('StaffListItem', function() {
     };
 
     var context = {
-        componentErrors: {},
-        staffTypes: {
-            3333: {}
-        }
-    }
+        store: createStore(() => {
+            componentErrors: {},
+            staffTypes: {
+                3333: {}
+            }
+        })
+    };
 
     it("shows the person's first and last name", function(){
         var item = TestUtils.renderIntoDocument(
