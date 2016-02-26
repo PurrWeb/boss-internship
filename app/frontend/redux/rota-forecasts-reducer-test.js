d81b8536025c876c1265b388884205d4612a9e78
Matt Zeunert <matt@mostlystatic.com>
2016-02-26 19:54:12 +0000

    Fix tests.

--- app/frontend/redux/rota-forecasts-reducer-test.js
+++ app/frontend/redux/rota-forecasts-reducer-test.js
@@ -2,13 +2,12 @@ import forecastsReducer from "./rota-forecasts-reducer"
 import expect from "expect"
 
 describe("forecastsReducer", function(){
-    it("Can add a new forecast and deletes the existing one if there's already one for that rota", function(){
+    it("Can add a new forecast and deletes the existing one if there's already one for that rota (date+venueId)", function(){
         var initalState = {
             5: {
                 id: 5,
-                rota: {
-                    id: 99
-                }
+                date: new Date(2016, 2, 1),
+                venueId: 20
             }
         }
 
@@ -16,18 +15,45 @@ describe("forecastsReducer", function(){
             type: "UPDATE_ROTA_FORECAST_SUCCESS",
             rotaForecast: {
                 id: 14,
-                rota: {
-                    id: 99
-                }
+                date: new Date(2016, 2, 1),
+                venueId: 20
             }
         }
 
         var expectedResultState = {
             14: {
                 id: 14,
-                rota: {
-                    id: 99
-                }
+                date: new Date(2016, 2, 1),
+                venueId: 20
+            }
+        }
+
+        expect(forecastsReducer(initalState, action)).toEqual(expectedResultState);
+    });
+
+    it("Can add a new forecast that has the same ID as the previous one", function(){
+        var initalState = {
+            5: {
+                id: 5,
+                date: new Date(2016, 2, 1),
+                venueId: 20
+            }
+        }
+
+        var action = {
+            type: "UPDATE_ROTA_FORECAST_SUCCESS",
+            rotaForecast: {
+                id: 5,
+                date: new Date(2016, 2, 1),
+                venueId: 20
+            }
+        }
+
+        var expectedResultState = {
+            5: {
+                id: 5,
+                date: new Date(2016, 2, 1),
+                venueId: 20
             }
         }
 
