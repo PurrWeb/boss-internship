38cf7708f7745dd7d6c3eabec75a71bdfac6e424
William Billington <william.billington@gmail.com>
2016-02-29 23:46:57 +0000

    Remove stupid comment

--- app/models/pay_rate.rb
+++ app/models/pay_rate.rb
@@ -4,13 +4,6 @@ class PayRate < ActiveRecord::Base
   validates :pay_rate_type, inclusion: { in: TYPES, message: 'is invalid' }
   validates :cents_per_hour, numericality: { greater_than: 0 }
 
-  #validate do |pay_rate|
-  #  case pay_rate_type
-  #  when 'named'
-  #    NamedPayrateValidator.new(pay_rate).validate
-  #  end
-  #end
-
   def self.named
     where(pay_rate_type: 'named')
   end
