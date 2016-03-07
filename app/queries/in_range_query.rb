ff5b8b9c0d1f4af47b8abbdfaa266a2c00675a11
William Billington <william.billington@gmail.com>
2016-03-07 16:19:57 +0000

    Allow shift boundries to overlap without conflict

--- app/queries/in_range_query.rb
+++ app/queries/in_range_query.rb
@@ -1,14 +1,19 @@
 class InRangeQuery
-  def initialize(relation:, start_value:, end_value:, start_column_name: 'starts_at', end_column_name: 'ends_at')
+  def initialize(relation:, start_value:, end_value:, start_column_name: 'starts_at', end_column_name: 'ends_at', include_boundaries: true)
     @relation = relation
     @start_value = start_value
     @end_value = end_value
     @start_column_name = start_column_name
     @end_column_name = end_column_name
+    @include_boundaries = include_boundaries
   end
 
   def all
-    relation.where("(? <= `#{end_column_name}`) AND (? >= `#{start_column_name}`)", start_value, end_value)
+    if include_boundaries
+      relation.where("(? <= `#{end_column_name}`) AND (? >= `#{start_column_name}`)", start_value, end_value)
+    else
+      relation.where("(? < `#{end_column_name}`) AND (? > `#{start_column_name}`)", start_value, end_value)
+    end
   end
 
   private
@@ -17,5 +22,6 @@ class InRangeQuery
     :start_value,
     :end_value,
     :start_column_name,
-    :end_column_name
+    :end_column_name,
+    :include_boundaries
 end
