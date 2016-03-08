29d9d7130f2669d5d91b4a1c84dd670a67bae95e
William Billington <william.billington@gmail.com>
2016-03-08 10:09:36 +0000

    Scope conflicting holidays query to relevant staff member

--- app/models/not_on_holiday_validator.rb
+++ app/models/not_on_holiday_validator.rb
@@ -1,22 +1,26 @@
 class NotOnHolidayValidator
-  def initialize(record)
-    @record = record
+  def initialize(rota_shift)
+    @rota_shift = rota_shift
   end
 
   def validate
-    rota_date = RotaShiftDate.to_rota_date(record.starts_at)
+    rota_date = RotaShiftDate.to_rota_date(rota_shift.starts_at)
+
+    staff_member_holidays = Holiday.
+      in_state(:enabled).
+      where(staff_member: rota_shift.staff_member)
 
     conflicting_holidays = HolidayInRangeQuery.new(
-      relation: Holiday.in_state(:enabled),
+      relation: staff_member_holidays,
       start_date: rota_date,
       end_date: rota_date
     ).all
 
     if conflicting_holidays.present?
-      record.errors.add(:base, 'Staff member is on holiday')
+      rota_shift.errors.add(:base, 'Staff member is on holiday')
     end
   end
 
   private
-  attr_accessor :record
+  attr_accessor :rota_shift
 end
