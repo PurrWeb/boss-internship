7dbb539876250ceaf13c9d5d27e0c2166eea251e
William Billington <william.billington@gmail.com>
2016-03-09 10:49:08 +0000

    Prevent email errors on staging

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -78,12 +78,6 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-    :user_name => '539115ce1447638c6',
-    :password => '75313dfadc6231',
-    :address => 'mailtrap.io',
-    :domain => 'mailtrap.io',
-    :port => '2525',
-    :authentication => :cram_md5
-  }
+  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.default_url_options = { :host => 'staging-boss.jsmbars.co.uk', :protocol => 'https'}
 end
