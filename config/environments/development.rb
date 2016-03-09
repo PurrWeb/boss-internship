135f65c37b9d6c093fcba2886e16a13937a64ef4
William Billington <william.billington@gmail.com>
2016-03-09 15:55:36 +0000

    Improve dev email previews

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -13,9 +13,8 @@ Rails.application.configure do
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = false
 
-  # Don't care if the mailer can't send.
   Rails.application.routes.default_url_options[:host] = 'localhost'
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.delivery_method = :letter_opener
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
