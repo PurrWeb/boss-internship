96010cbac5acb692f162fb2077e74ee9bf14e24a
William Billington <william.billington@gmail.com>
2016-03-21 09:20:16 +0000

    Update staff_member_index_filter_query_spec

--- spec/models/staff_member_index_filter_query_spec.rb
+++ spec/models/staff_member_index_filter_query_spec.rb
@@ -23,118 +23,189 @@ describe StaffMemberIndexFilterQuery do
   let(:venue_scope) { double("venue scope") }
   let(:accessible_venues) { double("accessible venues") }
   let(:accessible_venue_ids) { double "accessible venue ids" }
+  let(:venue_join_relation) { double "venue join relation" }
+  let(:result) { double 'result' }
   let(:query) do
     StaffMemberIndexFilterQuery.new(
+      status: status,
+      email_text: email_text,
+      name_text: name_text,
       staff_type: staff_type,
       venue: venue,
       relation: relation,
       accessible_venues: accessible_venues
     )
   end
+  let(:status) { nil }
+  let(:email_text) { nil }
+  let(:name_text) { nil }
 
-  context 'staff type is present' do
+  before do
+    allow(staff_type).to receive(:present?).and_return(false)
+    allow(venue).to receive(:present?).and_return(false)
+    allow(relation).to receive(:joins).with('LEFT JOIN `staff_member_venues` ON `staff_member_venues`.`staff_member_id` = `staff_members`.`id`').and_return(venue_join_relation)
+    allow(venue_join_relation).to receive(:where).with('(`staff_member_venues`.`staff_member_id` IS NULL) OR (`staff_member_venues`.`venue_id` IN (?))', accessible_venue_ids).and_return(result)
+    allow(accessible_venues).to receive(:pluck).with(:id).and_return(accessible_venue_ids)
+  end
+
+  specify do
+    expect(query.all).to eq(result)
+  end
+
+  context 'filtering by status enabled' do
+    let(:status) { 'enabled' }
+    let(:relation_filtered_by_enabled) { double 'relation filtered by enabled' }
+
     before do
-      allow(Venue).to receive(:where).and_return(venue_scope)
-      allow(accessible_venues).to receive(:pluck).and_return(accessible_venue_ids)
-      allow(relation).to receive(:enabled).and_return(relation_filtered_by_enabled)
-      allow(relation_filtered_by_enabled).to receive(:where).and_return(relation_filtered_by_staff_type)
-      allow(relation_filtered_by_staff_type).to(
-        receive(:joins).and_return(relation_joined_by_venue)
+      allow(relation).to receive(:enabled).and_return(
+        relation_filtered_by_enabled
       )
-      allow(relation_joined_by_venue).to(
-        receive(:merge).and_return(relation_filtered_by_venue)
-      )
+    allow(relation_filtered_by_enabled).to receive(:joins).with('LEFT JOIN `staff_member_venues` ON `staff_member_venues`.`staff_member_id` = `staff_members`.`id`').and_return(venue_join_relation)
     end
 
-    it 'should filter by enabled' do
-      expect(relation).to receive(:enabled).and_return(relation_filtered_by_enabled)
-      query.all
-    end
-
-    it 'should filter by staff type' do
-      expect(relation_filtered_by_enabled).to receive(:where).with(staff_type: staff_type)
-      query.all
-    end
-
-    it 'should filter by venue' do
-      expect(relation_filtered_by_staff_type).to receive(:joins).with(:venue)
-      expect(relation_joined_by_venue).to receive(:merge).with(venue_scope)
-      query.all
-    end
-
-    it 'should return filtered venue' do
-      expect(query.all).to eq(relation_filtered_by_venue)
+    specify do
+      expect(query.all).to eq(result)
     end
   end
 
-  context 'when no venue is present' do
+  context 'filtering by status disabled' do
+    let(:status) { 'disabled' }
+    let(:relation_filtered_by_disabled) { double 'relation filtered by enabled' }
+
     before do
-      allow(venue).to receive(:present?).and_return(false)
-      allow(Venue).to receive(:where).and_return(venue_scope)
-      allow(accessible_venues).to receive(:pluck).and_return(accessible_venue_ids)
-      allow(relation).to receive(:enabled).and_return(relation_filtered_by_enabled)
-      allow(relation_filtered_by_enabled).to receive(:where).and_return(relation_filtered_by_staff_type)
-      allow(relation_filtered_by_staff_type).to(
-        receive(:joins).and_return(relation_joined_by_venue)
+      allow(relation).to receive(:disabled).and_return(
+        relation_filtered_by_disabled
       )
-      allow(relation_joined_by_venue).to(
-        receive(:where).and_return(relation_filtered_by_venue)
-      )
+    allow(relation_filtered_by_disabled).to receive(:joins).with('LEFT JOIN `staff_member_venues` ON `staff_member_venues`.`staff_member_id` = `staff_members`.`id`').and_return(venue_join_relation)
     end
 
-    it 'should filter by enabled' do
-      expect(relation).to receive(:enabled).and_return(relation_filtered_by_enabled)
-      query.all
-    end
-
-    it 'should filter by staff type' do
-      expect(relation_filtered_by_enabled).to receive(:where).with(staff_type: staff_type)
-      query.all
-    end
-
-    it 'should filter by accessible venues' do
-      expect(relation_filtered_by_staff_type).to receive(:joins).with('LEFT JOIN `staff_member_venues` ON `staff_member_venues`.`staff_member_id` = `staff_members`.`id`')
-      expect(relation_joined_by_venue).to receive(:where).with('(`staff_member_venues`.`staff_member_id` IS NULL) OR (`staff_member_venues`.`venue_id` IN (?))', accessible_venue_ids)
-      query.all
-    end
-
-    it 'should return filtered venue' do
-      expect(query.all).to eq(relation_filtered_by_venue)
+    specify do
+      expect(query.all).to eq(result)
     end
   end
 
-  context 'when no staff type is present' do
+  context 'filtering by staff type' do
+    let(:relation_filtered_by_staff_type) { double 'relation filtered by staff type' }
+
     before do
-      allow(staff_type).to receive(:present?).and_return(false)
-      allow(Venue).to receive(:where).and_return(venue_scope)
-      allow(accessible_venues).to receive(:pluck).and_return(accessible_venue_ids)
-      allow(relation).to receive(:enabled).and_return(relation_filtered_by_enabled)
-      allow(relation_filtered_by_enabled).to(
-        receive(:joins).and_return(relation_joined_by_venue)
+      allow(staff_type).to receive(:present?).and_return(true)
+      allow(relation).to receive(:where).
+        with(staff_type: staff_type).
+        and_return(relation_filtered_by_staff_type)
+      allow(relation_filtered_by_staff_type).to(
+        receive(:joins).
+          with('LEFT JOIN `staff_member_venues` ON `staff_member_venues`.`staff_member_id` = `staff_members`.`id`').
+          and_return(venue_join_relation)
+      )
+    end
+
+    specify do
+      expect(query.all).to eq(result)
+    end
+  end
+
+  context 'filtering by venue' do
+    let(:relation_joined_by_venue) { double 'relation joined by venue' }
+    let(:venue_relation) { double 'venue relation' }
+
+    before do
+      allow(venue).to receive(:present?).and_return(true)
+      allow(relation).to(
+        receive(:joins).
+          with(:venue).
+          and_return(relation_joined_by_venue)
       )
       allow(relation_joined_by_venue).to(
-        receive(:merge).and_return(relation_filtered_by_venue)
+        receive(:merge).
+          with(venue_relation).
+          and_return(result)
+      )
+      allow(Venue).to(
+        receive(:where).
+        with(
+          '(`venues`.`id` = ?) AND (`venues`.`id` IN (?))',
+          venue_id,
+          accessible_venue_ids
+        ).
+        and_return(venue_relation)
       )
     end
 
-    it 'should filter supplied relation by specified venue' do
-      expect(relation).to receive(:enabled).and_return(relation_filtered_by_enabled)
+    specify 'should filter by venue' do
+      expect(relation_joined_by_venue).to(
+        receive(:merge).
+          with(venue_relation).
+          and_return(result)
+      )
+      expect(Venue).to(
+        receive(:where).
+        with(
+          '(`venues`.`id` = ?) AND (`venues`.`id` IN (?))',
+          venue_id,
+          accessible_venue_ids
+        ).
+        and_return(venue_relation)
+      )
       query.all
     end
 
-    it 'should filter supplied relation by specified venue' do
-      expect(relation_filtered_by_enabled).to receive(:joins).with(:venue)
-      expect(relation_joined_by_venue).to receive(:merge).with(venue_scope)
-      query.all
+    specify do
+      expect(query.all).to eq(result)
+    end
+  end
+
+  context 'filtering by name' do
+    let(:name_text) { 'example name' }
+    let(:relation_joined_by_name) { double 'relation joined by name' }
+    let(:relation_filtered_by_name) { double 'relation filtered by name' }
+
+    before do
+      allow(relation).to(
+        receive(:joins).
+          with(:name).
+          and_return(relation_joined_by_name)
+      )
+      allow(relation_joined_by_name).to(
+        receive(:where).
+          with(
+            "(`names`.first_name LIKE ?) OR (`names`.surname LIKE ?)",
+            "%#{name_text}%",
+            "%#{name_text}%"
+          ).
+          and_return(relation_filtered_by_name)
+      )
+      allow(relation_filtered_by_name).to receive(:joins).with('LEFT JOIN `staff_member_venues` ON `staff_member_venues`.`staff_member_id` = `staff_members`.`id`').and_return(venue_join_relation)
     end
 
-    it 'should not filter by staff type' do
-      expect(relation).to_not receive(:where).with(staff_type: staff_type)
-      query.all
+    specify do
+      expect(query.all).to eq(result)
+    end
+  end
+
+  context 'filtering by email' do
+    let(:email_text) { 'example email' }
+    let(:relation_joined_by_email) { double 'relation joined by email' }
+    let(:relation_filtered_by_email) { double 'relation filtered by email' }
+
+    before do
+      allow(relation).to(
+        receive(:joins).
+          with(:email_address).
+          and_return(relation_joined_by_email)
+      )
+      allow(relation_joined_by_email).to(
+        receive(:where).
+          with(
+            "LOWER(`email_addresses`.email) = LOWER(?)",
+            email_text
+          ).
+          and_return(relation_filtered_by_email)
+      )
+      allow(relation_filtered_by_email).to receive(:joins).with('LEFT JOIN `staff_member_venues` ON `staff_member_venues`.`staff_member_id` = `staff_members`.`id`').and_return(venue_join_relation)
     end
 
-    it 'should return filtered venue' do
-      expect(query.all).to eq(relation_filtered_by_venue)
+    specify do
+      expect(query.all).to eq(result)
     end
   end
 end
