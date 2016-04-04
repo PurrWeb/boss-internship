f144ff33b0a2bf024d689131d0f0cb193d57ae82
Matt Zeunert <matt@mostlystatic.com>
2016-04-04 17:29:23 +0100

    Fail tests if console.error is called.

--- webpack/tests.config.js
+++ webpack/tests.config.js
@@ -1,2 +1,13 @@
 var context = require.context('../app/frontend', true, /-test\.jsx?$/);
 context.keys().forEach(context);
+
+var originalErrorFunction = console.error;
+beforeEach(function(){
+    console.error = function(){
+        originalErrorFunction.apply(this, arguments);    
+        throw new Error("Tests fail if console.error is called, mostly to catch PropType issues")
+    }
+});
+afterEach(function(){
+    console.error = originalErrorFunction;
+})
