14720de41cdc44806011b947f2908bdbf83554b1
William Billington <william.billington@gmail.com>
2016-04-06 10:17:12 +0100

    Allow security staff be assigned a venue

--- app/models/staff_member_venue_validator.rb
+++ app/models/staff_member_venue_validator.rb
@@ -4,12 +4,8 @@ class StaffMemberVenueValidator
   end
 
   def validate
-    if staff_member.staff_type.present?
-      if security_staff_with_venue?(staff_member)
-        staff_member.errors.add(:venues, 'must be blank for security')
-      elsif non_security_staff_without_venue?(staff_member)
-        staff_member.errors.add(:venues, 'cannot be blank')
-      end
+    if staff_member.staff_type.present? && non_security_staff_without_venue?(staff_member)
+      staff_member.errors.add(:venues, 'cannot be blank')
     end
   end
 
