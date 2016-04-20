d5e3b355f4f0a25d2c2297c228cb5470d9b03d5e
William Billington <william.billington@gmail.com>
2016-04-20 18:24:01 +0100

    Feed data to frontend clock in out page

--- app/controllers/clock_in_clock_out_controller.rb
+++ app/controllers/clock_in_clock_out_controller.rb
@@ -1,5 +1,34 @@
 class ClockInClockOutController < ApplicationController
   def index
     authorize! :manage, :clock_in_clock_out
+
+    venue = venue_from_params
+    rota_date = RotaShiftDate.to_rota_date(Time.current)
+
+    api_key = ApiKey.current_for(venue: venue)
+    raise ActiveRecord::RecordNotFound unless api_key.present?
+
+    staff_members = venue.staff_members.enabled
+
+    rota = Rota.find_or_initialize_by(venue: venue, date: rota_date)
+
+    rota_shifts = rota.rota_shifts.enabled
+
+    staff_types = StaffType.all
+
+    render locals: {
+      api_key: api_key,
+      rota_date: rota_date,
+      staff_members: staff_members,
+      staff_types: staff_types,
+      rota_shifts: rota_shifts,
+      rota: rota,
+      venue: venue
+    }
+  end
+
+  private
+  def venue_from_params
+    Venue.find(params[:venue_id])
   end
 end
