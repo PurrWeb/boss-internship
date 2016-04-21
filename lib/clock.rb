2a4dc799a1cc5ada732d00f9c88ddd76f3d7231f
William Billington <william.billington@gmail.com>
2016-04-21 18:32:26 +0100

    Update reminder emails

--- lib/clock.rb
+++ lib/clock.rb
@@ -48,9 +48,9 @@ module Clockwork
   end
 
   every(20.minutes, "ShiftUpdateNotificationJob")
-  every(1.week, "ChangeOrderReminderJob",     at: "Monday 02:00", tz: 'Europe/London')
-  every(1.week, "TuesdayFruitOrderReminderJob", at: "Monday 22:00", tz: 'Europe/London')
-  every(1.week, "FridayFruitOrderReminderJob", at: "Thursday 22:00", tz: 'Europe/London')
+  every(1.week, "ChangeOrderReminderJob",     at: "Sunday 23:00", tz: 'Europe/London')
+  every(1.week, "TuesdayFruitOrderReminderJob", at: "Sunday 23:00", tz: 'Europe/London')
+  every(1.week, "FridayFruitOrderReminderJob", at: "Thursday 23:00", tz: 'Europe/London')
   every(1.hour, "BackupDatabaseJob",      at: "**:00")
   every(1.day,  "CleanBackupsJob",        at: "23:00")
   every(1.day, "SiaBadgeExpiryNotificationJob", at: "9:00")
