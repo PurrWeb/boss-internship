bd99a5434bcb0c1946d94d3ac6afb036341a3649
Matt Zeunert <matt@mostlystatic.com>
2016-04-22 01:57:19 +0100

    Tweak text position.

--- app/frontend/apps/hours-confirmation/components/hours-chart.js
+++ app/frontend/apps/hours-confirmation/components/hours-chart.js
@@ -66,7 +66,7 @@ class HoursChartUi extends React.Component {
             })
         rotaedMarkers
             .append("text")
-            .attr("transform", "translate(3, 14)")
+            .attr("transform", "translate(3, 16)")
             .text(function(interval){
                 return interval.label;
             })
