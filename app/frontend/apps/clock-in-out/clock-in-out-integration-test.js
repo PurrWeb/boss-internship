e7a357aa76da76d522a4eb69ac6a48694c955538
Matt Zeunert <matt@mostlystatic.com>
2016-05-03 16:28:22 +0100

    Fix clock in/out page integration test.

--- app/frontend/apps/clock-in-out/clock-in-out-integration-test.js
+++ app/frontend/apps/clock-in-out/clock-in-out-integration-test.js
@@ -9,37 +9,39 @@ import _ from "underscore"
 describe("Clock In/Out Page Integration Test", function(){
     var renderedApp, getNode, $$;
 
-    window.staffMembers = [{
-        "id":2,
-        "staff_type":{"id":7},
-        "first_name":"Dermot",
-        "surname":"O'Boyle"
-    }];
-    window.clock_in_statuses = [{
-        "staff_member":{"id":2},
-        "status":"clocked_out"
-    }];
-    window.staffTypes = [{
-        "id":7,
-        "name":"Bar Supervisor",
-        "color":"#bb4dff"
-    }];
-    window.rotaShifts = [];
-    window.venues = [{
-        "id":1,
-        "name":"McCooley's",
-        "staff_members":[{"id":2}]
-    }];
-    window.rotas = [{
-        "id":307,
-        "venue":{"id":1},
-        "date":"2016-04-29",
-        "status":"published"
-    }];
-    window.pageData = {
-      rotaDate: "2016-04-29",
-      rotaVenueId: 1
-    }
+    var data = {
+        staff_members: [{
+            "id":2,
+            "staff_type":{"id":7},
+            "first_name":"Dermot",
+            "surname":"O'Boyle"
+        }],
+        clock_in_statuses: [{
+            "staff_member":{"id":2},
+            "status":"clocked_out"
+        }],
+        staff_types: [{
+            "id":7,
+            "name":"Bar Supervisor",
+            "color":"#bb4dff"
+        }],
+        rota_shifts: [],
+        venues: [{
+            "id":1,
+            "name":"McCooley's",
+            "staff_members":[{"id":2}]
+        }],
+        rotas: [{
+            "id":307,
+            "venue":{"id":1},
+            "date":"2016-04-29",
+            "status":"published"
+        }],
+        page_data: {
+          rota_date: "2016-04-29",
+          rota_venue_id: 1
+        }
+    };
 
     it("Renders without errors", function(){
         renderedApp = simpleRender(<ClockInOutApp />)
@@ -52,12 +54,16 @@ describe("Clock In/Out Page Integration Test", function(){
     })
 
     it("Shows a large staff type selector after loading the venue's clock in/out data", function(done){
+        var promise = new Promise(function(resolve, reject){
+            resolve(data);
+        })
+        expect.spyOn($, "ajax").andReturn(promise)
         ReactTestUtils.Simulate.submit(getNode());
 
-        setTimeout(function(){
+        _.defer(function(){
             expect($$(".large-staff-type-selector__button").length).toBeGreaterThan(0);
             done();
-        }, 2500)
+        }, 0)
     })
 
     it("Shows a list of staff members after selecting a staff type", function(){
