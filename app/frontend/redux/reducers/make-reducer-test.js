1d359f07b39e7668b2b49a1aaf5104bf7b6593ec
Matt Zeunert <matt@mostlystatic.com>
2016-05-20 14:48:59 +0100

    Add test for makeReducer.

---
+++ app/frontend/redux/reducers/make-reducer-test.js
@@ -0,0 +1,60 @@
+import expect from "expect"
+import makeReducer, {makeHandlerForGenericReplaceAction} from "./make-reducer"
+
+describe("makeReducer", function(){
+    beforeEach(function(){
+        makeReducer.__Rewire__("actionTypes", {ADD: "ADD"});
+    })
+    afterEach(function(){
+        makeReducer.__ResetDependency__("actionTypes")
+    })
+    it("Creates a reducer that handles the correct action", function(){
+        var reducer = makeReducer({
+            ADD: function(state, action){
+                return state + action.amount
+            }
+        })
+
+        var newState = reducer(5, {
+            type: "ADD",
+            amount: 10
+        })
+
+        expect(newState).toBe(15)
+    })
+
+    it("Throws an exception when trying to handle a non-existent action", function(){
+        var reducer = makeReducer({});
+        expect(function(){
+            reducer(0, {type: "DOESNT_EXIST"})
+        }).toThrow();
+    })
+
+    it("Throws an exception when tring to define a handler for a non-existent action", function(){
+        expect(function(){
+            var reducer = makeReducer({
+                DOESNT_EXIST: function(){}
+            })
+        }).toThrow();
+    })
+})
+
+describe("makeReducer - makeHandlerForGenericReplaceAction", function(){
+    var handler = makeHandlerForGenericReplaceAction("names")
+
+    it("Returns a modified function that returns the new value if the property name matches", function(){
+        var newState = handler([], {
+            type: "GENERIC_REPLACE_ALL_ITEMS",
+            names: ["John", "Emma"]
+        })
+        expect(newState).toEqual(["John", "Emma"])
+    })
+
+    it("If the property name doesn't match the original state is returned", function(){
+        var newState = handler([], {
+            type: "GENERIC_REPLACE_ALL_ITEMS",
+            oranges: ["Orange 1", "Orange 2"]
+        })
+        expect(newState).toEqual([]);
+    })
+})
