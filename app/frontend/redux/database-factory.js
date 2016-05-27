e22a82b29e73abb1dc92c67a0eff7989a7988aa2
Matt Zeunert <matt@mostlystatic.com>
2016-05-27 18:54:40 +0100

    Use ES6 class for DatabaseFactory.

--- app/frontend/redux/database-factory.js
+++ app/frontend/redux/database-factory.js
@@ -1,48 +1,46 @@
 import { combineReducers } from "redux"
 
-function DatabaseFactory(){
-    this.actionTypes = [];
-    this.actionCreators = {};
-    this.reducers = {}
-}
-DatabaseFactory.prototype = {
-    registerActionTypes: function(actionTypes){
+export default class DatabaseFactory {
+    constructor(){
+        this.actionTypes = [];
+        this.actionCreators = {};
+        this.reducers = {}
+    }
+    registerActionTypes(actionTypes){
         actionTypes.forEach((actionType) =>
             this.actionTypes.push(actionType)
         )
-    },
+    }
     registerActionCreator(name, actionCreatorFunction) {
         if (this.actionCreators[name] !== undefined) {
             throw Error("Action creator " + name + " already exists")
         }
         this.actionCreators[name] = actionCreatorFunction;
-    },
-    registerActionCreators: function(actionCreators){
+    }
+    registerActionCreators(actionCreators){
         for (var name in actionCreators){
             this.registerActionCreator(name, actionCreators[name]);
         }
-    },
+    }
     registerReducer(name, reducerFunction){
         if (this.reducers[name] !== undefined){
             throw Error("Reducer " + name + " already exists")
         }
         this.reducers[name] = reducerFunction;
-    },
-    validate: function(){
+    }
+    validate(){
 
-    },
-    getActionTypes: function(){
+    }
+    getActionTypes(){
         this.validate()
         return this.actionTypes
-    },
-    getActionCreators: function(){
+    }
+    getActionCreators(){
         this.validate();
         return this.actionCreators
-    },
-    getRootReducer: function(){
+    }
+    getRootReducer(){
         this.validate();
         return combineReducers(this.reducers)
     }
 }
-
-export default DatabaseFactory
