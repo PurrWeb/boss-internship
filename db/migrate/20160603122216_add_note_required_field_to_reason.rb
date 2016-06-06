3e42c16e81f5a464edeccb961bc2b69e0d5b4f94
William Billington <william.billington@gmail.com>
2016-06-06 10:20:02 +0100

    Remove data from AddNoteRequiredFieldToReason migration

--- db/migrate/20160603122216_add_note_required_field_to_reason.rb
+++ db/migrate/20160603122216_add_note_required_field_to_reason.rb
@@ -3,12 +3,5 @@ class AddNoteRequiredFieldToReason < ActiveRecord::Migration
     change_table :hours_acceptance_reasons do |t|
       t.boolean :note_required, null: false, default: false
     end
-
-    HoursAcceptanceReason.create!(
-      text: 'Other',
-      enabled: true,
-      rank: 10000,
-      note_required: true
-    )
   end
 end
