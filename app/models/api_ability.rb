7c108d2b3f987ba5423290501ee2a6e5c1c01c57
William Billington <william.billington@gmail.com>
2016-06-27 17:21:05 +0100

    Create has_manager_mode_access? ability helper

--- app/models/api_ability.rb
+++ app/models/api_ability.rb
@@ -5,18 +5,21 @@ class ApiAbility
     # Define abilities for the passed in user here. For example:
     #
     can :perform_clocking_action, StaffMember do |other_staff_member|
-      staff_member.manager? ||
-        staff_member.bar_supervisor? ||
+      has_manager_mode_access?(staff_member) ||
         staff_member == other_staff_member
     end
 
     can :change_pin, StaffMember do |other_staff_member|
-      staff_member.manager?
+      has_manager_mode_access?(staff_member)
     end
 
     can :add_note, StaffMember do |other_staff_member|
-      staff_member.manager? ||
-        staff_member.bar_supervisor?
+      has_manager_mode_access?(staff_member)
     end
+  end
+
+  def has_manager_mode_access?(staff_member)
+    staff_member.manager? ||
+      staff_member.bar_supervisor?
   end
 end
