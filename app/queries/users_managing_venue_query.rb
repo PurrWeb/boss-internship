bd4de6eb1595b955b44987d4a2bf27b00fd181a1
William Billington <william.billington@gmail.com>
2016-06-28 16:14:23 +0100

    Only send reminders to managers who specifically manage the venue

--- app/queries/users_managing_venue_query.rb
+++ app/queries/users_managing_venue_query.rb
@@ -4,20 +4,10 @@ class UsersManagingVenueQuery
   end
 
   def all
-    all_venues_users = User.
-      enabled.
-      with_all_venue_access
-
-    specific_venue_users = User.
+    User.
       enabled.
       joins(:venue_users).
       where('venue_users.venue_id = ?', venue.id)
-
-    QueryCombiner.new(
-      base_scope: User,
-      relation_1: all_venues_users,
-      relation_2: specific_venue_users
-    ).all
   end
 
   private
