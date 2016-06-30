5a174bbf3e208d12e759063e97a255e2553350f7
Matt Zeunert <matt@mostlystatic.com>
2016-06-30 17:38:40 +0100

    Move selectClockInDayDetails.

--- app/frontend/redux/selectors.js
+++ app/frontend/redux/selectors.js
@@ -50,10 +50,12 @@ export {
 }
 
 import {
-    selectClockInDay
+    selectClockInDay,
+    selectClockInDayDetails
 } from "./selectors/clock-in-day"
 export {
-    selectClockInDay
+    selectClockInDay,
+    selectClockInDayDetails
 }
 
 import {
@@ -173,74 +175,4 @@ export function selectRotaShiftsOnDayOnStaffTypeRotaPage(state){
     });
     var rotaShifts = selectShiftsWithRotaClientIds(state, clientIds);
     return rotaShifts;
-}
-
-
-function addBreaksToClockInPeriod(clockInPeriod, clockInBreaks){
-    var breaks = _(clockInBreaks).filter(function(clockInBreak){
-        return clockInBreak.clock_in_period.clientId === clockInPeriod.clientId
-    })
-    return Object.assign({}, clockInPeriod, { breaks })
-}
-
-function addBreaksToHoursAcceptancePeriod(hoursAcceptancePeriod, hoursAcceptanceBreaks){
-    var breaks = _(hoursAcceptanceBreaks).filter(function(hoursAcceptanceBreak){
-        return hoursAcceptanceBreak.hours_acceptance_period.clientId === hoursAcceptancePeriod.clientId
-    })
-    return Object.assign({}, hoursAcceptancePeriod, { breaks })
-}
-
-export function selectClockInDayDetails(state, clockInDay){
-    var staffMember = clockInDay.staff_member.get(state.staffMembers);
-    var clockInDay = {...clockInDay}
-    clockInDay.forceClockoutIsInProgress = selectIsForceClockingOutClockInDay(state, staffMember.clientId)
-
-    var clockInPeriods = _(state.clockInPeriods).filter(function(clockInPeriod){
-        return clockInPeriod.clock_in_day.clientId === clockInDay.clientId;
-    });
-    var hoursAcceptancePeriods = _(state.hoursAcceptancePeriods).filter(function(hoursAcceptancePeriod){
-        return hoursAcceptancePeriod.clock_in_day.clientId === clockInDay.clientId;
-    })
-
-    clockInPeriods = clockInPeriods.map(function(clockInPeriod){
-        return addBreaksToClockInPeriod(clockInPeriod, state.clockInBreaks)
-    })
-    hoursAcceptancePeriods = hoursAcceptancePeriods.map(function(hoursAcceptancePeriod){
-        return addBreaksToHoursAcceptancePeriod(hoursAcceptancePeriod, state.hoursAcceptanceBreaks)
-    })
-    hoursAcceptancePeriods.forEach(function(hoursAcceptancePeriod){
-        hoursAcceptancePeriod.updateIsInProgress = selectEditHoursAcceptancePeriodIsInProgress(state, hoursAcceptancePeriod)
-        var hasReason = hoursAcceptancePeriod.hours_acceptance_reason !== null;
-        hoursAcceptancePeriod.hours_acceptance_reason =
-            state.hoursAcceptanceReasons[hoursAcceptancePeriod.hours_acceptance_reason.clientId]
-    })
-
-    var rota = getRotaFromDateAndVenue({
-        dateOfRota: clockInDay.date,
-        venueId: clockInDay.venue.clientId,
-        rotas: state.rotas
-    })
-
-    var rotaedShifts = _(state.rotaShifts).filter(function(shift){
-        return shift.rota.clientId === rota.clientId && shift.staff_member.clientId === staffMember.clientId
-    })
-
-    var clockInEvents = _(state.clockInEvents).filter(function(clockInEvent){
-        var clockInPeriod = clockInEvent.clock_in_period.get(state.clockInPeriods)
-        return clockInPeriod.clock_in_day.clientId === clockInDay.clientId
-    })
-
-    var clockInNotes = _(state.clockInNotes).filter(function(clockInNote){
-        return clockInNote.clock_in_day.clientId === clockInDay.clientId;
-    })
-
-    return {
-        clockedClockInPeriods: clockInPeriods,
-        hoursAcceptancePeriods: hoursAcceptancePeriods,
-        clockInEvents,
-        staffMember,
-        rotaedShifts,
-        clockInNotes,
-        clockInDay
-    }
 }
