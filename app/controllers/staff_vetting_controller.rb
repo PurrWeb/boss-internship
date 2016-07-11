7151d6b898a0e7d8c905ec39dfbd851cf5919b93
William Billington <william.billington@gmail.com>
2016-07-11 13:17:36 +0100

    Add Staff Vetting page

---
+++ app/controllers/staff_vetting_controller.rb
@@ -0,0 +1,57 @@
+class StaffVettingController < ApplicationController
+  def index
+    authorize! :manage, :admin
+
+    staff_without_email = StaffMember.
+      enabled.
+      where(email_address_id: nil).
+      includes([:name, :master_venue])
+
+    staff_members_table = Arel::Table.new(:staff_members)
+    staff_member_transitions = Arel::Table.new(:staff_member_transitions)
+    most_recent_staff_member_transitions = staff_member_transitions.alias('most_recent_staff_member_transition')
+
+    staff_without_ni_number_query = staff_members_table.
+      join(most_recent_staff_member_transitions, Arel::Nodes::OuterJoin).
+      on(
+        staff_members_table[:id].eq(
+          most_recent_staff_member_transitions[:staff_member_id]
+        ).
+        and(
+          most_recent_staff_member_transitions[:most_recent].eq(1)
+        )
+      ).
+      where(
+        most_recent_staff_member_transitions[:to_state].eq(nil).
+        or(
+          most_recent_staff_member_transitions[:to_state].eq("enabled")
+        )
+      ).
+      where(
+        staff_members_table[:national_insurance_number].eq(nil).
+        or(
+          Arel.sql("`staff_members`.`national_insurance_number` REGEXP '^[[:blank:]]+$'")
+        )
+      ).
+      project(staff_members_table[Arel.star])
+    staff_without_ni_number = StaffMember.find_by_sql(staff_without_ni_number_query)
+
+
+    staff_without_address = StaffMember.
+      enabled.
+      where(address_id: nil).
+      includes([:name, :master_venue])
+
+    staff_without_photo = StaffMember.
+      enabled.
+      where(avatar: nil).
+      includes([:name, :master_venue])
+
+    render locals: {
+      staff_without_email: staff_without_email,
+      staff_without_ni_number: staff_without_ni_number,
+      staff_without_address: staff_without_address,
+      staff_without_photo: staff_without_photo
+    }
+  end
+end
