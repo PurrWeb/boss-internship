8c27a0803fada8aced19aacca28c1260919c57c6
William Billington <william.billington@gmail.com>
2016-09-26 14:55:24 +0100

    Use Rails.application#assets[] to get digest of bundle js file

--- app/helpers/sourcemap_helper.rb
+++ app/helpers/sourcemap_helper.rb
@@ -4,7 +4,6 @@ module SourcemapHelper
   extend self
 
   def sourcemap_version
-    #Digest::SHA256.hexdigest File.read("#{Rails.root}/app/assets/javascripts/bundles/frontend_bundle.js.map")
-    ActionController::Base.helpers.asset_path('bundles/frontend_bundle.js').match(/frontend_bundle-(.*).js/).captures.first
+    Rails.application.assets['bundles/frontend_bundle.js'].digest
   end
 end
