7aca5f0624296d27cb0083c43b1289a9d24ea328
Yan Takushevich <takushevich@gmail.com>
2016-10-24 15:02:26 +0300

    Foundation Staff Members page.

--- app/models/staff_member_show_page.rb
+++ app/models/staff_member_show_page.rb
@@ -1,5 +1,8 @@
 class StaffMemberShowPage
-  def self.tab_class(section, active_tab)
-    "active" if active_tab.to_s == section.to_s
+  def self.tab_title_class(section, active_tab)
+    active_tab.to_s == section.to_s ? "tabs-title is-active" : "tabs-title"
+  end
+  def self.tab_content_class(section, active_tab)
+    active_tab.to_s == section.to_s ? "tabs-panel is-active" : "tabs-panel"
   end
 end
