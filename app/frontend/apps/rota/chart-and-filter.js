acdfdecb5c8d9161d88a5dda9f8341e2d900bdcd
Yan Takushevich <takushevich@gmail.com>
2016-10-25 20:14:21 +0300

    Styling on Rota show Graph.

--- app/frontend/apps/rota/chart-and-filter.js
+++ app/frontend/apps/rota/chart-and-filter.js
@@ -26,7 +26,7 @@ export class ChartAndFilter extends Component {
 
         return (
             <div className="row">
-                <div className="column">
+                <div className="small-12 medium-8 column">
                     <RotaChart
                         rotaShifts={rotaShifts}
                         staff={this.props.staff}
@@ -38,7 +38,7 @@ export class ChartAndFilter extends Component {
                         getShiftColor={(shift) => this.getShiftColor(shift)}
                         />
                 </div>
-                <div className="shrink column">
+                <div className="small-12 medium-4 column">
                     <label>Filter chart</label>
                     <StaffTypeDropdown
                         className="mb-base"
