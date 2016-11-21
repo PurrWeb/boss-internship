9aaf2a6a0e5011c546f45c89e96fab92e1e63e5e
William Billington <william.billington@gmail.com>
2016-11-21 15:58:35 +0000

    Add finance report PDF download

--- app/controllers/finance_reports_controller.rb
+++ app/controllers/finance_reports_controller.rb
@@ -13,7 +13,24 @@ class FinanceReportsController < ApplicationController
         filter_by_weekly_pay_rate: filter_by_weekly_pay_rate
       ).all
 
-      render_finance_reports_index(week: week, venue: venue, staff_members: staff_members)
+      respond_to do |format|
+        format.html do
+          render_finance_reports_index(
+            week: week,
+            venue: venue,
+            staff_members: staff_members
+          )
+        end
+
+        format.pdf do
+          render_finance_reports_pdf(
+            week: week,
+            venue: venue,
+            staff_members: staff_members,
+            filter_by_weekly_pay_rate: filter_by_weekly_pay_rate
+          )
+        end
+      end
     else
       redirect_to(finance_reports_path(index_redirect_params))
     end
@@ -30,6 +47,36 @@ class FinanceReportsController < ApplicationController
       ),
       pay_rate_filtering: params[:pay_rate_filter]
     }
+  end
+
+  def render_finance_reports_pdf(week:, venue:, filter_by_weekly_pay_rate:, staff_members:)
+    pdf = FinanceReportPDF.new(
+      venue: venue,
+      week: week,
+      filter_by_weekly_pay_rate: filter_by_weekly_pay_rate
+    )
+
+    staff_members.each do |staff_member|
+      pdf.add_report(
+        staff_type: staff_member.staff_type,
+        report: (
+          FinanceReport.find_by(
+            staff_member: staff_member,
+            week_start: week.start_date
+          ) || GenerateFinanceReportData.new(
+            staff_member: staff_member,
+            week: week
+          ).call.report
+        )
+      )
+    end
+
+    #TODO: Extract File Timestamp Format to somewhere
+    timestamp_start = week.start_date.strftime('%d-%b-%Y')
+    timestamp_end = week.end_date.strftime('%d-%b-%Y')
+    filename  = "#{venue.name.parameterize}_finance_report_#{timestamp_start}_#{timestamp_end}.pdf"
+    headers['Content-Disposition'] = "attachment; filename=#{filename}"
+    render text: pdf.render, content_type: 'application/pdf'
   end
 
   def create
