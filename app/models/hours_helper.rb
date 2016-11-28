6d4a8fb8bcd15a836b9b411e70f711e7d9fa1dd3
William Billington <william.billington@gmail.com>
2016-11-28 16:51:28 +0000

    Display owed hour minutes as composite value

--- app/models/hours_helper.rb
+++ app/models/hours_helper.rb
@@ -7,6 +7,25 @@ class HoursHelper
    (total_minutes % 60).floor
   end
 
+  def self.hour_and_minute_text(total_minutes)
+    parts = []
+    hours = hours_from_total_minutes(total_minutes)
+    minutes = hour_minutes_from_total_minutes(total_minutes)
+
+    if hours > 0
+      parts << "#{hours} Hours"
+      if minutes > 0
+        parts << " and "
+      end
+    end
+
+    if minutes > 0
+      parts << "#{minutes} Minutes"
+    end
+
+    parts.join(" ")
+  end
+
   def self.total_minutes_from_hours_and_minutes(hours:, minutes:)
     (hours * 60) + minutes
   end
