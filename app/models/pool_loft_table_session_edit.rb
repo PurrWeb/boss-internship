c7a4f719dcac43f02c09d0446d208e5a3cbf66af
William Billington <william.billington@gmail.com>
2017-01-11 10:31:35 +0000

    REmice unused from PoolLoftTableSessionEdit

--- app/models/pool_loft_table_session_edit.rb
+++ app/models/pool_loft_table_session_edit.rb
@@ -1,9 +1,5 @@
 class PoolLoftTableSessionEdit < ActiveRecord::Base
   validates :guid, presence: true
-  validates :table_id, numericality: { integer_only: 0, greater_than_or_equal_to: 0 }
-  validates :table_guid, presence: true
-  validates :table_name, presence: true
-  validates :table_type, inclusion: { in: PoolLoftTable::TABLE_TYPES }
   validates :table_session_id, numericality: { integer_only: 0, greater_than_or_equal_to: 0 }
   validates :admin_token_id, numericality: { integer_only: 0, greater_than_or_equal_to: 0 }
   validates :admin_token_guid, presence: true
