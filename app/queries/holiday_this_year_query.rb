d9cf56dc1a7aa5d25a3e32802720a1733ee0eb1d
William Billington <william.billington@gmail.com>
2017-02-08 16:48:07 +0000

    Use HolidayThisYearQuery everywhere

--- app/queries/holiday_this_year_query.rb
+++ app/queries/holiday_this_year_query.rb
@@ -5,35 +5,17 @@ class HolidayThisYearQuery
   end
 
   def all
+    tax_year = TaxYear.new(today)
+
     HolidayInRangeQuery.new(
       relation: relation,
-      start_date: start_date,
-      end_date: end_date,
+      start_date: tax_year.start_date,
+      end_date: tax_year.end_date
     ).all
   end
 
-  def past_current_year_start?
-    today > this_holiday_year_start
-  end
-
-  def this_holiday_year_start
-    today.beginning_of_year + 3.months
-  end
-
-  def start_date
-    if past_current_year_start?
-      this_holiday_year_start
-    else
-      this_holiday_year_start - 1.year
-    end
-  end
-
-  def end_date
-    if past_current_year_start?
-      this_holiday_year_start + 1.year
-    else
-      this_holiday_year_start
-    end
+  def count
+    all.count
   end
 
   private
