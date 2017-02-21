f638d932869a76738b4355c72a0b0a5f637f0179
vadimnekrasov <gdekefir@gmail.com>
2017-02-21 21:28:07 +0300

    apply window.boss

--- app/frontend/apps/add-staff-member/index.tsx
+++ app/frontend/apps/add-staff-member/index.tsx
@@ -3,10 +3,26 @@ import {Provider} from 'react-redux';
 
 import store from '../../store/index';
 import App from './components/common-page-content';
+import accessTokenChanged from '../../action-creators/access-token-changed';
+import payrateValuesChanged from '../../action-creators/payrate-values-changed';
+import genderValuesChanged from '../../action-creators/gender-values-changed';
+import staffTypeIdsChanged from '../../action-creators/staff-type-ids-changed';
+import venueValuesChanged from '../../action-creators/venue-values-changed';
+import {BossData} from '../../interfaces/common-data-types';
 // tslint:disable-next-line:no-require-imports
 require('../../../assets/stylesheets/new-style3/index.scss');
 
 const Root = class extends React.Component <{}, {}> {
+  componentDidMount() {
+    const boss: BossData = (window as any).boss;
+
+    store.dispatch( accessTokenChanged(boss.accessToken) );
+    store.dispatch( payrateValuesChanged(boss.payrateValues) );
+    store.dispatch( genderValuesChanged(boss.genderValues) );
+    store.dispatch( staffTypeIdsChanged(boss.staffTypeIds) );
+    store.dispatch( venueValuesChanged(boss.venueValues ) );
+  }
+
   render() {
     return (
       <Provider store={store}>
