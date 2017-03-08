6bfb2c1f08c1b668371edac9a5e7a90dc9ea3c7d
vadimnekrasov <gdekefir@gmail.com>
2017-03-08 11:33:23 +0300

    repair behavior of combo-boxes

--- app/frontend/components/venue-dropdown.js
+++ app/frontend/components/venue-dropdown.js
@@ -5,29 +5,34 @@ import getArrayOfIdsFromReactSelectValue from "~lib/get-array-of-ids-from-react-
 
 export default class VenueDropdown extends Component {
     static propTypes = {
-        selectedVenues: React.PropTypes.array.isRequired,
+        selectedVenues: React.PropTypes.arrayOf(React.PropTypes.string),
         venues: React.PropTypes.object.isRequired,
         onChange: React.PropTypes.func.isRequired,
         multi: React.PropTypes.bool,
         clearable: React.PropTypes.bool // defaults to true
-    }
+    };
     render(){
         var venueOptions = _.values(this.props.venues).map(function(venue){
             return {
                 value: venue.clientId,
                 label: venue.name
             };
-        })
+        });
 
         var multi = this.props.multi !== undefined ? this.props.multi : false;
+        const delimiter = ',';
+        const values = this.props.selectedVenues.join(delimiter);
 
         return <Select
-            value={this.props.selectedVenues}
+            value={values}
+            delimiter={delimiter}
             options={venueOptions}
             placeholder="All Venues"
             clearable={this.props.clearable !== undefined ? this.props.clearable : true}
             multi={multi}
-            onChange={(value) => {
+            onChange={(optionData) => {
+              const value = (optionData || {}).value || '';
+
                 this.props.onChange(
                     getArrayOfIdsFromReactSelectValue(value)
                 )
