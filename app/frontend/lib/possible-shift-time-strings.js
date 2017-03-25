53e5cbb370fbfd3f310917ef5d9b681b9c068c99
William Billington <william.billington@gmail.com>
2017-03-25 13:21:43 +0000

    Calculate select time values separate to calendar date

--- app/frontend/lib/possible-shift-time-strings.js
+++ app/frontend/lib/possible-shift-time-strings.js
@@ -8,7 +8,7 @@ function unmemoizedGetPossibleShiftTimes(intervalSizeInMinutes) {
     var rotaDate = new RotaDate({dateOfRota: new Date()});
     var possibleShiftStartTimeStrings = minutesOffsets.map(function(offset){
         var time = new Date(rotaDate.startTime);
-        time.setMinutes(time.getMinutes() + offset);
+        time = new Date(time.valueOf() + (offset * 60 * 1000 ));
         return moment(time).format("HH:mm");
     });
     var possibleShiftEndTimeStrings = _.clone(possibleShiftStartTimeStrings);
