27ca8240bbb321d836f32b13907d5ef3867320a3
Riqwan Thamir <rmthamir@gmail.com>
2017-05-05 08:57:34 +0530

    Added uploads in answer serializer

--- app/serializers/api/v1/questionnaire_answer_serializer.rb
+++ app/serializers/api/v1/questionnaire_answer_serializer.rb
@@ -1,6 +1,8 @@
 class Api::V1::QuestionnaireAnswerSerializer < ActiveModel::Serializer
   attributes :id, :questionnaire_question_id, :questionnaire_response_id, :value, :passed, :note
 
+  has_many :uploads
+
   def passed
     if object.questionnaire_question.is_a?(BinaryQuestion)
       object.pass_value?
