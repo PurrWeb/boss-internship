bcc9c6f8f53500e5e9081c0b448861aa2a8f6208
Igor Pugachev <ipugachev84@gmail.com>
2017-06-06 16:23:14 +0600

    Add names tests

---
+++ spec/feature/names/access_spec.rb
@@ -0,0 +1,56 @@
+require 'rails_helper'
+
+RSpec.describe 'Names pages access' do
+  include Rack::Test::Methods
+
+  def app
+    Rails.application
+  end
+
+  before do
+    login_as user if user.present?
+  end
+
+  describe 'index page' do
+    let(:url) { url_helpers.invites_path }
+
+    context 'manager' do
+      let(:user) { FactoryGirl.create(:user, :manager) }
+
+      specify 'should not have access' do
+        expect {
+          get(url)
+        }.to raise_error(CanCan::AccessDenied)
+      end
+    end
+
+    context 'admin' do
+      let(:user) { FactoryGirl.create(:user, :admin) }
+
+      specify 'should have access' do
+        expect(get(url).status).to eq(ok_status)
+      end
+    end
+
+    context 'dev' do
+      let(:user) { FactoryGirl.create(:user, :dev) }
+
+      specify 'should have access' do
+        expect(get(url).status).to eq(ok_status)
+      end
+    end
+  end
+
+  private
+  def url_helpers
+    Rails.application.routes.url_helpers
+  end
+
+  def ok_status
+    200
+  end
+
+  def redirect_status
+    302
+  end
+end
---
+++ spec/feature/pages/names_index_page.rb
@@ -0,0 +1,33 @@
+module PageObject
+  class NamesIndexPage < Page
+    include FlashHelpers
+
+    def surf_to
+      visit(url_helpers.names_path)
+    end
+
+    def navigation
+      @navigation ||= NavigationBar.new(self)
+    end
+
+    def filter
+      @filter ||= NamesIndexFilter.new(self)
+    end
+
+    def assert_on_correct_page
+      expect(page_heading).to(
+        have_text(expected_page_heading_text),
+        "expected page heading to have test '#{expected_page_heading_text}' got '#{page_heading.text}'"
+      )
+    end
+
+    private
+    def page_heading
+      page.find('main h1')
+    end
+
+    def expected_page_heading_text
+      'Names'
+    end
+  end
+end
