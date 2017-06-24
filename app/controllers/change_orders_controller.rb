7bf3a91228b889628efff857fde338bcefbd982c
Riqwan Thamir <rmthamir@gmail.com>
2017-06-24 12:46:06 +0530

    [card-2] Completed Change order form

--- app/controllers/change_orders_controller.rb
+++ app/controllers/change_orders_controller.rb
@@ -1,4 +1,6 @@
 class ChangeOrdersController < ApplicationController
+  before_action :set_new_layout, only: [:index]
+
   def index
     if venue_from_params.present?
       current_venue = venue_from_params
