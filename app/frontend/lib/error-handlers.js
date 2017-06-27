3b732914f6871eafba88bf0bc62bb2bfbab0c827
Igor Pugachev <ipugachev84@gmail.com>
2017-06-27 19:46:22 +0600

    Refactor `ErrorHandler` implement `class`

--- app/frontend/lib/error-handlers.js
+++ app/frontend/lib/error-handlers.js
@@ -1,24 +1,22 @@
-export default window.handleError = {
-    throwWithErrorPage: (errorText) => {
-        setTimeout(() => {
-            require('../assets/sass/index.sass');
-            let url = require("url-loader?limit=1000000!../../assets/images/something-went-wrong.jpg");
-
-            document.body.innerHTML = `
-              <main class="boss-page-main boss-page-main_full">
-                <div class="boss-page-main__content">
-                    <div class="boss-page-main__inner">
-                        <div class="boss-error">
-                            <h1 class="boss-error__title">Something has gone horribly wrong</h1>
-                            <img src="${url}" alt="Something went wrong" class="boss-error__image" style="max-width: 400px; max-height: 400px">
-                            <p class="boss-error__text">There was an error in the code on this page. If the problem persists please contact Sina and let him know about it so that he can relay this information to the technical team.</p>
-                            <a href="javascript:;" class="boss-button boss-button_role_reload-page boss-error__button" onClick="location.reload()">Reload page</a>
-                        </div>
-                    </div>
-                </div>
-              </main>
-            `;
-        }, 500);
-        throw Error(errorText);
-    }
+class Errorhandler {
+  throwErrorPage() {
+    require('../assets/sass/index.sass');
+    let url = require("url-loader?limit=1000000!../../assets/images/something-went-wrong.jpg");
+    document.body.innerHTML = `
+      <main class="boss-page-main boss-page-main_full">
+        <div class="boss-page-main__content">
+          <div class="boss-page-main__inner">
+            <div class="boss-error">
+              <h1 class="boss-error__title">Something has gone horribly wrong</h1>
+              <img src="${url}" alt="Something went wrong" class="boss-error__image" style="max-width: 400px; max-height: 400px">
+              <p class="boss-error__text">There was an error in the code on this page. If the problem persists please contact Sina and let him know about it so that he can relay this information to the technical team.</p>
+              <a href="javascript:;" class="boss-button boss-button_role_reload-page boss-error__button" onClick="location.reload()">Reload page</a>
+            </div>
+          </div>
+        </div>
+      </main>
+    `;
+  }
 }
+
+export default new Errorhandler();
