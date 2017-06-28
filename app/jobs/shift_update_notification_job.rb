26be3e00eed1d8a3f4e8a9ff8a353120546c76ae
William Billington <william.billington@gmail.com>
2017-06-28 16:02:24 +0100

    Fix shift_update_notification_job

--- app/jobs/shift_update_notification_job.rb
+++ app/jobs/shift_update_notification_job.rb
@@ -1,7 +1,7 @@
 class ShiftUpdateNotificationJob < ActiveJob::Base
   def perform
-    staff_members = StaffMemberWithShiftNotificationsQuery.new.all
     job_time = Time.current
+    staff_members = StaffMemberWithShiftNotificationsQuery.new.all
     staff_members.find_each do |staff_member|
       SendStaffMemberShiftUpdateJob.set(wait_until: job_time).perform_later(staff_member.id)
       job_time = job_time + 20.seconds
--- spec/feature/shift_update_notifications_spec.rb
+++ spec/feature/shift_update_notifications_spec.rb
@@ -4,18 +4,13 @@ RSpec.describe 'Shift notification Emails' do
   include ActiveSupport::Testing::TimeHelpers
 
   context 'when staff member is marked as requiring notification' do
-    let(:this_week) { RotaWeek.new(RotaShiftDate.to_rota_date(Time.current)) }
+    let(:now) { Time.current }
+    let(:this_week) { RotaWeek.new(RotaShiftDate.to_rota_date(now)) }
     let(:next_week) { RotaWeek.new(this_week.start_date + 1.week) }
-    let(:now) { this_week.start_date }
-    around(:each) do |example|
-      travel_to now do
-        example.run
-      end
-    end
 
     let(:staff_member) { FactoryGirl.create(:staff_member, shift_change_occured_at: now) }
     let(:rota) { FactoryGirl.create(:rota, :published, date: next_week.start_date) }
-    let!(:shift) do
+    let(:shift) do
       FactoryGirl.create(
         :rota_shift,
         rota: rota,
@@ -23,6 +18,12 @@ RSpec.describe 'Shift notification Emails' do
         starts_at: RotaShiftDate.new(rota.date).start_time,
         ends_at: RotaShiftDate.new(rota.date).start_time + 2.hours
       )
+    end
+    before do
+      travel_to now do
+        #initialise objects
+        shift
+      end
     end
 
     context 'when less than 30 minutes have passed' do
@@ -34,8 +35,10 @@ RSpec.describe 'Shift notification Emails' do
 
     context 'when more than 30 minutes have passed' do
       specify 'running the ShiftUpdateNotificationJob should send the staff member an update email' do
-        travel_to(Time.zone.now + 31.minutes) do
+        travel_to(now + 32.minutes) do
           ShiftUpdateNotificationJob.new.perform
+        end
+        travel_to(now + 34.minutes) do
           expect(ActionMailer::Base.deliveries).to_not be_empty
         end
       end
