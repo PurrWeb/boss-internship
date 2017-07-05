fbc0a651b1ef304bdb194d84a049a6e3e5080721
William Billington <william.billington@gmail.com>
2017-07-05 13:40:10 +0100

    Add venue to QuestionnaireResponse model

--- app/models/questionnaire_response.rb
+++ app/models/questionnaire_response.rb
@@ -1,7 +1,10 @@
 class QuestionnaireResponse < ActiveRecord::Base
   # Associations
   belongs_to :questionnaires
+  belongs_to :venue
   belongs_to :user
   has_many :questionnaire_answers, dependent: :destroy
   accepts_nested_attributes_for :questionnaire_answers
+
+  validates :venue, presence: true
 end
