e2fc1cfccc8a5b1ca22e6be7aa42122cdfc82169
William Billington <william.billington@gmail.com>
2017-07-06 18:07:42 +0100

    Remove trivial specs

--- spec/models/questionnaire_category_spec.rb
+++
@@ -1,6 +0,0 @@
-require 'rails_helper'
-
-RSpec.describe QuestionnaireCategory, type: :model do
-  it { is_expected.to have_and_belong_to_many :questionnaires }
-  it { is_expected.to have_many :questionnaire_questions }
-end
--- spec/models/questionnaire_spec.rb
+++
@@ -1,7 +0,0 @@
-require 'rails_helper'
-
-RSpec.describe Questionnaire, type: :model do
-  it { should have_and_belong_to_many :venues }
-  it { should have_and_belong_to_many :questionnaire_categories }
-  it { should have_many :questionnaire_questions }
-end
