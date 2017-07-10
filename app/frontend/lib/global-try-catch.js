8d0e912200930b0b4afc0737bba9ed3868d1d7e7
Igor Pugachev <ipugachev84@gmail.com>
2017-07-10 20:22:28 +0600

    Change global try catch rollbar additional data conditions

--- app/frontend/lib/global-try-catch.js
+++ app/frontend/lib/global-try-catch.js
@@ -3,6 +3,9 @@ import ReactDefaultBatchingStrategy from "react-dom/lib/ReactDefaultBatchingStra
 import errorHandler from '~lib/error-handlers';
 import oFetch from 'o-fetch';
 
+const rollbarData = oFetch(window, 'boss.rollbarData');
+const appVersion = oFetch(window, 'boss.currentVersion');
+
 const ReactTryCatchBatchingStrategy = {
   // this is part of the BatchingStrategy API. simply pass along
   // what the default batching strategy would do.
@@ -14,27 +17,27 @@ const ReactTryCatchBatchingStrategy = {
     } catch (e) {
       if (typeof Rollbar !== 'undefined') {
         let payload = {};
-        if (typeof oFetch(window, 'boss.rollbarData.currentVenue') !== 'undefined') {
-          let { id, name, rollbar_guid } = window.boss.rollbarData.currentVenue
+        payload.app_version = appVersion;
+
+        if (typeof rollbarData.currentVenue !== 'undefined') {
+          let { id, name, rollbar_guid } = rollbarData.currentVenue
           payload.venue = { id, name };
           payload.person = {
             id: rollbar_guid,
             username: `Venue: ${ name }`,
           }
         }
-        if (typeof oFetch(window, 'boss.currentVersion') !== 'undefined') {
-          payload.app_version = window.boss.currentVersion;
-        }
-        if (typeof oFetch(window, 'boss.rollbarData.currentUser') !== 'undefined') {
-          let { id, name, rollbar_guid } = window.boss.rollbarData.currentUser;
+
+        if (typeof rollbarData.currentUser !== 'undefined') {
+          let { id, name, rollbar_guid } = rollbarData.currentUser;
           payload.user = { id, name };
           payload.person = {
             id: rollbar_guid,
             username: `User: ${ name }`,
           }
         }
-        if (typeof oFetch(window, 'boss.rollbarData.currentStaffMember') !== 'undefined') {
-          let { id, name, rollbar_guid } = window.boss.rollbarData.currentStaffMember
+        if (typeof rollbarData.currentStaffMember !== 'undefined') {
+          let { id, name, rollbar_guid } = rollbarData.currentStaffMember
           payload.staff_member = { id, name };
           payload.person = {
             id: rollbar_guid,
