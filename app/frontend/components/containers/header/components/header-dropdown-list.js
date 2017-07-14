7a28793be4209e4df9bac9f9af9f04cfcd2b759f
Igor Pugachev <ipugachev84@gmail.com>
2017-07-14 12:56:30 +0600

    Update search input in quick menu

--- app/frontend/components/containers/header/components/header-dropdown-list.js
+++ app/frontend/components/containers/header/components/header-dropdown-list.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react'
 import utils from "~lib/utils"
 
-export const DropDownListItem = ({item}) => {
+export const DropDownListItem = ({item, color}) => {
   const {description, highlightedDescription, path} = item;
   return <div className="boss-alias">
-      <a href={path} className="boss-alias__link">
-      <span className="boss-alias__icon boss-alias__icon_type_solid boss-alias__icon_role_venue">{utils.generateQuickMenuAlias(description)}</span>
-      <span className="boss-alias__text boss-alias__text_role_venue" dangerouslySetInnerHTML={{__html: highlightedDescription || description}}></span>
+    <a href={path} className="boss-alias__link">
+      <span className="boss-alias__icon boss-alias__icon_type_solid" style={{backgroundColor: color, borderColor: color}}>{utils.generateQuickMenuAlias(description)}</span>
+      <span className="boss-alias__text" style={{color}} dangerouslySetInnerHTML={{__html: highlightedDescription || description}}></span>
     </a>
   </div>
 };
@@ -19,9 +19,9 @@ export default ({items}) => {
           <h4 className="boss-quick-access__group-title"> {item.name} </h4>
         </div>
         <div className="boss-quick-access__aliases">
-          { item.items.map( (item, key) => 
+          { item.items.map( (childItem, key) => 
             <div key={key} className="boss-quick-access__alias">
-              <DropDownListItem item={item} />
+              <DropDownListItem item={childItem} color={item.color} />
             </div>
           )}
         </div>
