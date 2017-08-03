45eda98b3298d20ac3daa96255b0291d5d60b046
William Billington <william.billington@gmail.com>
2017-08-03 14:10:55 +0100

    Update js paths

--- app/frontend/apps/hours-confirmation/components/hours-chart/hours-chart-content.js
+++ app/frontend/apps/hours-confirmation/components/hours-chart/hours-chart-content.js
@@ -1,7 +1,7 @@
 import React from "react"
 import d3 from "d3"
-import utils from "~lib/utils"
-import makeRotaHoursXAxis from "~lib/make-rota-hours-x-axis"
+import utils from "~/lib/utils"
+import makeRotaHoursXAxis from "~/lib/make-rota-hours-x-axis"
 import iScroll from 'iscroll'
 import ReactIScroll from 'react-iscroll'
 
