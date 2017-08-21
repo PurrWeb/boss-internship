ceb4d008b1afb81e12a4180d9003056e669336c1
William Billington <william.billington@gmail.com>
2017-08-21 10:33:16 +0100

    Fix accessbile venues on checklist pages

--- app/controllers/check_list_submissions_controller.rb
+++ app/controllers/check_list_submissions_controller.rb
@@ -11,17 +11,18 @@ class CheckListSubmissionsController < ApplicationController
       .all
       .includes(:check_list_submission_answers)
       .includes(:user)
-    
+
     submissions_page_data = ChecklistSubmissionsPageData.new(submissions: submissions, params: params)
     access_token = current_user.current_access_token || WebApiAccessToken.new(user: current_user).persist!
+    accessible_venues = AccessibleVenuesQuery.new(current_user).all
 
     render locals: {
       access_token: access_token,
       current_venue: Api::V1::VenueForSelectSerializer.new(venue_from_params),
-      venues:  ActiveModel::Serializer::CollectionSerializer.new(Venue.all, serializer: Api::V1::VenueForSelectSerializer),
+      venues:  ActiveModel::Serializer::CollectionSerializer.new(accessible_venues, serializer: Api::V1::VenueForSelectSerializer),
     }.merge(submissions_page_data.get_data)
   end
-  
+
   private
 
   def check_venue
