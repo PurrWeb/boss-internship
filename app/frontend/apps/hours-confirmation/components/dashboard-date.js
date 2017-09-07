f6227cc2bafce306d7e50d27269e336667047f1f
Igor Pugachev <ipugachev84@gmail.com>
2017-09-07 18:24:31 +0600

    Update  HoursConfirmation `view by date` view

---
+++ app/frontend/apps/hours-confirmation/components/dashboard-date.js
@@ -0,0 +1,71 @@
+import React from "react"
+import DashboardWrapper from './dashboard-wrapper';
+import VenueDropdown from "~/components/venue-dropdown"
+import moment from 'moment';
+import DatePicker from 'react-datepicker';
+
+export default function DashboardDate({
+  title,
+  date,
+  onChange,
+  venueClientId,
+  venues,
+}) {
+  const selectedVenues = [venueClientId];
+  const link = `/hours_confirmation/current?venue_id=${venues[venueClientId].serverId}`;
+
+  return (
+    <DashboardWrapper>
+      <div className="boss-page-dashboard__group">
+        <h1 className="boss-page-dashboard__title">
+          <span className="boss-page-dashboard__title-text boss-page-dashboard__title-text_marked">
+            {title}
+          </span>
+        </h1>
+        <div className="boss-page-dashboard__controls-group">
+          <div className="boss-form">
+            <div className="boss-form__row boss-form__row_position_last">
+              <div className="boss-form__control">
+                <p className="boss-form__label boss-form__label_type_icon-venue boss-form__label_type_icon-single"></p>
+                <div className="boss-form__select">
+                  <VenueDropdown
+                    venues={venues}
+                    selectedVenues={selectedVenues}
+                    clearable={false}
+                    onChange={(venueClientIds) => onChange({date, venueClientId: venueClientIds[0]})}
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="boss-page-dashboard__group">
+        <div className="boss-page-dashboard__controls-group">
+          <div className="boss-form">
+            <div className="boss-form__row boss-form__row_justify_space boss-form__row_position_last">
+              <div className="boss-form__field boss-form__field_role_control boss-form__field_layout_min boss-form__field_position_last">
+                <p className="boss-form__label boss-form__label_type_icon-date boss-form__label_type_icon-single"></p>
+                <div className="date-picker-input">
+                  <DatePicker
+                    withPortal="withPortal"
+                    calendarClassName="date-picker"
+                    showMonthDropdown
+                    showYearDropdown
+                    selected={moment(date)}
+                    onChange={(date) => onChange({date, venueClientId})}
+                    dateFormat="DD-MM-YYYY"
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="boss-page-dashboard__buttons-group boss-page-dashboard__buttons-group_position_last">
+          <a href={link} className="boss-button boss-button_role_calendar boss-page-dashboard__button">View current</a>
+        </div>
+      </div>
+    </DashboardWrapper>
+  )
+}
+
--- app/frontend/apps/hours-confirmation/containers/page-header.js
+++ app/frontend/apps/hours-confirmation/containers/page-header.js
@@ -3,62 +3,64 @@ import { connect } from "react-redux"
 import VenueDropdown from "~/components/venue-dropdown"
 import { appRoutes } from "~/lib/routes"
 import DateAndVenueSelector from "~/components/date-and-venue-selector"
+import DashboardCurrentWrapper from '../components/dashboard-current-wrapper';
+import DashboardDate from '../components/dashboard-date';
+import moment from 'moment';
 
 class PageHeader extends React.Component {
+
+    handleDateChange = ({date, venueClientId}) => {
+      var venue = this.props.venues[venueClientId];
+      location.href = appRoutes.hoursConfirmationDayPage({
+          venueId: venue.serverId,
+          date
+      })
+    }
+
     render(){
         var venueClientId = this.props.pageOptions.venue.clientId;
 
         var selector;
         var isDailyPage = this.props.pageOptions.date !== undefined;
         if (isDailyPage){
-            selector = <div className="">
-                <DateAndVenueSelector
-                    date={this.props.pageOptions.date}
-                    venueClientId={venueClientId}
-                    venues={this.props.venues}
-                    onChange={({date, venueClientId}) => {
-                        var venue = this.props.venues[venueClientId];
-                        location.href = appRoutes.hoursConfirmationDayPage({
-                            venueId: venue.serverId,
-                            date
-                        })
-                    }}
-                />
-            </div>
-        } else {
+            const venueName = this.props.venues[venueClientId].name;
+            const date = moment(this.props.pageOptions.date).format('DD MMMM YYYY');
+            const title = `Hours Confirmations for ${venueName} ${date}`;
+            selector = 
+              <DashboardDate
+                title={title}
+                date={this.props.pageOptions.date}
+                onChange={({date, venueClientId}) => this.handleDateChange({date, venueClientId})}
+                venueClientId={venueClientId}
+                venues={this.props.venues}
+              />
 
+        } else {
           selector = 
-          <div className="boss-page-main__dashboard">
-            <div className="boss-page-main__inner">
-              <div className="boss-page-dashboard boss-page-dashboard_updated">
-                <h1 className="boss-page-dashboard__title">{this.props.pageOptions.pageName}</h1>
-                <div className="boss-page-dashboard__group">
-                  <div className="boss-page-dashboard__controls-group">
-                    <div className="boss-form boss-form_page_hrc">
-                      <div className="boss-form__field">
-                        <div className="boss-form__select boss-form__select_size_small">
-                            <VenueDropdown
-                              selectedVenues={[venueClientId]}
-                              venues={this.props.venues}
-                              clearable={false}
-                              onChange={(venueIds) => {
-                                  var venue = this.props.venues[venueIds[0]]
-                                  location.href = appRoutes.hoursConfirmationCurrentPage({
-                                      venueId: venue.serverId
-                                  })
-                              }}
-                          />
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                  <div className="boss-page-dashboard__buttons-group boss-page-dashboard__buttons-group_position_last">
-                    <a href={`/hours_confirmation?date=${this.props.pageOptions.pageDateFormat.date}&venue_id=${this.props.pageOptions.venue.serverId}`} className="boss-button boss-button_role_calendar boss-page-dashboard__button">View by date </a>
+          <DashboardCurrentWrapper title={this.props.pageOptions.pageName}>
+            <div className="boss-page-dashboard__controls-group">
+              <div className="boss-form boss-form_page_hrc">
+                <div className="boss-form__field">
+                  <div className="boss-form__select boss-form__select_size_small">
+                      <VenueDropdown
+                        selectedVenues={[venueClientId]}
+                        venues={this.props.venues}
+                        clearable={false}
+                        onChange={(venueIds) => {
+                            var venue = this.props.venues[venueIds[0]]
+                            location.href = appRoutes.hoursConfirmationCurrentPage({
+                                venueId: venue.serverId
+                            })
+                        }}
+                    />
                   </div>
                 </div>
               </div>
             </div>
-          </div>
+            <div className="boss-page-dashboard__buttons-group boss-page-dashboard__buttons-group_position_last">
+              <a href={`/hours_confirmation?date=${this.props.pageOptions.pageDateFormat.date}&venue_id=${this.props.pageOptions.venue.serverId}`} className="boss-button boss-button_role_calendar boss-page-dashboard__button">View by date </a>
+            </div>
+          </DashboardCurrentWrapper>
         }
 
         return <div>
