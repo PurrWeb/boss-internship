9d2e14c245628eeed31a51706fa0471d85ada7a9
William Billington <william.billington@gmail.com>
2017-09-25 18:50:09 +0100

    Replace welcome message with weather widget

--- spec/feature/pages/home_page.rb
+++ spec/feature/pages/home_page.rb
@@ -6,28 +6,17 @@ module PageObject
       visit('/')
     end
 
-    page_action :ensure_welcome_text_displayed_for do |user|
-      expect(page_heading.text).to include("Welcome #{user.name.first_name}")
-    end
-
     def navigation
       @navigation ||= NavigationBar.new(self)
     end
 
     def assert_on_correct_page
-      expect(page_heading).to(
-        have_text(expected_page_heading_text),
-        "expected page heading to have test '#{expected_page_heading_text}' got '#{page_heading.text}'"
-      )
+      page_heading
     end
 
     private
     def page_heading
-      page.find('main h1')
-    end
-
-    def expected_page_heading_text
-      'Welcome'
+      page.find('.test-welcome-div')
     end
   end
 end
