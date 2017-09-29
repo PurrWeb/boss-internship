13a027e0a4e438ba661bce767d7a7a9c7e0a27a9
Igor Pugachev <ipugachev84@gmail.com>
2017-09-29 13:10:55 +0100

    [machines refloats] Add services

---
+++ app/serializers/api/v1/machines_refloats/machines_serializer.rb
@@ -0,0 +1,31 @@
+class Api::V1::MachinesRefloats::MachinesSerializer < ActiveModel::Serializer
+  attributes  :id,
+              :name,
+              :location,
+              :floatCents,
+              :refillX10p,
+              :cashInX10p,
+              :cashOutX10p,
+              :lastMachineRefloatId
+
+  def floatCents
+    object.float_cents
+  end
+
+  def refillX10p
+    object.initial_refill_x_10p
+  end
+
+  def cashInX10p
+    object.initial_cash_in_x_10p
+  end
+
+  def cashOutX10p
+    object.initial_cash_out_x_10p
+  end
+
+  def lastMachineRefloatId
+    lastRefloat = object.machines_refloats.last
+    lastRefloat.present? ? lastRefloat.id : nil
+  end
+end  
