f392309ee811ee1d27c13ce3c906c10a845d2891
William Billington <william.billington@gmail.com>
2017-10-09 18:44:30 +0100

    Add staff_member_start_date to holiday_in_tax_year_query

--- app/queries/holiday_in_tax_year_query.rb
+++ app/queries/holiday_in_tax_year_query.rb
@@ -1,13 +1,22 @@
 class HolidayInTaxYearQuery
-  def initialize(relation: Holiday.all, tax_year:)
+  def initialize(relation: Holiday.all, staff_member_start_date: nil, tax_year:)
     @relation = relation
     @tax_year = tax_year
+    @staff_member_start_date = staff_member_start_date
+  end
+
+  def start_date
+    if staff_member_start_date.present?
+      [staff_member_start_date, tax_year.start_date].max
+    else
+      tax_year.start_date
+    end
   end
 
   def all
     HolidayInRangeQuery.new(
       relation: relation,
-      start_date: tax_year.start_date,
+      start_date: start_date,
       end_date: tax_year.end_date
     ).all
   end
@@ -17,5 +26,5 @@ class HolidayInTaxYearQuery
   end
 
   private
-  attr_reader :relation, :tax_year
+  attr_reader :relation, :tax_year, :staff_member_start_date
 end
--- spec/queries/holiday_in_tax_year_query_spec.rb
+++ spec/queries/holiday_in_tax_year_query_spec.rb
@@ -5,6 +5,7 @@ RSpec.describe HolidayInTaxYearQuery do
 
   let(:now) { Time.zone.now }
   let(:tax_year_start_date) { tax_year.start_date }
+  let(:staff_member_start_date) { tax_year.start_date }
   let(:tax_year) { TaxYear.new(now.to_date) }
   let(:query) do
     HolidayInTaxYearQuery.new(
@@ -59,6 +60,53 @@ RSpec.describe HolidayInTaxYearQuery do
 
     specify 'holidays should be returned' do
       expect(result.to_a.map(&:id)).to eq(existing_holidays.map(&:id))
+    end
+  end
+
+  context 'when staff_member_start_date is supplied' do
+    let(:staff_member_start_date) { tax_year_start_date + 4.weeks }
+    let(:query) do
+      HolidayInTaxYearQuery.new(
+        tax_year: tax_year,
+        relation: relation,
+        staff_member_start_date: staff_member_start_date
+      )
+    end
+    let(:before_start_date_holidays) do
+      travel_to tax_year_start_date - 2.weeks do
+        date = tax_year_start_date + 1.day
+        2.times do
+          result << FactoryGirl.create(
+            :holiday,
+            start_date: date,
+            end_date: date
+          )
+          date = date + 1.day
+        end
+      end
+      result
+    end
+    let(:after_start_date_holidays) do
+      travel_to tax_year_start_date - 2.weeks do
+        date = staff_member_start_date
+        2.times do
+          result << FactoryGirl.create(
+            :holiday,
+            start_date: date,
+            end_date: date
+          )
+          date = date + 1.day
+        end
+      end
+      result
+    end
+    let(:existing_holidays) do
+      before_start_date_holidays
+      after_start_date_holidays
+    end
+
+    specify 'existing holidays after start date should be returned' do
+      expect(result.to_a.map(&:id)).to eq(after_start_date_holidays.map(&:id))
     end
   end
 end
