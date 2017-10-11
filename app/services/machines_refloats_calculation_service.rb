9e8493f260b33ade171b65b8b69043059cf65117
William Billington <william.billington@gmail.com>
2017-10-11 16:37:17 +0100

    Fix spec

--- app/services/machines_refloats_calculation_service.rb
+++ app/services/machines_refloats_calculation_service.rb
@@ -1,50 +1,33 @@
 class MachinesRefloatsCalculationService
   def initialize(machines_refloat:)
     @machines_refloat = machines_refloat
-    machine = @machines_refloat.machine
-    @machine_float_topup_cents = machine.initial_float_topup_cents
-    @machine_float_cents = machine.float_cents
+    @machine = machines_refloat.machine
+    machine_float_topup_cents = machine.initial_float_topup_cents
+    machine_float_cents = machine.float_cents
 
-    @last_machine_refloat = machine.venue.machines_refloats.find_by(id: machines_refloat.last_machine_refloat_id)
-    @last_float_topup_cents = @last_machine_refloat.present? ? @last_machine_refloat.float_topup_cents : @machine_float_topup_cents
-    @last_money_banked_cents = @last_machine_refloat.present? ? @last_machine_refloat.money_banked_cents : @machine_float_topup_cents
-    @last_calculated_float_topup_cents = @last_machine_refloat.present? ? @last_machine_refloat.calculated_float_topup_cents : @machine_float_cents
-    @last_calculated_money_banked_cents = @last_machine_refloat.present? ? @last_machine_refloat.calculated_money_banked_cents : 0
-    
-    @last_untoppedup_float_cents = @last_machine_refloat.present? ? @last_calculated_float_topup_cents - @last_float_topup_cents : @machine_float_cents - @machine_float_topup_cents 
-    @last_unbanked_cents = @last_calculated_money_banked_cents - @last_money_banked_cents
-    
-    @last_cash_in_cents = @last_machine_refloat.present? ? @last_machine_refloat.cash_in_x_10p * 10 : machine.initial_cash_in_x_10p * 10
-    @last_cash_out_cents = @last_machine_refloat.present? ? @last_machine_refloat.cash_out_x_10p * 10 : machine.initial_cash_out_x_10p * 10
-    @last_refill_cents = @last_machine_refloat.present? ? @last_machine_refloat.refill_x_10p * 10 : machine.initial_refill_x_10p * 10
-    @cash_in_x_10p_cents = @machines_refloat.cash_in_x_10p * 10
-    @cash_out_x_10p_cents = @machines_refloat.cash_out_x_10p * 10
-    @refill_x_10p_cents = @machines_refloat.refill_x_10p * 10
-    
-    @cash_in_x_10p_diff_cents = @cash_in_x_10p_cents - @last_cash_in_cents
-    @cash_out_x_10p_diff_cents = @cash_out_x_10p_cents - @last_cash_out_cents
-    @refill_x_10p_diff_cents = @refill_x_10p_cents - @last_refill_cents
+    @last_machine_refloat = MachinesRefloat.find_by(id: machines_refloat.last_machine_refloat_id)
+    if !last_machine_refloat.present?
+      @last_machine_refloat = machine.machines_refloats.order(:created_at).last
+    end
+    @last_float_topup_cents = last_machine_refloat.present? ? last_machine_refloat.float_topup_cents : machine_float_topup_cents
+    last_money_banked_cents = last_machine_refloat.present? ? last_machine_refloat.money_banked_cents : machine_float_topup_cents
+    last_calculated_float_topup_cents = last_machine_refloat.present? ? last_machine_refloat.calculated_float_topup_cents : machine_float_cents
+    last_calculated_money_banked_cents = last_machine_refloat.present? ? last_machine_refloat.calculated_money_banked_cents : 0
+
+    @last_untoppedup_float_cents = last_machine_refloat.present? ? last_calculated_float_topup_cents - last_float_topup_cents : 0
+    @last_unbanked_cents = last_calculated_money_banked_cents - last_money_banked_cents
+
+    last_cash_in_cents = last_machine_refloat.present? ? last_machine_refloat.cash_in_x_10p * 10 : machine.initial_cash_in_x_10p * 10
+    last_cash_out_cents = last_machine_refloat.present? ? last_machine_refloat.cash_out_x_10p * 10 : machine.initial_cash_out_x_10p * 10
+    last_refill_cents = last_machine_refloat.present? ? last_machine_refloat.refill_x_10p * 10 : machine.initial_refill_x_10p * 10
+    cash_in_x_10p_cents = machines_refloat.cash_in_x_10p * 10
+    cash_out_x_10p_cents = machines_refloat.cash_out_x_10p * 10
+    refill_x_10p_cents = machines_refloat.refill_x_10p * 10
+
+    @cash_in_x_10p_diff_cents = cash_in_x_10p_cents - last_cash_in_cents
+    @cash_out_x_10p_diff_cents = cash_out_x_10p_cents - last_cash_out_cents
+    @refill_x_10p_diff_cents = refill_x_10p_cents - last_refill_cents
   end
-  
-  attr_reader :machines_refloat,
-              :machine_float_topup_cents,
-              :machine_float_cents,
-              :last_machine_refloat,
-              :last_float_topup_cents,
-              :last_money_banked_cents,
-              :last_calculated_money_banked_cents,
-              :last_calculated_float_topup_cents,
-              :last_unbanked_cents,
-              :last_untoppedup_float_cents,
-              :last_cash_in_cents,
-              :last_cash_out_cents,
-              :last_refill_cents,
-              :cash_in_x_10p_cents,
-              :cash_out_x_10p_cents,
-              :refill_x_10p_cents,
-              :cash_in_x_10p_diff_cents,
-              :cash_out_x_10p_diff_cents,
-              :refill_x_10p_diff_cents
 
   def calculated_float_topup_cents
     cash_out_x_10p_diff_cents - (refill_x_10p_diff_cents - last_float_topup_cents) + last_untoppedup_float_cents
@@ -53,4 +36,16 @@ class MachinesRefloatsCalculationService
   def calculated_money_banked_cents
     cash_in_x_10p_diff_cents - (refill_x_10p_diff_cents - last_float_topup_cents) + last_unbanked_cents
   end
+
+  private
+  attr_reader \
+    :machines_refloat,
+    :machine,
+    :last_machine_refloat,
+    :cash_out_x_10p_diff_cents,
+    :cash_in_x_10p_diff_cents,
+    :refill_x_10p_diff_cents,
+    :last_untoppedup_float_cents,
+    :last_unbanked_cents,
+    :last_float_topup_cents
 end
--- spec/services/machines_refloats_calculation_spec.rb
+++ spec/services/machines_refloats_calculation_spec.rb
@@ -1,37 +1,174 @@
 require 'rails_helper'
 
 RSpec.describe 'Machines refloat calculation' do
-
   let(:user) { FactoryGirl.create(:user, venues: [venue]) }
   let(:venue) { FactoryGirl.create(:venue) }
-  let(:machine) { FactoryGirl.create(
-    :machine,
-    venue: venue,
-    created_by_user: user,
-    disabled_by: nil,
-    float_cents: 40000,
-    initial_refill_x_10p: 0,
-    initial_cash_in_x_10p: 0,
-    initial_cash_out_x_10p: 0
-  ) }
+  # £400
+  let(:machine_float_cents) { 40000 }
+  # £200
+  let(:initial_refill_reading) { 2000 }
+  # £400
+  let(:initial_cash_in_reading) { 4000 }
+  # £200
+  let(:initial_cash_out_reading) { 2000 }
 
-  let(:new_machines_refloat) { FactoryGirl.create(
-    :machines_refloat,
-    machine: machine,
-    user: user,
-  )}
-  
-  let(:service) do
-    MachinesRefloatsCalculationService.new(
-      machines_refloat: new_machines_refloat,
-    )
-  end
+  describe 'for machine with zero initial readings' do
+    let(:machine) do
+      FactoryGirl.create(
+        :machine,
+        venue: venue,
+        created_by_user: user,
+        float_cents: machine_float_cents,
+        initial_refill_x_10p: initial_refill_reading,
+        initial_cash_in_x_10p: initial_cash_in_reading,
+        initial_cash_out_x_10p: initial_cash_out_reading,
+        initial_float_topup_cents: 0
+      )
+    end
 
-  it 'calculated_float_topup_cents should be succeed' do
-    expect(service.calculated_float_topup_cents).to eq(new_machines_refloat.calculated_float_topup_cents)
-  end
+    let(:service) do
+      MachinesRefloatsCalculationService.new(
+        machines_refloat: new_machine_refloat,
+      )
+    end
 
-  it 'calculated_money_banked_cents should be succeed' do
-    expect(service.calculated_money_banked_cents).to eq(new_machines_refloat.calculated_money_banked_cents)
+    context 'initial refloat' do
+      context 'readings have not changed' do
+        let(:new_machine_refloat) do
+          FactoryGirl.build(
+            :machines_refloat,
+            refill_x_10p: initial_refill_reading,
+            cash_in_x_10p: initial_cash_in_reading,
+            cash_out_x_10p: initial_cash_out_reading,
+            float_topup_cents: 0,
+            money_banked_cents: 0,
+            machine: machine,
+            user: user
+          )
+        end
+
+        specify 'calculated_float_topup_cents should be 0' do
+          expect(service.calculated_float_topup_cents).to eq(0)
+        end
+
+        specify 'calculated_money_banked_cents should be 0' do
+          expect(service.calculated_money_banked_cents).to eq(0)
+        end
+      end
+
+      context 'readings have changed' do
+        # £300 (+£100)
+        let(:new_refill_reading) { initial_refill_reading + 1000 }
+        # £600 (+£200)
+        let(:new_cash_out_reading) { initial_cash_out_reading  + 2000}
+        # £1000 (+£600)
+        let(:new_cash_in_reading) { initial_cash_in_reading + 6000 }
+
+        let(:new_machine_refloat) do
+          FactoryGirl.build(
+            :machines_refloat,
+            refill_x_10p: new_refill_reading,
+            cash_in_x_10p: new_cash_in_reading,
+            cash_out_x_10p: new_cash_out_reading,
+            float_topup_cents: 0,
+            money_banked_cents: 0,
+            machine: machine.reload,
+            user: user
+          )
+        end
+
+        let(:expected_float_topup_pounds) { 100 }
+        let(:expected_money_banked_pounds) { 500 }
+
+        specify 'calculated_float_topup_cents should be £100' do
+          expect(service.calculated_float_topup_cents).to eq(expected_float_topup_pounds * 100)
+        end
+
+        specify 'calculated_money_banked_cents should be £500' do
+          expect(service.calculated_money_banked_cents).to eq(expected_money_banked_pounds * 100)
+        end
+      end
+    end
+
+    context 'after multiple reading where numbers didnt work' do
+      let(:refloat_1) do
+        FactoryGirl.create(
+          :machines_refloat,
+          # £400 (+£200)
+          refill_x_10p: initial_refill_reading + 2000,
+          # £1000 (+£600)
+          cash_in_x_10p: initial_cash_in_reading + 6000,
+          # £500 (+£300)
+          cash_out_x_10p: initial_cash_out_reading + 3000,
+          # £100
+          float_topup_cents: 10000,
+          # £400
+          money_banked_cents: 40000,
+          machine: machine,
+          user: user
+        )
+      end
+      let(:refloat_2_base_refill_reading) do
+        # if nothing changes the next refill reading will this, because the
+        # topup adds to it
+        refloat_1.refill_x_10p + (refloat_1.float_topup_cents / 10)
+      end
+      let(:refloat_2) do
+        FactoryGirl.create(
+          :machines_refloat,
+          # £700 (+£300)
+          refill_x_10p: refloat_2_base_refill_reading + 3000,
+          # £2000 (+£1000)
+          cash_in_x_10p: refloat_1.cash_in_x_10p + 10000,
+          # £1000 (+£500)
+          cash_out_x_10p: refloat_1.cash_out_x_10p + 5000,
+          # £100 (-100)
+          float_topup_cents: 10000,
+          float_topup_note: 'Not enough money',
+          # £500 (-£200)
+          money_banked_cents: 50000,
+          money_banked_note: 'Not enough money',
+          machine: machine,
+          user: user
+        )
+      end
+      let(:refloat_3_base_refill_reading) do
+        # if nothing changes the next refill reading will this, because the
+        # topup adds to it
+        refloat_2.refill_x_10p + (refloat_2.float_topup_cents / 10)
+      end
+      let(:create_refloats) do
+        refloat_1
+        refloat_2
+      end
+
+      let(:new_machine_refloat) do
+        FactoryGirl.build(
+          :machines_refloat,
+          refill_x_10p: refloat_3_base_refill_reading,
+          cash_in_x_10p: refloat_2.cash_in_x_10p,
+          cash_out_x_10p: refloat_2.cash_out_x_10p,
+          float_topup_cents: 0,
+          money_banked_cents: 0,
+          machine: machine.reload,
+          user: user
+        )
+      end
+
+      # untopped up from refloat 2
+      let(:expected_float_topup_pounds) { 100 }
+      # unbanked from refloat 2
+      let(:expected_money_banked_pounds) { 200 }
+
+      specify 'calculated_float_topup_cents should be untopped up from previous' do
+        create_refloats
+        expect(service.calculated_float_topup_cents).to eq(expected_float_topup_pounds * 100)
+      end
+
+      specify 'calculated_money_banked_cents should be unbanked from previous' do
+        create_refloats
+        expect(service.calculated_money_banked_cents).to eq(expected_money_banked_pounds * 100)
+      end
+    end
   end
 end
