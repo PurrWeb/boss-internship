1c2f9cb7fe7099f82c887ed378b212c0c3ba7348
William Billington <william.billington@gmail.com>
2017-10-16 22:01:29 +0100

    Add test for machineRefloatCalculation

--- app/frontend/lib/machine-refloat-calculation.js
+++ app/frontend/lib/machine-refloat-calculation.js
@@ -1,33 +1,34 @@
 import oFetch from "o-fetch"
 
-export default function(options){
+function machineRefloatCalculation(options){
   let selectedMachine = oFetch(options, 'selectedMachine');
   let lastMachineRefloat = oFetch(options, 'lastMachineRefloat');
   let refillX10pReading = oFetch(options, 'refillX10pReading');
   let cashInX10pReading = oFetch(options, 'cashInX10pReading');
   let cashOutX10pReading = oFetch(options, 'cashOutX10pReading');
 
-  const machineFloatCents = selectedMachine.floatCents;
-  const machineFloatTopupCents = selectedMachine.initialFloatTopupCents;
+  const machineFloatCents = oFetch(selectedMachine, 'floatCents');
+  const machineFloatTopupCents = oFetch(selectedMachine, 'initialFloatTopupCents');
+  const lastMachineRefloatPresent = !(lastMachineRefloat === null || lastMachineRefloat === undefined)
 
-  const lastRefillCents = lastMachineRefloat ? lastMachineRefloat.refillX10pReading * 10 : selectedMachine.refillX10pReading * 10;
-  const lastCashInCents = lastMachineRefloat ? lastMachineRefloat.cashInX10pReading * 10 : selectedMachine.cashInX10pReading * 10;
-  const lastCashOutCents = lastMachineRefloat ? lastMachineRefloat.cashOutX10pReading * 10 : selectedMachine.cashOutX10pReading * 10;
-  const lastCalculatedMoneyBankedCents = lastMachineRefloat ? lastMachineRefloat.calculatedMoneyBankedCentsCents : 0;
+  const lastRefillCents = lastMachineRefloatPresent ? oFetch(lastMachineRefloat, 'refillX10pReading') * 10 : oFetch(selectedMachine, 'refillX10pReading') * 10;
+  const lastCashInCents = lastMachineRefloatPresent ? oFetch(lastMachineRefloat, 'cashInX10pReading') * 10 : oFetch(selectedMachine, 'cashInX10pReading') * 10;
+  const lastCashOutCents = lastMachineRefloatPresent ? oFetch(lastMachineRefloat, 'cashOutX10pReading') * 10 : oFetch(selectedMachine, 'cashOutX10pReading') * 10;
+  const lastCalculatedMoneyBankedCents = lastMachineRefloat ? oFetch(lastMachineRefloat, 'calculatedMoneyBankedCents') : 0;
 
-  const lastCalculatedFloatTopupCents = lastMachineRefloat ? lastMachineRefloat.calculatedFloatTopupCents : machineFloatCents;
-  const lastMoneyBankedCents = lastMachineRefloat ? lastMachineRefloat.moneyBankedCents : machineFloatTopupCents;
+  const lastCalculatedFloatTopupCents = lastMachineRefloatPresent ? oFetch(lastMachineRefloat, 'calculatedFloatTopupCents') : machineFloatCents;
+  const lastMoneyBankedCents = lastMachineRefloatPresent ? oFetch(lastMachineRefloat, 'moneyBankedCents') : machineFloatTopupCents;
   const refillCents = refillX10pReading * 10;
   const cashInCents = cashInX10pReading * 10;
   const cashOutCents = cashOutX10pReading * 10;
-  const lastFloatTopupCents = lastMachineRefloat ? lastMachineRefloat.floatTopupCents : machineFloatTopupCents;
+  const lastFloatTopupCents = lastMachineRefloatPresent ? oFetch(lastMachineRefloat, 'floatTopupCents') : machineFloatTopupCents;
   const cashInDiffCents = cashInCents - lastCashInCents;
   const cashOutDiffCents = cashOutCents - lastCashOutCents;
   const refillDiffCents = refillCents - lastRefillCents;
   const lastUnbankedCents = lastCalculatedMoneyBankedCents - lastMoneyBankedCents;
   const lastUntoppedupFloatCents = lastMachineRefloat ?  lastCalculatedFloatTopupCents - lastFloatTopupCents : 0
 
-  const topupAndBankedCanEdit = (refillX10pReading || refillX10pReading === 0) && (cashInX10pReading || cashInX10pReading === 0) && (cashOutX10pReading || cashOutX10pReading === 0);
+  const topupAndBankedCanEdit = !!((refillX10pReading || refillX10pReading === 0) && (cashInX10pReading || cashInX10pReading === 0) && (cashOutX10pReading || cashOutX10pReading === 0));
   let calculatedFloatTopupCents = null;
   let calculatedMoneyBankedCents = null;
   if (topupAndBankedCanEdit) {
@@ -41,3 +42,5 @@ export default function(options){
     calculatedFloatTopupCents: calculatedFloatTopupCents
   };
 }
+
+export default machineRefloatCalculation;
