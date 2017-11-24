62fdf3e1a55ab27f9cffbb64485efd2da4e57907
William Billington <william.billington@gmail.com>
2017-11-24 17:59:26 +0000

    Update email copy

--- app/mailers/staff_member_verification_mailer.rb
+++ app/mailers/staff_member_verification_mailer.rb
@@ -1,6 +1,6 @@
 class StaffMemberVerificationMailer < ApplicationMailer
   def send_verification_email(staff_member:, token:)
-    mail(to: staff_member.email, subject: 'Test') do |format|
+    mail(to: staff_member.email, subject: 'Setup your JSM Bars Password') do |format|
       format.html do
         render locals: {
           staff_member: staff_member,
