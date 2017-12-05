badcff003c1b6ab0a9194e304577206245dd4969
Igor Pugachev <ipugachev84@gmail.com>
2017-12-05 07:50:34 +0000

    Hours confirmation loadMore when bottom reached

--- app/frontend/apps/hours-confirmation/containers/staff-day-list.js
+++ app/frontend/apps/hours-confirmation/containers/staff-day-list.js
@@ -33,6 +33,28 @@ class StaffDayList extends React.Component {
     return {currentSize, loadSize};
   }
 
+  handleScroll = () => {
+    const windowHeight = "innerHeight" in window ? window.innerHeight : document.documentElement.offsetHeight;
+    const body = document.body;
+    const html = document.documentElement;
+    const docHeight = Math.max(body.scrollHeight, body.offsetHeight, html.clientHeight,  html.scrollHeight, html.offsetHeight);
+    const windowBottom = windowHeight + window.pageYOffset;
+    if (windowBottom >= docHeight) {
+      const handleLoadMore = this.state.clockInDays.length !== this.state.filteredClockInDays.length;
+      if (handleLoadMore) {
+        this.loadMore();
+      }
+    }
+  }
+
+  componentDidMount() {
+    window.addEventListener("scroll", this.handleScroll);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener("scroll", this.handleScroll);
+  }
+
   loadMore = () => {
     this.setState(state => {
       const {currentSize, loadSize} = this.getLoadMoreSizes();
