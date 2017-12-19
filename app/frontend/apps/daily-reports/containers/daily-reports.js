630dafa6f3c4fbdca056541588a6ca694e140051
William Billington <william.billington@gmail.com>
2017-12-19 16:00:56 +0000

    Update pending message

--- app/frontend/apps/daily-reports/containers/daily-reports.js
+++ app/frontend/apps/daily-reports/containers/daily-reports.js
@@ -14,9 +14,10 @@ class DailyReports extends React.Component {
   }
 
   reportUpdatePendingMessage() {
-    return <div>
-        <p>A report is scheduled to be calculated for this date.</p>
-        <p>The updated data should be online within the hour.</p>
+    return <div className="boss-page-main__note boss-page-main__note_context_stack boss-page-main__note_role_alert">
+      <p className="boss-page-main__text-placeholder"></p>
+      <p className="boss-page-main__note-text boss-page-main__note-text_role_primary">A report is scheduled to be calculated for this date.</p>
+      <p className="boss-page-main__note-text">The updated data should be online within the hour.</p>
     </div>;
   }
 
