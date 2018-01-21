7ff7287b6ddbb305e8a71095089ff7d71182264c
William Billington <william.billington@gmail.com>
2018-01-21 22:20:39 +0000

    Render basic errors on reset password page

--- app/frontend/apps/staff-member-password-reset/reset-password-form.js
+++ app/frontend/apps/staff-member-password-reset/reset-password-form.js
@@ -13,10 +13,21 @@ import {
 } from '~/components/boss-form'
 
 class ResetPasswordForm extends React.Component {
+  renderBaseError(error){
+    return (
+      <div className="boss-modal-window__alert">
+        <div className="boss-alert boss-alert_role_area boss-alert_context_above">
+          <p className="boss-alert__text">{error}</p>
+        </div>
+      </div>
+    )
+  }
+
   render() {
     const submitting = oFetch(this.props, 'submitting');
     const actionDescription = oFetch(this.props, 'actionDescription');
     const showPassword = this.props.showPassword;
+    const error = this.props.error;
 
     return (
       <div className="boss-modal-window boss-modal-window_role_set">
@@ -26,6 +37,7 @@ class ResetPasswordForm extends React.Component {
           </h2>
         </div>
         <div className="boss-modal-window__content">
+          { error && this.renderBaseError(error) }
           <div className="boss-modal-window__form">
             <form onSubmit={this.props.handleSubmit} className="boss-form">
               <Field
