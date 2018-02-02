8d0062d6c007b8cb7f7d7e9a2a0fd12b65201484
Igor Pugachev <ipugachev84@gmail.com>
2018-02-02 11:31:20 +0000

    Add `last updated dateTime` to accessory request serializer

--- app/serializers/api/v1/staff_member_profile/accessory_request_serializer.rb
+++ app/serializers/api/v1/staff_member_profile/accessory_request_serializer.rb
@@ -2,13 +2,29 @@ class Api::V1::StaffMemberProfile::AccessoryRequestSerializer < ActiveModel::Ser
   attributes \
     :id,
     :createdAt,
+    :updatedAt,
     :status,
     :accessoryName,
     :size,
-    :hasRefundRequest
+    :hasRefundRequest,
+    :refundRequestStatus,
+    :timeline
+
+  def updatedAt
+    last_refund_state_change = if object.has_refund_request?
+      refund_request = object.accessory_refund_request
+      refund_request.current_state == "pending" \
+        ? refund_request.created_at \
+        : refund_request.last_transition.created_at
+    end
+    last_request_state_change = object.current_state == "pending" \
+      ? object.created_at \
+      : object.last_transition.created_at
+    [last_refund_state_change, last_request_state_change].compact.max.utc
+  end
 
   def createdAt
-    object.updated_at.utc
+    object.created_at.utc
   end
 
   def status
@@ -19,11 +35,15 @@ class Api::V1::StaffMemberProfile::AccessoryRequestSerializer < ActiveModel::Ser
     object.accessory.name
   end
 
-  def hasRefundRequest
-    object.accepted? && object.accessory_refund_request.present?
+  def timeline
+    AccessoryRequestTimeline.new(accessory_request: object).serialize
   end
 
-  def requestTimeLine
+  def hasRefundRequest
+    object.has_refund_request?
+  end
 
+  def refundRequestStatus
+    object.accessory_refund_request.current_state if object.has_refund_request?
   end
 end
