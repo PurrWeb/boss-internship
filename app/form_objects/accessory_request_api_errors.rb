31263fd22da4ab1e790f5dafb2a08abdff8b281c
Igor Pugachev <ipugachev84@gmail.com>
2018-02-06 14:56:00 +0000

    Update `AccessoryRequestApiErrors` service

--- app/form_objects/accessory_request_api_errors.rb
+++ app/form_objects/accessory_request_api_errors.rb
@@ -7,11 +7,8 @@ class AccessoryRequestApiErrors
   def errors
     result = {}
     result[:base] = accessory_request.errors[:base] if accessory_request.errors[:base].present?
-    result[:priceCents] = accessory_request.errors[:price_cents] if accessory_request.errors[:price_cents].present?
     result[:size] = accessory_request.errors[:size] if accessory_request.errors[:size].present?
-    result[:accessoryType] = accessory_request.errors[:accessory_type] if accessory_request.errors[:accessory_type].present?
     result[:accessoryId] = accessory_request.errors[:accessory] if accessory_request.errors[:accessory].present?
-    result[:staffMember] = accessory_request.errors[:staff_member] if accessory_request.errors[:staff_member].present?
 
     result
   end
