a916c8609180e404699bebf680ea20be7f82c8d8
William Billington <william.billington@gmail.com>
2018-02-06 14:18:46 +0000

    Add access level class for effective checks

---
+++ app/models/access_level.rb
@@ -0,0 +1,49 @@
+class AccessLevel
+  DEV_ACCESS_LEVEL = 'dev'
+  ADMIN_ACCESS_LEVEL = 'admin'
+  AREA_MANAGER_ACCESS_LEVEL = 'area_manager'
+  MANAGER_ACCESS_LEVEL = 'manager'
+  RESTRICTED_ACCESS_LEVEL = 'restricted'
+  LEVEL_DATA = {
+    DEV_ACCESS_LEVEL => 4,
+    ADMIN_ACCESS_LEVEL => 3,
+    AREA_MANAGER_ACCESS_LEVEL => 2,
+    MANAGER_ACCESS_LEVEL => 1,
+    RESTRICTED_ACCESS_LEVEL => 0
+  }
+
+  def initialize(access_level)
+    raise "Unsupported access level #{access_level} encounted" unless LEVEL_DATA.keys.include?(access_level)
+    @access_level = access_level
+  end
+  attr_reader :access_level
+
+  def is_effectively?(target_access_level)
+    value >= target_access_level.value
+  end
+
+  def value
+    LEVEL_DATA.fetch(access_level) do
+      raise "unsupported access level #{access_level} encountered"
+    end
+  end
+
+  def self.for_user_role(role)
+    access_level = case role
+    when User::DEV_ROLE
+      DEV_ACCESS_LEVEL
+    when User::ADMIN_ROLE
+      ADMIN_ACCESS_LEVEL
+    when User::OPS_MANAGER_ROLE
+      AREA_MANAGER_ACCESS_LEVEL
+    when User::MANAGER_ROLE
+      MANAGER_ACCESS_LEVEL
+    when User::MAINTENANCE_ROLE, User::MARKETING_ROLE, User::SECURITY_MANAGER_ROLE
+      RESTRICTED_ACCESS_LEVEL
+    else
+      raise "Unsupported role '#{role}' encountered"
+    end
+
+    AccessLevel.new(access_level)
+  end
+end
