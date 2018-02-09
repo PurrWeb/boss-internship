bb31ab68e89bda031e7961d118dce69533ede342
William Billington <william.billington@gmail.com>
2018-02-09 11:09:14 +0000

    Give area managers access level all venue access in permissions

--- app/models/user_ability.rb
+++ app/models/user_ability.rb
@@ -379,7 +379,7 @@ class UserAbility
 
   def can_manage_venue?(user, venue)
     user.ops_manager? ||
-    user.has_effective_access_level?(AccessLevel.admin_access_level) ||
+    user.has_effective_access_level?(AccessLevel.area_manager_access_level) ||
     (
       user.has_effective_access_level?(AccessLevel.manager_access_level) &&
         user.venues.include?(venue)
