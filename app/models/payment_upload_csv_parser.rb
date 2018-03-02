efd18a8f7ab5b9a964b6afe021c77e9d21b522ce
William Billington <william.billington@gmail.com>
2018-03-02 15:16:28 +0000

    PaymentsUploadCSVParser

--- app/models/payment_upload_csv_parser.rb
+++ app/models/payment_upload_csv_parser.rb
@@ -9,45 +9,89 @@ class PaymentUploadCSVParser
   NI_HEADER = 'Employees.NINumber'
   DOB_HEADER = 'Employees.DateOfBirth'
   NET_PAY_HEADER = 'CurrentPay.RndNetPay'
+  HEADERS = [
+    PROCCESS_DATE_HEADER,
+    VENUE_NAME_HEADER,
+    DEPARTMENT_NAME_HEADER,
+    FIRST_INITIAL_HEADER,
+    SURNAME_HEADER,
+    NI_HEADER,
+    DOB_HEADER,
+    NET_PAY_HEADER
+  ]
   UPLOAD_DATE_FORMAT = '%d/%m/%Y'
 
   DATE_INVALID_PARSE_ERROR_MESSAGE = 'invalid date'
   DATE_FORMAT_INVALID_ERROR_MESSAGE = 'date format invalid'
   MUST_BE_PRESENT_ERROR_MESSAGE = 'must be present'
 
-  def initialize(csvData)
-    @csvData = csvData
+  def initialize(csv_data:, requester:)
+    @csv_data = csv_data
+    @requester = requester
   end
-  attr_reader :csvData
+  attr_reader :csv_data, :requester
 
   def parse
     valid_payments = []
     invalid_payments = []
-    errors = initial_errors
+    title_row_errors = {}
+    header_row_errors = {}
 
     row_index = 0
-    CSV.parse(csvData, headers: headers, return_headers: false, skip_blanks: true) do |row|
-      if (row_index == 0 || row_index == 1)
-        #TODO: validate
+    CSV.parse(csv_data, headers: HEADERS, return_headers: false, skip_blanks: true) do |row|
+      if (row_index == 0)
+        validate_title_row(row: row, errors: title_row_errors)
+        break unless title_row_errors.empty?
+      elsif (row_index == 1)
+        validate_header_row(row: row, errors: header_row_errors)
+        break unless header_row_errors.empty?
       else
         payment_data = processRowData(row)
+        if payment_data.fetch(:errors).empty?
+          valid_payments << payment_data
+        else
+          invalid_payments << payment_data
+        end
       end
       row_index = row_index + 1
     end
 
     return {
+      title_row_errors: title_row_errors,
+      header_row_errors: header_row_errors,
       valid_payments: valid_payments,
-      invalid_payments: invalid_payments,
-      errors: errors
+      invalid_payments: invalid_payments
     }
   end
 
   private
+  def validate_title_row(row:, errors:)
+    title_column = row[0]
+    if !(/Title:/.match(title_column).present?)
+      errors[:title] ||= []
+      errors[:title] << "Is invalid should have format 'Title: <Title>'"
+    end
+
+    file_name_column = row[1]
+    if !(/FileName:/.match(file_name_column).present?)
+      errors[:filename] ||= []
+      errors[:filename] << "Is invalid should have format 'FileName: <Filename>'"
+    end
+  end
+
+  def validate_header_row(row:, errors:)
+    HEADERS.each_with_index do |header, index|
+      column_value = row[index]
+      if column_value != header
+        errors[header] = 'Was missing or in unexpected position.'
+      end
+    end
+  end
+
   def processRowData(row)
     payment_data = {
       raw_data: nil,
       normalised_data: nil,
-      payment: nil,
       staff_member: nil,
       venue: nil,
       errors: {}
@@ -60,49 +104,57 @@ class PaymentUploadCSVParser
     all_fields_present = true
     raw_data[:process_date_raw] = row[PROCCESS_DATE_HEADER]
     if !raw_data[:process_date_raw].present?
-      errors.fetch(PROCCESS_DATE_HEADER) << MUST_BE_PRESENT_ERROR_MESSAGE
+      payment_data.fetch(:errors)[PROCCESS_DATE_HEADER] ||= []
+      payment_data.fetch(:errors).fetch(PROCCESS_DATE_HEADER) << MUST_BE_PRESENT_ERROR_MESSAGE
       all_fields_present = false
     end
 
     raw_data[:venue_name_raw] = row[VENUE_NAME_HEADER]
     if !raw_data[:venue_name_raw].present?
-      errors.fetch(VENUE_NAME_HEADER) << MUST_BE_PRESENT_ERROR_MESSAGE
+      payment_data.fetch(:errors)[VENUE_NAME_HEADER] ||= []
+      payment_data.fetch(:errors).fetch(VENUE_NAME_HEADER) << MUST_BE_PRESENT_ERROR_MESSAGE
       all_fields_present = false
     end
 
     raw_data[:department_name_raw] = row[DEPARTMENT_NAME_HEADER]
     if !raw_data[:department_name_raw].present?
-      errors.fetch(DEPARTMENT_NAME_HEADER) << MUST_BE_PRESENT_ERROR_MESSAGE
+      payment_data.fetch(:errors)[DEPARTMENT_NAME_HEADER] ||= []
+      payment_data.fetch(:errors).fetch(DEPARTMENT_NAME_HEADER) << MUST_BE_PRESENT_ERROR_MESSAGE
       all_fields_present = false
     end
 
     raw_data[:first_initial_raw]= row[FIRST_INITIAL_HEADER]
     if !raw_data[:first_initial_raw].present?
-      errors.fetch(FIRST_INITIAL_HEADER) << MUST_BE_PRESENT_ERROR_MESSAGE
+      payment_data.fetch(:errors)[FIRST_INITIAL_HEADER] ||= []
+      payment_data.fetch(:errors).fetch(FIRST_INITIAL_HEADER) << MUST_BE_PRESENT_ERROR_MESSAGE
       all_fields_present = false
     end
 
     raw_data[:surname_raw] = row[SURNAME_HEADER]
     if !raw_data[:surname_raw].present?
-      errors.fetch(SURNAME_HEADER) << MUST_BE_PRESENT_ERROR_MESSAGE
+      payment_data.fetch(:errors)[SURNAME_HEADER] ||= []
+      payment_data.fetch(:errors).fetch(SURNAME_HEADER) << MUST_BE_PRESENT_ERROR_MESSAGE
       all_fields_present = false
     end
 
     raw_data[:ni_number_raw] = row[NI_HEADER]
     if !raw_data[:ni_number_raw].present?
-      errors.fetch(NI_HEADER) << MUST_BE_PRESENT_ERROR_MESSAGE
+      payment_data.fetch(:errors)[NI_HEADER] ||= []
+      payment_data.fetch(:errors).fetch(NI_HEADER) << MUST_BE_PRESENT_ERROR_MESSAGE
       all_fields_present = false
     end
 
     raw_data[:date_of_birth_raw] = row[DOB_HEADER]
     if !raw_data[:date_of_birth_raw].present?
-      errors.fetch(DOB_HEADER) << MUST_BE_PRESENT_ERROR_MESSAGE
+      payment_data.fetch(:errors)[DOB_HEADER] ||= []
+      payment_data.fetch(:errors).fetch(DOB_HEADER) << MUST_BE_PRESENT_ERROR_MESSAGE
       all_fields_present = false
     end
 
     raw_data[:net_pay_raw] = row[NET_PAY_HEADER]
     if !raw_data[:net_pay_raw].present?
-      errors.fetch(NET_PAY_HEADER) << MUST_BE_PRESENT_ERROR_MESSAGE
+      payment_data.fetch(:errors)[NET_PAY_HEADER] ||= []
+      payment_data.fetch(:errors).fetch(NET_PAY_HEADER) << MUST_BE_PRESENT_ERROR_MESSAGE
       all_fields_present = false
     end
 
@@ -123,7 +175,9 @@ class PaymentUploadCSVParser
       )
     rescue ArgumentError => e
       raise unless e.message == DATE_INVALID_PARSE_ERROR_MESSAGE
-      errors[PROCCESS_DATE_HEADER] << DATE_FORMAT_INVALID_ERROR_MESSAGE
+
+      payment_data.fetch(:errors)[PROCCESS_DATE_HEADER] ||= []
+      payment_data.fetch(:errors).fetch(PROCCESS_DATE_HEADER) << DATE_FORMAT_INVALID_ERROR_MESSAGE
       all_data_valid = false
     end
     normalised_data[:first_initial] = raw_data.fetch(:first_initial_raw).strip
@@ -148,13 +202,14 @@ class PaymentUploadCSVParser
       )
     rescue ArgumentError => e
       raise unless e.message == DATE_INVALID_PARSE_ERROR_MESSAGE
-      errors[DOB_HEADER] << DATE_FORMAT_INVALID_ERROR_MESSAGE
+      payment_data.fetch(:errors)[DOB_HEADER] ||= []
+      payment_data.fetch(:errors).fetch(DOB_HEADER) << DATE_FORMAT_INVALID_ERROR_MESSAGE
       all_data_valid = false
     end
-    # e.g. 0 236.72 23.2
-    normalised_data[:net_pay_cents] = Float(raw_data.fetch(:net_pay_raw).strip)
-    if normalised_data[:net_pay_cents].present?
-      errors.fetch(NET_PAY_HEADER) << "format invalid"
+    normalised_data[:net_pay_cents] = (Float(raw_data.fetch(:net_pay_raw).strip) * 100).to_i
+    if !normalised_data[:net_pay_cents].present?
+      payment_data.fetch(:errors)[NET_PAY_HEADER] ||= []
+      payment_data.fetch(:errors).fetch(NET_PAY_HEADER) << "format invalid"
       all_data_valid = false
     end
 
@@ -168,48 +223,22 @@ class PaymentUploadCSVParser
     payment_data[:venue] = venue
     if !venue.present?
       lookups_successful = false
-      errors.fetch(VENUE_NAME_HEADER) << ['does not match venue in system']
+      payment_data.fetch(:errors)[VENUE_NAME_HEADER] ||= []
+      payment_data.fetch(:errors).fetch(VENUE_NAME_HEADER) << ['does not match venue in system']
     end
 
     staff_member = StaffMember.find_by(
-      national_insurance_number: national_insurance_number,
-      date_of_birth: normalised_data.fetch(date_of_birth),
+      national_insurance_number: normalised_data.fetch(:ni_number),
+      date_of_birth: normalised_data.fetch(:date_of_birth),
       master_venue: venue
     )
     payment_data[:staff_member] = staff_member
     if !staff_member.present?
-      errors.fetch(:base) << 'no matching staff member found'
+      payment_data.fetch(:errors)[:base] ||= []
+      payment_data.fetch(:errors).fetch(:base) << 'no matching staff member found'
       lookups_successful = false
     end
-    return payment_data unless lookups_successful
 
-    # Create Payments params
-    #############
-  end
-
-  def headers
-    [
-      PROCCESS_DATE_HEADER,
-      VENUE_NAME_HEADER,
-      DEPARTMENT_NAME_HEADER,
-      FIRST_INITIAL_HEADER,
-      SURNAME_HEADER,
-      NI_HEADER,
-      DOB_HEADER,
-      NET_PAY_HEADER
-    ]
-  end
-
-  def initial_errors
-    {
-      PROCCESS_DATE_HEADER => [],
-      VENUE_NAME_HEADER => [],
-      DEPARTMENT_NAME_HEADER => [],
-      FIRST_INITIAL_HEADER => [],
-      SURNAME_HEADER => [],
-      NI_HEADER => [],
-      DOB_HEADER => [],
-      NET_PAY_HEADER => [],
-    }
+    payment_data
   end
 end
--- spec/models/payment_upload_csv_parser_spec.rb
+++ spec/models/payment_upload_csv_parser_spec.rb
@@ -1,28 +1,186 @@
 require 'rails_helper'
-require_relative '../support/test_data_helper'
 
 describe PaymentUploadCSVParser do
+  let(:venue) { FactoryGirl.create(:venue, name: 'blue') }
   let(:valid_title_row) { ['Title: Departmental Analysis Gross To Net Portrait', "FileName: EDIT 1.REPORT", nil, nil, nil, nil, nil, nil] }
   let(:valid_header_row) { ['DateParameters.ProcessDate','CompanyDetails.Name','Employees.DepartmentName','Employees.Initials','Employees.Surname','Employees.NINumber','Employees.DateOfBirth','CurrentPay.RndNetPay'] }
-  let(:valid_payment_row) { ['28/01/2018','soho','Manager','P','Coulter','JS031127D','04/04/1992','350.69'] }
-
-  it 'should parse' do
-    rowData = [
-      valid_title_row,
-      valid_header_row,
-      valid_payment_row
+  let(:process_date) { Date.new(2018, 2, 20)}
+  let(:department_name) { 'Manager' }
+  let(:venue_name) { venue.name }
+  let(:ni_number ) { 'JS031127D' }
+  let(:first_initial) { 'P' }
+  let(:surname) { 'Coulter' }
+  let(:date_of_birth) { Time.current.to_date - 18.years }
+  let(:net_pay_cents) { 35069 }
+  let(:net_pay) { (Float(net_pay_cents) / 100.0).to_s }
+  let(:raw_process_date) do
+    process_date.strftime(PaymentUploadCSVParser::UPLOAD_DATE_FORMAT)
+  end
+  let(:raw_department_name) { department_name + '   ' }
+  let(:raw_venue_name) { venue_name + '    ' }
+  let(:raw_ni_number ) { '    ' + ni_number }
+  let(:raw_first_initial) { first_initial + '    '}
+  let(:raw_surname) { surname + '   ' }
+  let(:raw_net_pay) { net_pay + '  ' }
+  let(:raw_date_of_birth) do
+    date_of_birth.strftime(PaymentUploadCSVParser::UPLOAD_DATE_FORMAT) + '   '
+  end
+  let(:valid_payment_row) do
+    [
+      raw_process_date,
+      raw_venue_name,
+      raw_department_name,
+      raw_first_initial,
+      raw_surname,
+      raw_ni_number,
+      raw_date_of_birth,
+      raw_net_pay
     ]
-
-    csvData = CSV.generate do |csv|
-      rowData.each do |rowDatum|
+  end
+  let(:requester) { FactoryGirl.create(:user) }
+  let(:csv_data) do
+    CSV.generate do |csv|
+      row_data.each do |rowDatum|
         csv << rowDatum
       end
     end
+  end
+  let(:parser) { PaymentUploadCSVParser.new(csv_data: csv_data, requester: requester) }
 
-    parser = PaymentUploadCSVParser.new(csvData)
+  context 'Matching staff Member exists' do
+    let(:staff_member) do
+      FactoryGirl.create(
+        :staff_member,
+        national_insurance_number: raw_ni_number,
+        date_of_birth: date_of_birth,
+        master_venue: venue
+      )
+    end
 
-    result = parser.parse
-    expect(result.fetch(:invalid_payments).count).to eq(0)
-    expect(result.fetch(:valid_payments).count).to eq(1)
+    before do
+      staff_member
+    end
+
+    context 'valid data is supplied ' do
+      let(:row_data) do
+        [valid_title_row, valid_header_row, valid_payment_row]
+      end
+
+      it 'should create a valid payment' do
+        result = parser.parse
+        expect(result.fetch(:invalid_payments).count).to eq(0)
+        expect(result.fetch(:valid_payments).count).to eq(1)
+      end
+
+      describe 'valid_payment' do
+        it 'should contain raw data' do
+          valid_payment = parser.parse.fetch(:valid_payments).first
+          raw_data = valid_payment.fetch(:raw_data)
+
+          expect(raw_data.fetch(:process_date_raw)).to eq(raw_process_date)
+          expect(raw_data.fetch(:venue_name_raw)).to eq(raw_venue_name)
+          expect(raw_data.fetch(:department_name_raw)).to eq(raw_department_name)
+          expect(raw_data.fetch(:first_initial_raw)).to eq(raw_first_initial)
+          expect(raw_data.fetch(:surname_raw)).to eq(raw_surname)
+          expect(raw_data.fetch(:ni_number_raw)).to eq(raw_ni_number)
+          expect(raw_data.fetch(:date_of_birth_raw)).to eq(raw_date_of_birth)
+          expect(raw_data.fetch(:net_pay_raw)).to eq(raw_net_pay)
+        end
+
+        it 'should contain normalised data' do
+          valid_payment = parser.parse.fetch(:valid_payments).first
+          normalised_data = valid_payment.fetch(:normalised_data)
+
+          expect(normalised_data.fetch(:process_date)).to eq(process_date)
+          expect(normalised_data.fetch(:venue_name)).to eq(venue_name)
+          expect(normalised_data.fetch(:department_name)).to eq(department_name)
+          expect(normalised_data.fetch(:first_initial)).to eq(first_initial)
+          expect(normalised_data.fetch(:surname)).to eq(surname)
+          expect(normalised_data.fetch(:ni_number)).to eq(ni_number)
+          expect(normalised_data.fetch(:date_of_birth)).to eq(date_of_birth)
+          expect(normalised_data.fetch(:net_pay_cents)).to eq(net_pay_cents)
+        end
+
+        it 'should contain venue' do
+          valid_payment = parser.parse.fetch(:valid_payments).first
+          returned_venue = valid_payment.fetch(:venue)
+          expect(returned_venue).to eq(venue)
+        end
+
+        it 'should contain staff_member' do
+          valid_payment = parser.parse.fetch(:valid_payments).first
+          returned_staff_member = valid_payment.fetch(:staff_member)
+          expect(returned_staff_member).to eq(staff_member)
+        end
+
+        it 'should have no errors' do
+          valid_payment = parser.parse.fetch(:valid_payments).first
+          expect(valid_payment.fetch(:errors)).to eq({})
+        end
+      end
+
+      describe 'title row is invalid' do
+        let(:row_data) do
+          [ invalid_title_row, valid_header_row, valid_payment_row ]
+        end
+
+        context 'title row has invalid Title field' do
+          let(:invalid_title_row) { ['Tite: Departmental Analysis Gross To Net Portrait', "FileName: EDIT 1.REPORT", nil, nil, nil, nil, nil, nil] }
+          it 'should not process any payments' do
+            result = parser.parse
+            expect(result.fetch(:invalid_payments).count).to eq(0)
+            expect(result.fetch(:valid_payments).count).to eq(0)
+          end
+
+          it 'should return header error' do
+            title_row_errors = parser.parse.fetch(:title_row_errors)
+            expect(title_row_errors).to eq(
+              title: ["Is invalid should have format 'Title: <Title>'"]
+            )
+          end
+        end
+
+        context 'title row has invalid Filename field' do
+          let(:invalid_title_row) { ['Title: Departmental Analysis Gross To Net Portrait', "Fileame: EDIT 1.REPORT", nil, nil, nil, nil, nil, nil] }
+          it 'should not process any payments' do
+            result = parser.parse
+            expect(result.fetch(:invalid_payments).count).to eq(0)
+            expect(result.fetch(:valid_payments).count).to eq(0)
+          end
+
+          it 'should return error' do
+            title_row_errors = parser.parse.fetch(:title_row_errors)
+            expect(title_row_errors).to eq({
+              filename: ["Is invalid should have format 'FileName: <Filename>'"]
+            })
+          end
+        end
+      end
+
+      describe 'header row is invalid' do
+        let(:row_data) do
+          [ valid_title_row, invalid_header_row, valid_payment_row ]
+        end
+
+        PaymentUploadCSVParser::HEADERS.each do |header|
+          context "header row has invalid #{header} header" do
+            let(:invalid_header_row) do
+              row = PaymentUploadCSVParser::HEADERS.clone
+              row[row.index(header)] = nil
+              row
+            end
+
+            it 'should return error for the header' do
+              result = parser.parse
+              expect(
+                result.fetch(:header_row_errors)
+              ).to eq({
+                header => "Was missing or in unexpected position."
+              })
+            end
+          end
+        end
+      end
+    end
   end
 end
