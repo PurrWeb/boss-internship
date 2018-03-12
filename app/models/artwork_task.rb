4e1b6361ede06ffbc48d3f74a0ffe557d47bdf6e
William Billington <william.billington@gmail.com>
2018-03-12 14:20:40 +0000

    Validate artwork values on backend

--- app/models/artwork_task.rb
+++ app/models/artwork_task.rb
@@ -1,10 +1,24 @@
 class ArtworkTask < MarketingTask
-  # Constants
-  OTHER = 'other'.freeze
+  A1_SIZE = 'a1'
+  A2_SIZE = 'a2'
+  A3_SIZE = 'a3'
+  A4_SIZE = 'a4'
+  A5_SIZE = 'a5'
+  A6_SIZE = 'a6'
+  OTHER_SIZE = 'other'
+  SIZES = [
+    A1_SIZE,
+    A2_SIZE,
+    A3_SIZE,
+    A4_SIZE,
+    A5_SIZE,
+    A6_SIZE,
+    OTHER_SIZE
+  ]
 
   # Validations
-  validates :size, presence: true
-  validates :height_cm, :width_cm, presence: true, if: Proc.new { |a| a.size == OTHER }
+  validates :size, inclusion: { in: SIZES, message: 'is required' }
+  validates :height_cm, :width_cm, presence: true, if: Proc.new { |a| a.size == OTHER_SIZE }
   validates :quantity, presence: true, if: Proc.new { |m| m.print? }
 
   # Callbacks
@@ -13,7 +27,7 @@ class ArtworkTask < MarketingTask
   private
 
   def clear_size_dimensions
-    return if size == OTHER
+    return if size == OTHER_SIZE
 
     self.height_cm = nil
     self.width_cm = nil
