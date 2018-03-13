2c20d5b5c83777befe29d9125f8325049711d82a
William Billington <william.billington@gmail.com>
2018-03-13 16:27:43 +0000

    Add MobileApp model and tables

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180228113905) do
+ActiveRecord::Schema.define(version: 20180313162045) do
 
   create_table "accessories", force: :cascade do |t|
     t.integer  "venue_id",         limit: 4
@@ -709,6 +709,15 @@ ActiveRecord::Schema.define(version: 20180228113905) do
   add_index "marketing_tasks", ["assigned_to_user_id"], name: "index_marketing_tasks_on_assigned_to_user_id", using: :btree
   add_index "marketing_tasks", ["completed_by_user_id"], name: "index_marketing_tasks_on_completed_by_user_id", using: :btree
   add_index "marketing_tasks", ["venue_id"], name: "index_marketing_tasks_on_venue_id", using: :btree
+
+  create_table "mobile_apps", force: :cascade do |t|
+    t.string   "name",             limit: 255, null: false
+    t.string   "ios_download_url", limit: 255
+    t.string   "google_play_url",  limit: 255
+    t.datetime "disabled_at"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "names", force: :cascade do |t|
     t.string   "first_name", limit: 255, null: false
