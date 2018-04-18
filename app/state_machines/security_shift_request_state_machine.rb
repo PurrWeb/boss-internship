1eb0182fbc253ca59f194cbcaedb2698da1a96d5
Igor Pugachev <ipugachev84@gmail.com>
2018-04-18 08:38:09 +0100

    Add security shift request state machine

---
+++ app/state_machines/security_shift_request_state_machine.rb
@@ -0,0 +1,21 @@
+class SecurityShiftRequestStateMachine
+  include Statesman::Machine
+
+  state :pending, initial: true
+  state :accepted
+  state :rejected
+  state :assigned
+
+  transition from: :pending, to: [:accepted, :rejected]
+  transition from: :rejected, to: [:pending]
+  transition from: :accepted, to: [:pending, :assigned]
+
+  after_transition(from: :rejected, to: :pending) do |model, transition|
+    model.reject_reason = nil
+    model.save
+  end
+
+  guard_transition do |model, transition|
+    model.validate
+  end
+end
