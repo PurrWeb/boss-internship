9c8e571c73582160931fabb48acd061914c5a5f9
Igor Pugachev <ipugachev84@gmail.com>
2018-04-19 11:12:06 +0100

    Add security shift requests serializers

---
+++ app/serializers/api/v1/security_shift_requests/rota_shift_serializer.rb
@@ -0,0 +1,24 @@
+class Api::V1::SecurityShiftRequests::RotaShiftSerializer < ActiveModel::Serializer
+  attributes \
+    :id,
+    :rotaId,
+    :startsAt,
+    :endsAt,
+    :staffMemberId
+
+  def rotaId
+    object.rota_id
+  end
+
+  def startsAt
+    object.starts_at.utc.iso8601
+  end
+
+  def endsAt
+    object.ends_at.utc.iso8601
+  end
+
+  def staffMemberId
+    object.staff_member.id
+  end
+end
---
+++ app/serializers/api/v1/security_shift_requests/staff_member_serializer.rb
@@ -0,0 +1,25 @@
+class Api::V1::SecurityShiftRequests::StaffMemberSerializer < ActiveModel::Serializer
+
+  attributes \
+    :id,
+    :avatarUrl,
+    :firstName,
+    :surname,
+    :staffTypeId
+
+  def avatarUrl
+    object.avatar_url
+  end
+
+  def firstName
+    object.name.first_name
+  end
+
+  def surname
+    object.name.surname
+  end
+
+  def staffTypeId
+    object.staff_type.id
+  end
+end
