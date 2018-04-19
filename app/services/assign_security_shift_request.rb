9e8c4c8494b5d9716029406b09f6488d61ebff37
Igor Pugachev <ipugachev84@gmail.com>
2018-04-19 11:13:36 +0100

    Fix validation check in assign security shift request service

--- app/services/assign_security_shift_request.rb
+++ app/services/assign_security_shift_request.rb
@@ -12,28 +12,30 @@ class AssignSecurityShiftRequest
   end
 
   def call
-    success = false
     frontend_updates = FrontendUpdates.new
+    success = false
 
     ActiveRecord::Base.transaction do
-      success = security_shift_request.transition_to(:assigned)
-      create_rota_shift_result = CreateRotaShift.new(
+      security_shift_request.transition_to(:assigned)
+      created_rota_shift_result = CreateRotaShift.new(
         creator: requester,
         rota_date: RotaShiftDate.to_rota_date(security_shift_request.starts_at),
         venue: security_shift_request.venue,
         rota_shift_params: {
           starts_at: security_shift_request.starts_at,
           ends_at: security_shift_request.ends_at,
+          shift_type: 'normal',
           staff_member: staff_member,
         },
         frontend_updates: frontend_updates,
         authorization_proc: lambda do |rota_shift|
         end
       ).call
-      security_shift_request.created_shift = create_rota_shift_result.rota_shift
+      security_shift_request.created_shift = created_rota_shift_result.rota_shift
       security_shift_request.save
+      success = security_shift_request.valid?
 
-      raise ActiveRecord::Rollback unless success && create_rota_shift_result.success?
+      raise ActiveRecord::Rollback unless success
     end
     Result.new(success, security_shift_request)
   end
