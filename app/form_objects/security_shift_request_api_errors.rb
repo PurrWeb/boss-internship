0e54ba67760c16938338072e2dbf05571710f094
Igor Pugachev <ipugachev84@gmail.com>
2018-04-19 11:09:26 +0100

    Add rejectReason api error

--- app/form_objects/security_shift_request_api_errors.rb
+++ app/form_objects/security_shift_request_api_errors.rb
@@ -12,6 +12,7 @@ class SecurityShiftRequestApiErrors
     result[:endsAt] = security_shift_request.errors[:ends_at] if security_shift_request.errors[:ends_at].present?
     result[:note] = security_shift_request.errors[:note] if security_shift_request.errors[:note].present?
     result[:state] = security_shift_request.errors[:state] if security_shift_request.errors[:state].present?
+    result[:rejectReason] = security_shift_request.errors[:reject_reason] if security_shift_request.errors[:reject_reason].present?
 
     result
   end
