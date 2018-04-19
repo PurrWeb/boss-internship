8c2fbb96e997926c4d1087e54daeb6849496ef84
Igor Pugachev <ipugachev84@gmail.com>
2018-04-19 11:11:43 +0100

    Add validations to security shift request model

--- app/models/security_shift_request.rb
+++ app/models/security_shift_request.rb
@@ -5,14 +5,17 @@ class SecurityShiftRequest < ActiveRecord::Base
   belongs_to :creator, class_name: "User"
   belongs_to :venue
   has_many :security_shift_request_transitions
+  validates_associated :created_shift
 
   scope :accepted, -> { in_state(:accepted) }
 
   validates :creator, presence: true
   validates :starts_at, presence: true
   validates :ends_at, presence: true
+  validate :times_in_correct_order
+  validate :times_in_fifteen_minute_increments, if: :pending?
   validates :created_shift, presence: true, if: :assigned?
-  # validates :reject_reason, presence: true, if: :rejected?
+  validates :reject_reason, presence: true, if: :rejected?
 
   def state_machine
     @state_machine ||= SecurityShiftRequestStateMachine.new(self,
@@ -52,4 +55,25 @@ class SecurityShiftRequest < ActiveRecord::Base
     :current_state,
     :last_transition,
     to: :state_machine
+
+  private
+  # validation
+  def times_in_correct_order
+    if starts_at.present? && ends_at.present?
+      errors.add(:base, 'starts time must be after end time') if starts_at >= ends_at
+    end
+  end
+
+  # validation
+  def times_in_fifteen_minute_increments
+    [:starts_at, :ends_at].each do |field|
+      time = public_send(field)
+      if time.present?
+        minute = Integer(time.strftime('%M'))
+        if ![0, 30].include?(minute)
+          errors.add(field, 'must be 30 minute intervals')
+        end
+      end
+    end
+  end
 end
