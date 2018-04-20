0fd79bd518ad07a41e209449ec2a8e653d184d19
vlad <tgkfdjk@gmail.com>
2018-04-20 08:27:03 +0100

    Remove mock data

--- app/frontend/apps/security-shift-requests/index.js
+++ app/frontend/apps/security-shift-requests/index.js
@@ -5,69 +5,6 @@ import { loadInitialData } from './redux/actions';
 import reducers from './redux/reducers';
 import SecurityShiftRequestsPage from './containers/security-shift-requests-page';
 
-const initialData = {
-  venueId: 1,
-  startDate: '09-04-2018',
-  endDate: '16-04-2018',
-  securityShiftRequests: [
-    {
-      id: 1,
-      startsAt: '2018-04-10T09:00:00.000Z',
-      endsAt: '2018-04-10T10:00:00.000Z',
-      venueId: 1,
-      note: 'dfgdfg',
-      shiftType: 'normal',
-      status: 'pending',
-    },
-    {
-      id: 2,
-      startsAt: '2018-04-10T11:00:00.000Z',
-      endsAt: '2018-04-10T12:00:00.000Z',
-      venueId: 1,
-      note: 'dfgdfhfgdhgfg',
-      shiftType: 'stand_by',
-      status: 'pending',
-    },
-    {
-      id: 3,
-      startsAt: '2018-04-10T13:00:00.000Z',
-      endsAt: '2018-04-10T14:30:00.000Z',
-      venueId: 1,
-      note: 'dfggfhhgfdfg',
-      shiftType: 'normal',
-      status: 'pending',
-    },
-    {
-      id: 4,
-      startsAt: '2018-04-10T13:00:00.000Z',
-      endsAt: '2018-04-10T14:30:00.000Z',
-      venueId: 1,
-      note: 'dfggfhhgfdfg',
-      shiftType: 'normal',
-      status: 'accepted',
-    },
-    {
-      id: 5,
-      startsAt: '2018-04-10T13:00:00.000Z',
-      endsAt: '2018-04-10T14:30:00.000Z',
-      venueId: 1,
-      note: 'dfggfhhgfdfg',
-      shiftType: 'stand_by',
-      status: 'assigned',
-      rotaShiftId: 9856,
-    },
-    {
-      id: 6,
-      startsAt: '2018-04-10T13:00:00.000Z',
-      endsAt: '2018-04-10T14:30:00.000Z',
-      venueId: 1,
-      note: 'dfggfhhgfdfg',
-      shiftType: 'stand_by',
-      status: 'rejected',
-    },
-  ],
-};
-
 class SecurityShiftRequestsApp extends Component {
   componentWillMount() {
     const { accessToken } = this.props;
