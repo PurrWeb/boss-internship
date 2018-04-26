1d32b6133f1739961ddb87a0431bc8f848549db7
Igor Pugachev <ipugachev84@gmail.com>
2018-04-26 11:17:25 +0100

    [finance reports] Add styles for links

--- app/frontend/apps/finance-reports/index.js
+++ app/frontend/apps/finance-reports/index.js
@@ -2,12 +2,13 @@ import React, { Component } from 'react';
 import { Provider } from 'react-redux';
 import configureStore from '~/apps/store';
 
- import reducers from './redux/reducers';
- import { loadInitialData } from './redux/actions';
+import reducers from './redux/reducers';
+import { loadInitialData } from './redux/actions';
 import FinanceReportsPage from './containers/page';
 
 export default class FinanceReportsApp extends Component {
   componentWillMount() {
+    require('./styles.css');
     const { accessToken } = this.props;
     if (!accessToken) {
       throw new Error('Access token must be present');
@@ -22,7 +23,7 @@ export default class FinanceReportsApp extends Component {
     return (
       <Provider store={this.store}>
         <FinanceReportsPage />
-     </Provider>
+      </Provider>
     );
   }
 }
