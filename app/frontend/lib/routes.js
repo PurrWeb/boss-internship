e90af7e75a05fc6d5c2e1bff2f0d1b7d61c27194
vlad <tgkfdjk@gmail.com>
2018-05-08 14:34:11 +0100

    [payroll-reports] add routes and fix staffMemberHoursOverview bug

--- app/frontend/lib/routes.js
+++ app/frontend/lib/routes.js
@@ -37,6 +37,12 @@ export const appRoutes = {
         var payRateFilter = oFetch(options, "payRateFilter");
         return `/finance_reports/${utils.formatRotaUrlDate(date)}.pdf?venue_id=${venueId}&pay_rate_filter=${payRateFilter}`;
     },
+    payrollReportsPdfDownload: function(options){
+        var date = oFetch(options, "date");
+        var venueId = oFetch(options, "venueId");
+        var payRateFilter = oFetch(options, "payRateFilter");
+        return `/payroll_reports/${utils.formatRotaUrlDate(date)}.pdf?venue_id=${venueId}&pay_rate_filter=${payRateFilter}`;
+    },
     securityRotaOverview: (options) => {
         const startDate = oFetch(options, "startDate");
         return `/security_rotas?highlight_date=${utils.formatRotaUrlDate(startDate)}`;
@@ -60,6 +66,11 @@ export const appRoutes = {
         const startDate = oFetch(options, "startDate");
         const venueId = oFetch(options, "venueId");
         return `/finance_reports/${utils.formatRotaUrlDate(startDate)}?venue_id=${venueId}`;
+    },
+    payrollReports: (options) => {
+        const startDate = oFetch(options, "startDate");
+        const venueId = oFetch(options, "venueId");
+        return `/payroll_reports/${utils.formatRotaUrlDate(startDate)}?venue_id=${venueId}`;
     },
     rotaOverview: function(options){
         var [venueId, startDate] = oFetch(options, "venueId", "startDate");
