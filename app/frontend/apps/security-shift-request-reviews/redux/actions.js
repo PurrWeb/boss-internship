f056345caf17fd038193a1c952430f6c86b1c21b
Igor Pugachev <ipugachev84@gmail.com>
2018-05-14 12:32:11 +0100

    Remove shift request when updateing if not in current week range

--- app/frontend/apps/security-shift-request-reviews/redux/actions.js
+++ app/frontend/apps/security-shift-request-reviews/redux/actions.js
@@ -1,5 +1,6 @@
 import { createAction } from 'redux-actions';
 import oFetch from 'o-fetch';
+import utils from '~/lib/utils';
 import * as types from './types';
 
 import {
@@ -12,8 +13,14 @@ import {
 
 export const loadInitialData = createAction(types.INITIAL_LOAD);
 export const updateSecurityShiftRequestAction = createAction(types.UPDATE_SECURITY_SHIFT_REQUEST);
+export const removeSecurityShiftRequestAction = createAction(types.REMOVE_SECURITY_SHIFT_REQUEST);
 
 export const editSecurityShiftRequest = values => (dispatch, getState) => {
+  const pageOptions = getState()
+    .get('pageOptions')
+    .toJS();
+  const weekStartDate = oFetch(pageOptions, 'startDate');
+  const weekEndDate = oFetch(pageOptions, 'endDate');
   const startsAt = oFetch(values, 'startsAt').toISOString();
   const endsAt = oFetch(values, 'endsAt').toISOString();
   const venueId = oFetch(values, 'venueId');
@@ -27,7 +34,12 @@ export const editSecurityShiftRequest = values => (dispatch, getState) => {
     id,
     note,
   }).then(response => {
-    dispatch(updateSecurityShiftRequestAction(response.data));
+    const securityShiftRequest = oFetch(response, 'data.securityShiftRequest');
+    if (utils.shiftInRotaWeek(weekStartDate, weekEndDate, securityShiftRequest)) {
+      dispatch(updateSecurityShiftRequestAction(response.data));
+    } else {
+      dispatch(removeSecurityShiftRequestAction(response.data));
+    }
     return response;
   });
 };
--- app/frontend/apps/security-shift-requests/redux/actions.js
+++ app/frontend/apps/security-shift-requests/redux/actions.js
@@ -11,6 +11,7 @@ import {
 export const loadInitialData = createAction(types.INITIAL_LOAD);
 export const addSecurityShiftRequestAction = createAction(types.ADD_SECURITY_SHIFT_REQUEST);
 export const updateSecurityShiftRequestAction = createAction(types.UPDATE_SECURITY_SHIFT_REQUEST);
+export const removeSecurityShiftRequestAction = createAction(types.REMOVE_SECURITY_SHIFT_REQUEST);
 
 export const addSecurityShiftRequest = params => (dispatch, getState) => {
   const pageOptions = getState()
@@ -28,13 +29,19 @@ export const addSecurityShiftRequest = params => (dispatch, getState) => {
     note,
     venueId,
   }).then(resp => {
-    if (utils.shiftInRotaWeek(weekStartDate, weekEndDate, resp.data)) {
+    const securityShiftRequest = oFetch(resp, 'data.securityShiftRequest');
+    if (utils.shiftInRotaWeek(weekStartDate, weekEndDate, securityShiftRequest)) {
       dispatch(addSecurityShiftRequestAction(resp.data));
     }
   });
 };
 
 export const editSecurityShiftRequest = values => (dispatch, getState) => {
+  const pageOptions = getState()
+    .get('pageOptions')
+    .toJS();
+  const weekStartDate = oFetch(pageOptions, 'startDate');
+  const weekEndDate = oFetch(pageOptions, 'endDate');
   const startsAt = oFetch(values, 'startsAt').toISOString();
   const endsAt = oFetch(values, 'endsAt').toISOString();
   const venueId = oFetch(values, 'venueId');
@@ -48,7 +55,12 @@ export const editSecurityShiftRequest = values => (dispatch, getState) => {
     id,
     note,
   }).then(response => {
-    dispatch(updateSecurityShiftRequestAction(response.data));
+    const securityShiftRequest = oFetch(response, 'data.securityShiftRequest');
+    if (utils.shiftInRotaWeek(weekStartDate, weekEndDate, securityShiftRequest)) {
+      dispatch(updateSecurityShiftRequestAction(response.data));
+    } else {
+      dispatch(removeSecurityShiftRequestAction(response.data))
+    }
     return response;
   });
 };
