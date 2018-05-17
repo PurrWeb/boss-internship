23203595417cf93dc987b8c933fc18e6dd8961cb
William Billington <william.billington@gmail.com>
2018-05-17 15:39:25 +0100

    Separate zip and dump steps when preforming backup

--- app/services/upload_backup.rb
+++ app/services/upload_backup.rb
@@ -12,17 +12,26 @@ module Services
         cmd << "-u #{uri.user}"
         cmd << "--password=#{uri.password}" if uri.password
         cmd << name
-        cmd << "|"
-        cmd << "gzip -9"
 
-        path = "#{dir}/#{name}-#{Time.zone.now.strftime("%Y.%m.%d-%H.%M.%S")}.sql.gz"
+        sql_filename = "#{name}-#{Time.zone.now.strftime("%Y.%m.%d-%H.%M.%S")}.sql"
+        sql_file_path = "#{dir}/#{sql_filename}"
+        sql_command = "#{cmd.join(" ")} 2>&1 > #{sql_file_path}"
+        puts "running: #{sql_command}"
 
-        out = `#{cmd.join(" ")} 2>&1 > #{path}`
+        sql_command_out = `#{sql_command}`
         unless $?.success?
-          raise "mysqldump failed: #{out}"
+          raise "mysqldump failed: #{sql_command_out}"
         end
 
-        backup = Backup.create! dump: File.open(path), size: File.size(path)
+        zip_file_path = "#{sql_file_path}.gz"
+        zip_command = "gzip -c #{sql_file_path} 2>&1 > #{zip_file_path}"
+
+        zip_command_out = `#{zip_command}`
+        unless $?.success?
+          raise "zipping dump file failed: #{zip_command_out}"
+        end
+
+        backup = Backup.create! dump: File.open(zip_file_path), size: File.size(zip_file_path)
 
         "Created DB dump of #{name} (#{number_to_human_size backup.size})"
       end
