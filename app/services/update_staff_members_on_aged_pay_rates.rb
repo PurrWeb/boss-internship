6f5b7d84f9776dbe89d3c83fc20f24f233d15d64
William Billington <william.billington@gmail.com>
2018-06-18 12:21:30 +0100

    Use correct service to update staff member pay rate

--- app/services/update_staff_members_on_aged_pay_rates.rb
+++ app/services/update_staff_members_on_aged_pay_rates.rb
@@ -9,8 +9,9 @@ class UpdateStaffMembersOnAgedPayRates
       StaffMember.enabled.with_aged_payrates.each do |staff_member|
         begin
           pay_rate = StaffMemberRealPayRate.new(now: now, staff_member: staff_member).call
-          if pay_rate.present?
-              staff_member.update!(pay_rate: pay_rate)
+          if  pay_rate.present?
+            result = UpdateStaffMemberEmploymentDetails.new(staff_member: staff_member, pay_rate: pay_rate).call
+            raise "Pay rate update to pay_rate: #{pay_rate.id} unsuccessful for staff_member:#{staff_member.id}" unless result.success?
           end
         rescue PayRateException => e
           Rollbar.error(e.message)
