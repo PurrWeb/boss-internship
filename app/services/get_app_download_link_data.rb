1bd5522c923d7c8e0395ffac3905a948bf2b9c54
William Billington <william.billington@gmail.com>
2018-06-20 16:51:36 +0100

    Fix 500

--- app/services/get_app_download_link_data.rb
+++ app/services/get_app_download_link_data.rb
@@ -1,8 +1,9 @@
 class GetAppDownloadLinkData
-  def initialize(staff_member:)
+  def initialize(staff_member:, url_helpers: Rails.application.routes.url_helpers)
     @staff_member = staff_member
+    @url_helpers = url_helpers
   end
-  attr_reader :staff_member
+  attr_reader :staff_member, :url_helpers
 
   def call
     MobileApp.enabled.with_download_url.map do |mobile_app|
@@ -11,7 +12,7 @@ class GetAppDownloadLinkData
         AppDownloadLink.new(
           mobile_app_id: mobile_app.id,
           app_name: mobile_app.name,
-          download_url: api_v1_staff_member_send_app_download_email_path(staff_member.id),
+          download_url: url_helpers.api_v1_staff_member_send_app_download_email_path(staff_member.id),
           last_sent_at: last_download_sent.andand.sent_at
         )
       else
