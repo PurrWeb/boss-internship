432b1b525ba46138c2ba50414c0e45a6f09e7b81
vlad <tgkfdjk@gmail.com>
2018-07-12 13:43:53 +0100

    Move handleClose inside function to open/close multiple modals

--- app/frontend/components/modals/modal-decorator.js
+++ app/frontend/components/modals/modal-decorator.js
@@ -3,21 +3,11 @@ import ReactDOM from 'react-dom';
 export default function modalDecorator(openModalFn) {
   return function({ submit = () => {}, config = {}, props, closeCallback = () => {} }) {
     let bodyFirst, wrapper;
-
     const handleClose = () => {
-      destroyModal();
-      return closeCallback();
-    };
-
-    const destroyModal = () => {
       setTimeout(() => {
         ReactDOM.unmountComponentAtNode(wrapper);
         wrapper.remove();
       }, 50);
-    };
-
-    const handleCloseAfterSubmit = () => {
-      destroyModal();
     };
 
     bodyFirst = document.body.firstChild;
@@ -25,9 +15,8 @@ export default function modalDecorator(openModalFn) {
     bodyFirst.parentNode.insertBefore(wrapper, bodyFirst);
 
     const handleSubmit = (...args) => {
-      return Promise.resolve(submit(handleCloseAfterSubmit, ...args));
+      return Promise.resolve(submit(handleClose, ...args));
     };
-
-    return openModalFn(config, props, handleSubmit, handleClose, wrapper);
+    return openModalFn(config, props, handleSubmit, handleClose, wrapper, closeCallback);
   };
 }
