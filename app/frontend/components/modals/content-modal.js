13a8f18f076165fccdfb1620f7c3a5533b83faf9
vlad <tgkfdjk@gmail.com>
2018-07-12 13:34:51 +0100

    Add modalClassName and onClose to content-modal

--- app/frontend/components/modals/content-modal.js
+++ app/frontend/components/modals/content-modal.js
@@ -7,67 +7,38 @@ import modalDecorator from './modal-decorator';
 
 class ContentModal extends React.Component {
   render() {
-    const {
-      show,
-      children,
-      title,
-      modalClassName,
-      onClose,
-      modalRoleClassName,
-      closeButtonOverflowed,
-    } = this.props;
-
+    const { show, children, title, modalClassName, onClose } = this.props;
     return (
       <Modal
         isOpen={show}
         contentLabel="Modal"
         className={{
-          base: `ReactModal__Content ReactModal__Content--after-open boss-modal-window ${modalRoleClassName} ${modalClassName}`,
+          base: `ReactModal__Content ReactModal__Content--after-open boss-modal-window ${modalClassName}`,
         }}
       >
-        {closeButtonOverflowed ? (
-          <button onClick={onClose} type="button" className="boss-modal-window__close" />
-          ) : (
-            <button onClick={onClose} className="boss-modal-window__close-inner" />
-          )}
-        <div className="boss-modal-window__header">
-          {title}
-        </div>
-        <div className="boss-modal-window__content">
-          {children}
-        </div>
+        <button onClick={onClose} type="button" className="boss-modal-window__close" />
+        <div className="boss-modal-window__header">{title}</div>
+        <div className="boss-modal-window__content">{children}</div>
       </Modal>
-    )
-  }
-}
-
-function openContentModal(
-  {
-    title: title = 'Content Modal',
-    modalRoleClassName: modalRoleClassName = 'boss-modal-window_role_edit',
-    modalClassName,
-    closeButtonOverflowed: closeButtonOverflowed = false,
-  },
-  props = {},
-  onSubmit,
-  onClose,
-  wrapper
-) {
-  return function(Component) {
-    ReactDOM.render(
-      <ContentModal
-        show={true}
-        title={title}
-        modalRoleClassName={modalRoleClassName}
-        closeButtonOverflowed={closeButtonOverflowed}
-        onClose={onClose}
-        modalClassName={modalClassName}
-      >
-        <Component onSubmit={onSubmit} onClose={onClose} {...props} />
-      </ContentModal>,
-      wrapper
     );
   }
 }
 
-export default modalDecorator(openContentModal)
+function openContentModal(
+  { title: title = 'Content Modal', modalClassName: modalClassName = 'boss-modal-window_role_edit' },
+  props = {},
+  onSubmit,
+  onClose,
+  wrapper,
+) {
+  return function(Component) {
+    ReactDOM.render(
+      <ContentModal show={true} title={title} onClose={onClose} modalClassName={modalClassName}>
+        <Component onSubmit={onSubmit} onClose={onClose} {...props} />
+      </ContentModal>,
+      wrapper,
+    );
+  };
+}
+
+export default modalDecorator(openContentModal);
