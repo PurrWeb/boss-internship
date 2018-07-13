35cb234b26915cfff0544e52951256a23138e22f
William Billington <william.billington@gmail.com>
2018-07-13 13:56:12 +0100

    Refactor global try catch code

--- app/frontend/lib/global-try-catch.js
+++ app/frontend/lib/global-try-catch.js
@@ -5,6 +5,7 @@ import oFetch from 'o-fetch';
 
 const rollbarData = oFetch(window, 'boss.rollbarData');
 const appVersion = oFetch(window, 'boss.currentVersion');
+import { rollbarPresent, getRollbarPayload } from '~/lib/rollbar-helpers';
 
 const ReactTryCatchBatchingStrategy = {
   // this is part of the BatchingStrategy API. simply pass along
@@ -15,36 +16,8 @@ const ReactTryCatchBatchingStrategy = {
     try {
       ReactDefaultBatchingStrategy.batchedUpdates(...args);
     } catch (e) {
-      if (typeof Rollbar !== 'undefined') {
-        let payload = {};
-        payload.app_version = appVersion;
-
-        if (typeof rollbarData.currentVenue !== 'undefined') {
-          let { id, name, rollbar_guid } = rollbarData.currentVenue
-          payload.venue = { id, name };
-          payload.person = {
-            id: rollbar_guid,
-            username: `Venue: ${ name }`,
-          }
-        }
-
-        if (typeof rollbarData.currentUser !== 'undefined') {
-          let { id, name, rollbar_guid } = rollbarData.currentUser;
-          payload.user = { id, name };
-          payload.person = {
-            id: rollbar_guid,
-            username: `User: ${ name }`,
-          }
-        }
-        if (typeof rollbarData.currentStaffMember !== 'undefined') {
-          let { id, name, rollbar_guid } = rollbarData.currentStaffMember
-          payload.staff_member = { id, name };
-          payload.person = {
-            id: rollbar_guid,
-            username: `StaffMember: ${ name }`,
-          }
-        }
-        Rollbar.configure({payload: payload});
+      if (rollbarPresent()) {
+        Rollbar.configure({payload: getRollbarPayload()});
       }
       errorHandler.throwErrorPage();
       throw e;
