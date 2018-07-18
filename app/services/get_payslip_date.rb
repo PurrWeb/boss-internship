c35f55b6ed68846c35426d94ec3b6b3bf3cc3792
William Billington <william.billington@gmail.com>
2018-07-18 16:36:21 +0100

    Set payslip date to same week as item in question

--- app/services/get_payslip_date.rb
+++ app/services/get_payslip_date.rb
@@ -7,13 +7,12 @@ class GetPayslipDate
   def call(now: Time.current)
     rota_date = RotaShiftDate.to_rota_date(now)
     current_week = RotaWeek.new(rota_date)
-    next_week = RotaWeek.new(rota_date + 1.week)
 
     if item_date > current_week.end_date
-      item_payslip_week = RotaWeek.new(item_date + 1.week)
+      item_payslip_week = RotaWeek.new(item_date)
       item_payslip_week.start_date
     else
-      next_week.start_date
+      current_week.start_date
     end
   end
 end
