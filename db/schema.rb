d1dfb0cb4bf088dfbea5319770d6ad6fb084400b
William Billington <william.billington@gmail.com>
2018-07-31 12:12:37 +0100

    Migrate records to have accossiated finance reports

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180731103829) do
+ActiveRecord::Schema.define(version: 20180731105458) do
 
   create_table "accessories", force: :cascade do |t|
     t.integer  "venue_id",         limit: 4
