8b3d9ae0e77d5135f592b043c5019de5fd423ded
William Billington <william.billington@gmail.com>
2018-08-03 17:30:32 +0100

    Update finance report when deleting owed hours

--- spec/services/delete_owed_hour_spec.rb
+++ spec/services/delete_owed_hour_spec.rb
@@ -1,8 +1,40 @@
 require 'rails_helper'
 
 RSpec.describe 'DeleteOwedHour service'  do
+  let(:now) { Time.current }
+  let(:today) { RotaShiftDate.to_rota_date(now) }
+  let(:current_week) { RotaWeek.new(today) }
   let(:requester) { FactoryGirl.create(:user) }
-  let(:owed_hour) { FactoryGirl.create(:owed_hour) }
+  let(:date) { current_week.start_date  }
+  let(:start_of_owed_hour_day) { RotaShiftDate.new(date).start_time }
+  let(:duration_minutes) { 50 }
+  let(:payslip_date) { current_week.start_date }
+  let(:requester) { FactoryGirl.create(:user) }
+  let(:owed_hour) do
+    FactoryGirl.create(
+      :owed_hour,
+      staff_member: staff_member,
+      date: date,
+      payslip_date: payslip_date,
+      minutes: duration_minutes,
+      starts_at: start_of_owed_hour_day,
+      ends_at: start_of_owed_hour_day + duration_minutes.minutes,
+      finance_report: existing_finance_report
+    )
+  end
+  let(:staff_member) { FactoryGirl.create(:staff_member, master_venue: venue) }
+  let(:venue) { FactoryGirl.create(:venue) }
+  let(:existing_finance_report) do
+    FactoryGirl.create(
+      :finance_report,
+      staff_member: staff_member,
+      venue: venue,
+      week_start: current_week.start_date
+    ).tap do |report|
+      report.mark_ready!
+    end
+  end
+
   let(:service) do
     DeleteOwedHour.new(
       requester: requester,
@@ -13,6 +45,9 @@ RSpec.describe 'DeleteOwedHour service'  do
   context 'before call' do
     specify 'owed_hour should be enabled' do
       expect(owed_hour.disabled?).to eq(false)
+    end
+
+    specify 'finance report should be ready' do
     end
   end
 
@@ -27,6 +62,11 @@ RSpec.describe 'DeleteOwedHour service'  do
 
     specify 'owed_hour disabled_by is requester' do
       expect(owed_hour.disabled_by).to eq(requester)
+    end
+
+    specify 'finance report should be marked as update' do
+      finance_report = existing_finance_report.reload
+      expect(finance_report.requiring_update?).to eq(true)
     end
   end
 
