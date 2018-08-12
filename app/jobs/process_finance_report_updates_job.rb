48c6c8c3bf0aea77a87d80c06198c370a2e4254f
William Billington <william.billington@gmail.com>
2018-08-12 22:10:52 +0200

    Update app/jobs/process_finance_report_updates_job.rb

--- app/jobs/process_finance_report_updates_job.rb
+++ app/jobs/process_finance_report_updates_job.rb
@@ -1,6 +1,6 @@
 class ProcessFinanceReportUpdatesJob < ActiveJob::Base
   def perform
-    FinanceReport.requiring_update.limit(5).each do |finance_report|
+    FinanceReport.in_state(:requiring_update).order(week_start: :desc).limit(5).each do |finance_report|
       UpdateFinanceReportData.new(requester: User.first, finance_report: finance_report).call
     end
   end
