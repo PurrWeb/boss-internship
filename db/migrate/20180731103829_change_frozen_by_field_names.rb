95ead81b3682c43814bad01db2f29fcd793602c8
William Billington <william.billington@gmail.com>
2018-08-13 23:08:19 +0100

    Remove foreign_key constraint from db before migration

--- db/migrate/20180731103829_change_frozen_by_field_names.rb
+++ db/migrate/20180731103829_change_frozen_by_field_names.rb
@@ -1,6 +1,8 @@
 class ChangeFrozenByFieldNames < ActiveRecord::Migration
   def change
+    remove_foreign_key :accessory_refund_requests, :frozen_by
     rename_column :accessory_refund_requests, :frozen_by_id, :finance_report_id
+    remove_foreign_key :accessory_requests, :frozen_by
     rename_column :accessory_requests, :frozen_by_id, :finance_report_id
     rename_column :holidays, :frozen_by_finance_report_id, :finance_report_id
     rename_column :hours_acceptance_periods, :frozen_by_finance_report_id, :finance_report_id
