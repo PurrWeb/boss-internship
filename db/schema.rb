fa1ab539318291f6ae0a659f7ca6d7117f912335
Igor Pugachev <ipugachev84@gmail.com>
2018-08-30 15:42:27 +0100

    Add `Diffing Versions` for `paper_trail`

--- db/schema.rb
+++ db/schema.rb
@@ -1350,12 +1350,13 @@ ActiveRecord::Schema.define(version: 20180830152720) do
   add_index "venues_users", ["enabled"], name: "index_venues_users_on_enabled", using: :btree
 
   create_table "versions", force: :cascade do |t|
-    t.string   "item_type",  limit: 255,        null: false
-    t.integer  "item_id",    limit: 4,          null: false
-    t.string   "event",      limit: 255,        null: false
-    t.string   "whodunnit",  limit: 255
-    t.text     "object",     limit: 4294967295
+    t.string   "item_type",      limit: 255,        null: false
+    t.integer  "item_id",        limit: 4,          null: false
+    t.string   "event",          limit: 255,        null: false
+    t.string   "whodunnit",      limit: 255
+    t.text     "object",         limit: 4294967295
     t.datetime "created_at"
+    t.text     "object_changes", limit: 4294967295
   end
 
   add_index "versions", ["item_type", "item_id"], name: "index_versions_on_item_type_and_item_id", using: :btree
