c163e5904fd7d96caef55c50a9a186f8e6853cc4
Igor Pugachev <ipugachev84@gmail.com>
2018-08-31 16:25:53 +0100

    Add card_number to WtlClientHistoryService

--- app/services/wtl_client_history_service.rb
+++ app/services/wtl_client_history_service.rb
@@ -10,7 +10,12 @@ class WtlClientHistoryService
       created_at = version.created_at
       changeset = version.changeset
       full_name = whodunnit.present? ? whodunnit["full_name"] : "System"
-
+      if changeset.key?("wtl_card_id")
+        from, to = changeset["wtl_card_id"]
+        from = WtlCard.find_by(id: from).number if from.present?
+        to = WtlCard.find_by(id: to).number if to.present?
+        changeset["card_number"] = [from, to]
+      end
       acc[created_at] = {
         by: full_name,
         to: nil,
