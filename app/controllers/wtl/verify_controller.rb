a41a7d95dd776d68595914fab628e699adf83e2c
Igor Pugachev <ipugachev84@gmail.com>
2018-09-03 12:51:35 +0100

    Add VerifyController redirect to error conditions

--- app/controllers/wtl/verify_controller.rb
+++ app/controllers/wtl/verify_controller.rb
@@ -4,25 +4,31 @@ module Wtl
     skip_before_filter :authenticate_user!
 
     def verify
-      verification_token = params.fetch('verificationToken')
+      verification_token = params.fetch("verificationToken")
       wtl_client = WtlClient.find_by(verification_token: verification_token)
       if !wtl_client.present?
-        return redirect_to wtl_something_went_wrong_path(message: 'There was an issue with your validation token.')
+        return redirect_to wtl_something_went_wrong_path(message: "There was an issue with your validation token.")
       end
       if wtl_client.verified?
-        return redirect_to wtl_something_went_wrong_path(message: 'This account has already been verified.')
+        return redirect_to wtl_something_went_wrong_path(message: "This verification link has already been used. Your card is ready to use.")
+      end
+      if wtl_client.disabled?
+        return redirect_to wtl_something_went_wrong_path(message: "We were unable to verify this email address due to an issue with your account.")
+      end
+      if wtl_client.wtl_card.present? && wtl_client.wtl_card.disabled?
+        return redirect_to wtl_something_went_wrong_path(message: "We were unable to verify this email address due to an issue with your account.")
       end
 
       wtl_client.verify!
 
       render(
-        locals: {}
+        locals: {},
       )
     end
 
     def something_went_wrong
       render(
-        locals: { message: params.fetch("message") }
+        locals: {message: params.fetch("message")},
       )
     end
 
