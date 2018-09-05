ce7c34bbf625b5311fbf58dbe7803013da00acf8
Igor Pugachev <ipugachev84@gmail.com>
2018-09-05 08:17:41 +0100

    Add `ash_cash_cents` and `security_plus_cents` to SafeCheck

--- app/frontend/components/safe-checks/details-modal.js
+++ app/frontend/components/safe-checks/details-modal.js
@@ -157,6 +157,26 @@ export default class DetailsModal extends React.Component {
 
                   <div className="boss-stats__table-row">
                     <div className="boss-stats__table-cell">
+                      <p className="boss-stats__table-text boss-stats__table-text_role_label">Ash Cash</p>
+                    </div>
+
+                    <div className="boss-stats__table-cell">
+                      <p className="boss-stats__table-text">{this.props.costs.ash_cash_cents}</p>
+                    </div>
+                  </div>
+
+                  <div className="boss-stats__table-row">
+                    <div className="boss-stats__table-cell">
+                      <p className="boss-stats__table-text boss-stats__table-text_role_label">Security Plus</p>
+                    </div>
+
+                    <div className="boss-stats__table-cell">
+                      <p className="boss-stats__table-text">{this.props.costs.security_plus_cents}</p>
+                    </div>
+                  </div>
+
+                  <div className="boss-stats__table-row">
+                    <div className="boss-stats__table-cell">
                       <p className="boss-stats__table-text boss-stats__table-text_role_label">Other</p>
                     </div>
 
