3bf9467b5921020e24ba8fca5bcafa5ab03a1bd3
vlad <tgkfdjk@gmail.com>
2018-09-06 13:43:49 +0100

    Add updating reports from response

--- app/frontend/apps/finance-reports/redux/actions.js
+++ app/frontend/apps/finance-reports/redux/actions.js
@@ -8,7 +8,6 @@ import { markReportCompletedRequest, markReportsCompletedRequest } from '../requ
 export const loadInitialData = createAction(types.LOAD_INITIAL_DATA);
 export const setReportStatusDone = createAction(types.SET_REPORT_STATUS_DONE);
 export const changePayRateFilter = createAction(types.CHANGE_PAY_RATE_FILTER);
-export const markReportCompletedAction = createAction(types.MARK_REPORT_COMPLETED);
 export const markReportsCompletedAction = createAction(types.MARK_REPORTS_COMPLETED);
 
 export const markReportCompleted = options => (dispatch, getState) => {
@@ -16,10 +15,9 @@ export const markReportCompleted = options => (dispatch, getState) => {
   const date = oFetch(pageStateJS, 'date');
   const venueId = oFetch(pageStateJS, 'venueId');
   const staffMemberId = oFetch(options, 'staffMemberId');
-  const reportsId = oFetch(options, 'reportsId');
 
   return markReportCompletedRequest({ date, staffMemberId, venueId }).then(response => {
-    dispatch(markReportCompletedAction({ reportsId }));
+    dispatch(markReportsCompletedAction(response.data));
   });
 };
 
@@ -28,9 +26,8 @@ export const markReportsCompleted = options => (dispatch, getState) => {
   const date = oFetch(pageStateJS, 'date');
   const venueId = oFetch(pageStateJS, 'venueId');
   const staffMemberIds = oFetch(options, 'staffMemberIds');
-  const reportsIds = oFetch(options, 'reportsIds');
 
   return markReportsCompletedRequest({ date, staffMemberIds, venueId }).then(response => {
-    dispatch(markReportsCompletedAction({ reportsIds }));
+    dispatch(markReportsCompletedAction(response.data));
   });
 };
