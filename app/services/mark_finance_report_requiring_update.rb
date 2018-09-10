e62abab1f9ba9af11fac07263f5776194adc2c8f
William Billington <william.billington@gmail.com>
2018-09-10 12:53:37 +0100

    Create FindOrCreateFinanceReport service

--- app/services/mark_finance_report_requiring_update.rb
+++ app/services/mark_finance_report_requiring_update.rb
@@ -6,31 +6,6 @@ class MarkFinanceReportRequiringUpdate
   attr_reader :staff_member, :week
 
   def call
-    raise "attempt to update finance report for staff member #{staff_member.id} who can't have finance report" unless staff_member.can_have_finance_reports?
-
-    week_start = week.start_date
-    venue = staff_member.master_venue
-
-    finance_report = FinanceReport.find_by(
-      staff_member: staff_member,
-      venue: venue,
-      week_start: week_start
-    )
-
-    if finance_report.present?
-     raise "Attempt to mark comepleted finance report #{finance_report.id} requiring update" if finance_report.done?
-     finance_report.mark_requiring_update!
-    else
-      finance_report = FinanceReport.create!(
-        staff_member: staff_member,
-        staff_member_name: staff_member.full_name,
-        pay_rate_description: staff_member.pay_rate.text_description_short,
-        venue: venue,
-        venue_name: venue.name,
-        week_start: week_start,
-        requiring_update: true
-      )
-    end
-    finance_report
+    FindOrCreateFinanceReport.new(staff_member: staff_member, week: week, mark_existing_requiring_update: true).call
   end
 end
