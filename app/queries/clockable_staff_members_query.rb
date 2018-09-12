0d1f7a6ecc3ad4d46019922666eeed039bda38a1
Igor Pugachev <ipugachev84@gmail.com>
2018-09-12 13:10:22 +0100

    Exclude staff members with holidays on current rota day

--- app/queries/clockable_staff_members_query.rb
+++ app/queries/clockable_staff_members_query.rb
@@ -1,26 +1,34 @@
 class ClockableStaffMembersQuery
-  def initialize(venue:, rota_shifts:)
+  def initialize(venue:, rota_shifts:, rota_date: RotaShiftDate.to_rota_date(Time.current))
     @venue = venue
     @rota_shifts = rota_shifts
+    @rota_date = rota_date
   end
+
   attr_reader :venue
 
   def all
     venue_members = StaffMember.for_venue(venue)
-    rotaed_members = StaffMember.where(
-      id: rota_shifts.map(&:staff_member_id).uniq
+    rotaed_members_ids = StaffMember.where(
+      id: rota_shifts.pluck(:staff_member_id).uniq,
     )
 
-    security_staff = StaffMember.where(
-      staff_type_id: StaffType.security.map(&:id)
-    )
+    security_staff_ids = StaffMember.where(
+      staff_type_id: StaffType.security.pluck(:id),
+    ).pluck(:id)
 
-    StaffMember.enabled.where(
-      id: venue_members.map(&:id) + security_staff.map(&:id) + rotaed_members.map(&:id)
-    )
+    staff_with_holidays_ids = InRangeQuery.new(
+      relation: Holiday.in_state(:enabled),
+      start_value: rota_date,
+      end_value: rota_date,
+      start_column_name: "start_date",
+      end_column_name: "end_date",
+    ).all.pluck(:staff_member_id).uniq
+
+    StaffMember.enabled.where(id: venue_members.pluck(:id) + security_staff_ids + rotaed_members_ids - staff_with_holidays_ids)
   end
 
   private
 
-  attr_reader :rota_shifts
+  attr_reader :rota_shifts, :rota_date
 end
