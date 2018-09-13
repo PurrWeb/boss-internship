461f65b4ddda630013a4ef6657d35c10afc5ca7e
vlad <tgkfdjk@gmail.com>
2018-09-13 12:49:50 +0100

    Fix owed hours deletion, add oFetch

--- app/frontend/apps/staff-member-profile/owed-hours-page/components/owed-hours-mobile-item.js
+++ app/frontend/apps/staff-member-profile/owed-hours-page/components/owed-hours-mobile-item.js
@@ -1,100 +1,105 @@
 import React from 'react';
-import humanize from 'string-humanize';
-import confirm from '~/lib/confirm-utils';
-import {getOwedHourUIData} from './owed-hours-table';
+import { openWarningModal } from '~/components/modals';
+import { getOwedHourUIData } from './owed-hours-table';
 import oFetch from 'o-fetch';
 
-const OwedHourMobileItem = ({owedHour, deleteOwedHour, openEditModal, isStaffMemberDisabled}) => {
-  
-  const onEdit = (owedHour) => {
+const OwedHourMobileItem = props => {
+  const [owedHour, deleteOwedHours, openEditModal, isStaffMemberDisabled] = oFetch(
+    props,
+    'owedHour',
+    'deleteOwedHours',
+    'openEditModal',
+    'isStaffMemberDisabled',
+  );
+  const onEdit = owedHour => {
     openEditModal(owedHour);
-  }
+  };
 
-  const onDelete = (id) => {
-    confirm('Are you sure ?', {
-      title: 'Delete Owed Hour',
-      actionButtonText: 'Delete',
-    }).then(() => {
-      deleteOwedHour(id);
+  const onDelete = owedHourId => {
+    openWarningModal({
+      submit: hideModal => deleteOwedHours(owedHourId).then(hideModal),
+      config: {
+        title: 'Delete owed hours',
+        text: 'Are You Sure?',
+        buttonText: 'Delete',
+      },
     });
-  }
+  };
 
   const owedHourJS = owedHour.toJS();
-  const [
-    date,
-    times,
-    durationHours,
-    durationMinutes,
-    creator,
-    created,
-    note,
-    editable,
-    payslipDate,
-  ] = oFetch(getOwedHourUIData(owedHourJS), "date", "times", "durationHours", "durationMinutes", "creator", "created", "note", "editable", "payslipDate");
+  const [date, times, durationHours, durationMinutes, creator, created, note, editable, payslipDate] = oFetch(
+    getOwedHourUIData(owedHourJS),
+    'date',
+    'times',
+    'durationHours',
+    'durationMinutes',
+    'creator',
+    'created',
+    'note',
+    'editable',
+    'payslipDate',
+  );
 
   const duration = `${durationHours} hours ${durationMinutes} minutes`;
   const owedHourId = oFetch(owedHourJS, 'id');
 
-  return <div className="boss-check boss-check_role_panel boss-check_page_smp-owed-hours">
-    <div className="boss-check__row">
-      <div className="boss-check__cell">
-        <p className="boss-check__title">{date}</p>
-      </div>
-    </div>
-    <div className="boss-check__row">
-      <div className="boss-check__cell">
-        <p className="boss-check__text boss-check__text_role_time boss-check__text_marked">
-          <span>
-            {times}&nbsp;
-          </span>
-          <span>
-            ({duration})
-          </span>
-        </p>
-      </div>
-    </div>
-    <div className="boss-check__row">
-      <div className="boss-check__cell">
-        <p className="boss-check__text boss-check__text_role_user">
-          <span className="boss-check__text-label">Created by: </span>
-          {creator}
-        </p>
-        <p className="boss-check__text boss-check__text_role_secondary">
-          {created}
-        </p>
-      </div>
-    </div>
-    {
-    note !== '-' && 
+  return (
+    <div className="boss-check boss-check_role_panel boss-check_page_smp-owed-hours">
       <div className="boss-check__row">
         <div className="boss-check__cell">
-          <div className="boss-check__box">
-            <p className="boss-check__text boss-check__text_role_note">{note}</p>
-          </div>
+          <p className="boss-check__title">{date}</p>
         </div>
       </div>
-    }
-    <div className="boss-check__row">
-      <div className="boss-check__cell">
-        <p className="boss-check__text boss-check__text_role_date">
-          <span className="boss-check__text-label">Payslip Date: </span>
-          {payslipDate}
-        </p>
+      <div className="boss-check__row">
+        <div className="boss-check__cell">
+          <p className="boss-check__text boss-check__text_role_time boss-check__text_marked">
+            <span>{times}&nbsp;</span>
+            <span>({duration})</span>
+          </p>
+        </div>
       </div>
+      <div className="boss-check__row">
+        <div className="boss-check__cell">
+          <p className="boss-check__text boss-check__text_role_user">
+            <span className="boss-check__text-label">Created by: </span>
+            {creator}
+          </p>
+          <p className="boss-check__text boss-check__text_role_secondary">{created}</p>
+        </div>
+      </div>
+      {note !== '-' && (
+        <div className="boss-check__row">
+          <div className="boss-check__cell">
+            <div className="boss-check__box">
+              <p className="boss-check__text boss-check__text_role_note">{note}</p>
+            </div>
+          </div>
+        </div>
+      )}
+      <div className="boss-check__row">
+        <div className="boss-check__cell">
+          <p className="boss-check__text boss-check__text_role_date">
+            <span className="boss-check__text-label">Payslip Date: </span>
+            {payslipDate}
+          </p>
+        </div>
+      </div>
+      {editable &&
+        !isStaffMemberDisabled && (
+          <div className="boss-check__row boss-check__row_role_actions">
+            <button onClick={() => onEdit(owedHour)} className="boss-button boss-button_role_update boss-check__action">
+              Edit
+            </button>
+            <button
+              onClick={() => onDelete(owedHourId)}
+              className="boss-button boss-button_role_cancel boss-check__action"
+            >
+              Delete
+            </button>
+          </div>
+        )}
     </div>
-    { (editable && !isStaffMemberDisabled) && <div className="boss-check__row boss-check__row_role_actions">
-        <button
-          onClick={() => onEdit(owedHour)}
-          className="boss-button boss-button_role_update boss-check__action"
-        >Edit</button>
-        <button
-          onClick={() => onDelete(owedHourId)}
-          className="boss-button boss-button_role_cancel boss-check__action"
-        >Delete</button>
-      </div>
-    }
-
-  </div>
-}
+  );
+};
 
 export default OwedHourMobileItem;
--- app/frontend/apps/staff-member-profile/owed-hours-page/components/owed-hours-table.js
+++ app/frontend/apps/staff-member-profile/owed-hours-page/components/owed-hours-table.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import safeMoment from "~/lib/safe-moment";
+import safeMoment from '~/lib/safe-moment';
 import oFetch from 'o-fetch';
 import { openWarningModal } from '~/components/modals';
 
@@ -16,54 +16,70 @@ function getOwedHourPermissions(owedHour, permissions) {
   return owedHourPermissions;
 }
 
-export const getOwedHourUIData = (owedHourJS) => {
+export const getOwedHourUIData = owedHourJS => {
   let hasDate = oFetch(owedHourJS, 'hasDate');
   const date = safeMoment.uiDateParse(oFetch(owedHourJS, 'date')).format('ddd DD MMM YYYY');
   let times = 'N/A';
   if (hasDate) {
-    const startTime = safeMoment.iso8601Parse(oFetch(owedHourJS, 'times.startsAt')).utcOffset(oFetch(owedHourJS, 'times.startsAt')).format('HH:mm');
-    const endTime = safeMoment.iso8601Parse(oFetch(owedHourJS, 'times.endsAt')).utcOffset(oFetch(owedHourJS, 'times.endsAt')).format('HH:mm');
+    const startTime = safeMoment
+      .iso8601Parse(oFetch(owedHourJS, 'times.startsAt'))
+      .utcOffset(oFetch(owedHourJS, 'times.startsAt'))
+      .format('HH:mm');
+    const endTime = safeMoment
+      .iso8601Parse(oFetch(owedHourJS, 'times.endsAt'))
+      .utcOffset(oFetch(owedHourJS, 'times.endsAt'))
+      .format('HH:mm');
     times = `${startTime} - ${endTime}`;
   }
   const durationHours = oFetch(owedHourJS, 'duration.hours');
   const durationMinutes = oFetch(owedHourJS, 'duration.minutes');
   const note = owedHourJS.note || '-';
   const creator = oFetch(owedHourJS, 'createdBy');
-  const created = safeMoment.iso8601Parse(oFetch(owedHourJS, 'createdAt')).utcOffset(oFetch(owedHourJS, 'createdAt')).format('Do MMMM YYYY - HH:mm');
+  const created = safeMoment
+    .iso8601Parse(oFetch(owedHourJS, 'createdAt'))
+    .utcOffset(oFetch(owedHourJS, 'createdAt'))
+    .format('Do MMMM YYYY - HH:mm');
   const editable = oFetch(owedHourJS, 'editable');
   const payslipDate = oFetch(owedHourJS, 'payslipDate');
 
   return { hasDate, date, times, durationHours, durationMinutes, note, creator, created, editable, payslipDate };
-}
+};
 
-const ActionsCell = ({
-  label,
-  owedHourId,
-  deleteOwedHours,
-  openEditModal,
-  owedHour,
-  editable,
-  isStaffMemberDisabled,
-  owedHourPermissions,
-}) => {
+const ActionsCell = props => {
+  const [
+    label,
+    owedHourId,
+    deleteOwedHours,
+    openEditModal,
+    owedHour,
+    editable,
+    isStaffMemberDisabled,
+    owedHourPermissions,
+  ] = oFetch(
+    props,
+    'label',
+    'owedHourId',
+    'deleteOwedHours',
+    'openEditModal',
+    'owedHour',
+    'editable',
+    'isStaffMemberDisabled',
+    'owedHourPermissions',
+  );
 
-  const onEdit = (owedHour) => {
+  const onEdit = owedHour => {
     openEditModal(owedHour);
-  }
+  };
 
-  const onDelete = (owedHourId) => {
+  const onDelete = owedHourId => {
     openWarningModal({
-      submit: (hideModal) => deleteOwedHours(owedHourId).then(hideModal),
+      submit: hideModal => deleteOwedHours(owedHourId).then(hideModal),
       config: {
         title: 'Delete owed hours',
         text: 'Are You Sure?',
         buttonText: 'Delete',
       },
     });
-    // return confirm('Are you sure ?', {
-    //   title: 'Delete owed hours',
-    //   actionButtonText: 'Delete',
-    // }).then();
   };
 
   const isEditable = oFetch(owedHourPermissions, 'isEditable');
@@ -71,22 +87,33 @@ const ActionsCell = ({
 
   return (
     <div className="boss-table__cell">
-      {(editable && !isStaffMemberDisabled) && <div className="boss-table__info">
-        <p className="boss-table__label">{label}</p>
-        <p className="boss-table__actions">
-          {isEditable && <button
-            onClick={() => (onEdit(owedHour))}
-            className="boss-button boss-button_type_small boss-button_role_update boss-table__action"
-          >Edit</button>}
-          {isDeletable && <button
-            onClick={() => (onDelete(owedHourId))}
-            className="boss-button boss-button_type_small boss-button_role_cancel boss-table__action"
-          >Delete</button>}
-        </p>
-      </div>}
+      {editable &&
+        !isStaffMemberDisabled && (
+          <div className="boss-table__info">
+            <p className="boss-table__label">{label}</p>
+            <p className="boss-table__actions">
+              {isEditable && (
+                <button
+                  onClick={() => onEdit(owedHour)}
+                  className="boss-button boss-button_type_small boss-button_role_update boss-table__action"
+                >
+                  Edit
+                </button>
+              )}
+              {isDeletable && (
+                <button
+                  onClick={() => onDelete(owedHourId)}
+                  className="boss-button boss-button_type_small boss-button_role_cancel boss-table__action"
+                >
+                  Delete
+                </button>
+              )}
+            </p>
+          </div>
+        )}
     </div>
-  )
-}
+  );
+};
 
 const SimpleCell = ({ label, text }) => {
   return (
@@ -96,8 +123,8 @@ const SimpleCell = ({ label, text }) => {
         <p className="boss-table__text">{text}</p>
       </div>
     </div>
-  )
-}
+  );
+};
 
 const CreatedByCell = ({ label, creator, created }) => {
   return (
@@ -105,31 +132,28 @@ const CreatedByCell = ({ label, creator, created }) => {
       <div className="boss-table__info">
         <p className="boss-table__label">{label}</p>
         <p className="boss-table__text">
-          <span className="boss-table__text-line">
-            {creator}
-          </span>
-          <span className="boss-table__text-meta">
-            {created}
-          </span>
+          <span className="boss-table__text-line">{creator}</span>
+          <span className="boss-table__text-meta">{created}</span>
         </p>
       </div>
     </div>
-  )
-}
+  );
+};
 
 const Row = ({ owedHour, deleteOwedHours, openEditModal, isStaffMemberDisabled, owedHourPermissions }) => {
   const owedHourJS = owedHour.toJS();
-  const [
-    date,
-    times,
-    durationHours,
-    durationMinutes,
-    creator,
-    created,
-    note,
-    editable,
-    payslipDate,
-  ] = oFetch(getOwedHourUIData(owedHourJS), "date", "times", "durationHours", "durationMinutes", "creator", "created", "note", "editable", "payslipDate");
+  const [date, times, durationHours, durationMinutes, creator, created, note, editable, payslipDate] = oFetch(
+    getOwedHourUIData(owedHourJS),
+    'date',
+    'times',
+    'durationHours',
+    'durationMinutes',
+    'creator',
+    'created',
+    'note',
+    'editable',
+    'payslipDate',
+  );
 
   return (
     <div className="boss-table__row">
@@ -151,8 +175,8 @@ const Row = ({ owedHour, deleteOwedHours, openEditModal, isStaffMemberDisabled, 
         owedHourPermissions={owedHourPermissions}
       />
     </div>
-  )
-}
+  );
+};
 
 const Header = () => {
   return (
@@ -164,10 +188,10 @@ const Header = () => {
       <div className="boss-table__cell boss-table__cell_role_header">Created By</div>
       <div className="boss-table__cell boss-table__cell_role_header">Note</div>
       <div className="boss-table__cell boss-table__cell_role_header">Payslip Date</div>
-      <div className="boss-table__cell boss-table__cell_role_header"></div>
+      <div className="boss-table__cell boss-table__cell_role_header" />
     </div>
-  )
-}
+  );
+};
 
 const OwedStats = ({ week }) => {
   const startDate = safeMoment.uiDateParse(week.get('startDate')).format('ddd DD MMM YYYY');
@@ -175,56 +199,61 @@ const OwedStats = ({ week }) => {
   return (
     <div>
       <div className="boss-count boss-count_adjust_flow boss-count_type_solid">
-        <p className="boss-count__label boss-count__label_role_date">{startDate} - {endDate}</p>
+        <p className="boss-count__label boss-count__label_role_date">
+          {startDate} - {endDate}
+        </p>
       </div>
       <div className="boss-count boss-count_adjust_flow boss-count_type_solid">
         <p className="boss-count__label boss-count__label_role_time">Total: {week.get('totalHours')}</p>
       </div>
     </div>
-  )
+  );
 };
 
-
-const OwedHoursTable = (props) => {
-  const [
-    owedHours,
-    deleteOwedHours,
-    openEditModal,
-    isStaffMemberDisabled,
-    owedHoursPermissions
-  ] = oFetch(props, 'owedHours', 'deleteOwedHours', 'openEditModal', 'isStaffMemberDisabled', 'owedHoursPermissions');
+const OwedHoursTable = props => {
+  const [owedHours, deleteOwedHours, openEditModal, isStaffMemberDisabled, owedHoursPermissions] = oFetch(
+    props,
+    'owedHours',
+    'deleteOwedHours',
+    'openEditModal',
+    'isStaffMemberDisabled',
+    'owedHoursPermissions',
+  );
 
   const renderRows = (owedHours, deleteOwedHours, openEditModal) => {
     return owedHours.map(owedHour => {
       const owedHourJS = owedHour.toJS();
       const owedHourId = oFetch(owedHourJS, 'id');
-      const owedHourPermissions = getOwedHourPermissions(owedHourJS, owedHoursPermissions)
+      const owedHourPermissions = getOwedHourPermissions(owedHourJS, owedHoursPermissions);
 
-      return <Row
-        key={`${owedHourId}`}
-        owedHour={owedHour}
-        deleteOwedHours={deleteOwedHours}
-        openEditModal={openEditModal}
-        isStaffMemberDisabled={isStaffMemberDisabled}
-        owedHourPermissions={owedHourPermissions}
-      />
+      return (
+        <Row
+          key={`${owedHourId}`}
+          owedHour={owedHour}
+          deleteOwedHours={deleteOwedHours}
+          openEditModal={openEditModal}
+          isStaffMemberDisabled={isStaffMemberDisabled}
+          owedHourPermissions={owedHourPermissions}
+        />
+      );
     });
-  }
+  };
 
   const renderMobileItems = (owedHours, deleteOwedHours, openEditModal) => {
     return owedHours.map(owedHour => {
-      return <OwedHoursMobileItem
-        key={owedHour.get('id')}
-        owedHour={owedHour}
-        isStaffMemberDisabled={isStaffMemberDisabled}
-        deleteOwedHours={deleteOwedHours}
-        openEditModal={openEditModal}
-      />;
-    })
-  }
+      return (
+        <OwedHoursMobileItem
+          key={owedHour.get('id')}
+          owedHour={owedHour}
+          isStaffMemberDisabled={isStaffMemberDisabled}
+          deleteOwedHours={deleteOwedHours}
+          openEditModal={openEditModal}
+        />
+      );
+    });
+  };
 
-
-  const renderOwedhours = (owedHours) => {
+  const renderOwedhours = owedHours => {
     return owedHours.map((owedHour, index) => {
       return (
         <div key={index} className="boss-board__manager-group boss-board__manager-group_context_stack">
@@ -239,22 +268,21 @@ const OwedHoursTable = (props) => {
             {renderMobileItems(owedHour.get('owedHours'), deleteOwedHours, openEditModal)}
           </div>
         </div>
-      )
+      );
     });
-  }
+  };
 
   const hasOwedHours = !!owedHours.size;
 
   return (
     <div>
-      {hasOwedHours
-        ? renderOwedhours(owedHours)
-        : <h1 className="boss-table__cell boss-table__cell_role_header">
-          NO OWED HOURS FOUND
-            </h1>
-      }
+      {hasOwedHours ? (
+        renderOwedhours(owedHours)
+      ) : (
+        <h1 className="boss-table__cell boss-table__cell_role_header">NO OWED HOURS FOUND</h1>
+      )}
     </div>
-  )
-}
+  );
+};
 
 export default OwedHoursTable;
