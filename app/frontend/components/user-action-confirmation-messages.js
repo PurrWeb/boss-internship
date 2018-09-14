a7fceba63a35b54bb8849c202737501a127b03f0
vlad <tgkfdjk@gmail.com>
2018-09-14 14:21:14 +0100

    Change select staff type screen styles

--- app/frontend/components/user-action-confirmation-messages.js
+++ app/frontend/components/user-action-confirmation-messages.js
@@ -8,13 +8,17 @@ class UserActionConfirmationMessages extends React.Component {
             return null;
         }
         var messageElements = messages.map((message, i) => {
-            return <p className="boss-message__text" key={i}>
+            return <p className="boss-message__text boss-message__text_marked" key={i}>
                 {message}
             </p>
         })
-        return <div className="boss-message boss-message_role_ca-note">
-            {messageElements}
-        </div>
+        return (
+            <div className="boss-staff-type-menu__row">
+                <div className="boss-message boss-message_role_ca-note">
+                    {messageElements}
+                </div>
+            </div>
+        )
     }
 }
 
