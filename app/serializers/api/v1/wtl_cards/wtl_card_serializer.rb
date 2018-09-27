0fb87d1df9798265478de8ae4eaa15f3ddeda735
Igor Pugachev <ipugachev84@gmail.com>
2018-09-27 08:21:50 +0100

    Remove history from `WtlCardSerializer`

--- app/serializers/api/v1/wtl_cards/wtl_card_serializer.rb
+++ app/serializers/api/v1/wtl_cards/wtl_card_serializer.rb
@@ -2,14 +2,9 @@ class Api::V1::WtlCards::WtlCardSerializer < ActiveModel::Serializer
   attributes \
     :id,
     :number,
-    :disabled,
-    :history
+    :disabled
 
   def disabled
     object.disabled?
-  end
-
-  def history
-    WtlCardHistoryService.new(wtl_card: object).call
   end
 end
