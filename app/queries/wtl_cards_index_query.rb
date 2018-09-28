12e4d560ff9d1deb8b48ef757c61dcec3e9b7910
Igor Pugachev <ipugachev84@gmail.com>
2018-09-28 16:47:21 +0100

    Fix wtl index queries

--- app/queries/wtl_cards_index_query.rb
+++ app/queries/wtl_cards_index_query.rb
@@ -6,9 +6,13 @@ class WtlCardsIndexQuery
   def all
     wtl_cards = WtlCard.includes([versions: [:item]]).all
     card_number = filter[:card_number]
+    status = filter[:status]
 
     if card_number.present?
-      wtl_cards = wtl_cards.search(number)
+      wtl_cards = wtl_cards.search(card_number)
+    end
+    if status.present? && status == "active"
+      wtl_cards = wtl_cards.enabled
     end
     wtl_cards
   end
--- app/queries/wtl_clients_index_query.rb
+++ app/queries/wtl_clients_index_query.rb
@@ -1,10 +1,12 @@
 class WtlClientsIndexQuery
+  ALLOWED_STATUSES = WtlClient.statuses.keys
+
   def initialize(filter:)
     @filter = filter
   end
 
   def all
-    wtl_clients = WtlClient.includes([:wtl_card, versions: [:item]]).all
+    wtl_clients = WtlClient.includes([:wtl_card]).all
     name = filter[:name]
     email = filter[:email]
     status = filter[:status]
@@ -17,14 +19,20 @@ class WtlClientsIndexQuery
       wtl_clients = wtl_clients.where(email: email)
     end
     if status.present?
-      wtl_clients = wtl_clients.where(status: status)
+      if status == "verified"
+        wtl_clients = wtl_clients.where.not(verified_at: nil)
+      end
+      if status == "pending_verification"
+        wtl_clients = wtl_clients.where(verified_at: nil)
+      end
     end
     if card_number.present?
-      wtl_clients = wtl_clients.where(card_number: card_number)
+      wtl_clients = wtl_clients.joins(:wtl_card).where(wtl_cards: {number: card_number})
     end
     wtl_clients
   end
 
   private
+
   attr_reader :filter
 end
