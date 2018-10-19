e64347f8f138b99f448f404781eacd390bb20aea
Igor Pugachev <ipugachev84@gmail.com>
2018-10-19 16:22:13 +0100

    Change requests count link on SecurityRotaOverview page

--- app/frontend/apps/security-rota-overview/components/security-rota-week-days.js
+++ app/frontend/apps/security-rota-overview/components/security-rota-week-days.js
@@ -24,20 +24,14 @@ class RotaWeekDays extends Component {
   }
 
   renderDays(week) {
-    return (
-      <div className="boss-paginator boss-paginator_size_full">
-        {this.renderTabList(week)}
-      </div>
-    );
+    return <div className="boss-paginator boss-paginator_size_full">{this.renderTabList(week)}</div>;
   }
 
   renderTabList(week) {
     const highlightDate = safeMoment.uiDateParse(this.state.highlightDate);
     return week.map((item, index) => {
       const modifiedItem = safeMoment.uiDateParse(item);
-      const tabClassName = highlightDate.isSame(modifiedItem, 'days')
-        ? 'boss-paginator__action_state_active'
-        : '';
+      const tabClassName = highlightDate.isSame(modifiedItem, 'days') ? 'boss-paginator__action_state_active' : '';
       const formatedDate = highlightDate.isSame(modifiedItem, 'days')
         ? modifiedItem.format('D MMMM')
         : modifiedItem.format('D');
@@ -51,9 +45,12 @@ class RotaWeekDays extends Component {
             {formatedDate}
           </button>
           <div className="boss-paginator__meta">
-            <button className="boss-paginator__meta-link boss-paginator__meta-link_role_button-light">
+            <a
+              href={`/security-shift-requests/${item}`}
+              className="boss-paginator__meta-link boss-paginator__meta-link_role_button-light"
+            >
               Requests: {this.props.securityShiftRequestsCount[item]}
-            </button>
+            </a>
           </div>
         </div>
       );
@@ -71,9 +68,7 @@ class RotaWeekDays extends Component {
     const { date } = this.props;
     const currentWeek = this.generateWeek(date);
 
-    return (
-      <div className="boss-rotas__days-nav">{this.renderDays(currentWeek)}</div>
-    );
+    return <div className="boss-rotas__days-nav">{this.renderDays(currentWeek)}</div>;
   }
 }
 
