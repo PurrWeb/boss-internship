bee06582db67638804ab1871e6f9c3950b8ba78a
William Billington <william.billington@gmail.com>
2018-10-19 13:27:44 +0100

    Revert "Fix hours acceptance periods with no accepted by set"
    This reverts commit 23680b71124da1232b47eb93b49f86eede32c84d.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181017171945) do
+ActiveRecord::Schema.define(version: 20180924181537) do
 
   create_table "accessories", force: :cascade do |t|
     t.integer  "venue_id",         limit: 4
