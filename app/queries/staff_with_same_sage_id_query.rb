1e6d821ef9de15a3c320253ebb88fb35c5af8c08
Igor Pugachev <ipugachev84@gmail.com>
2018-10-22 12:24:58 +0100

    Update StaffWithSameSageIdQuery t oreturn staff_members only

--- app/queries/staff_with_same_sage_id_query.rb
+++ app/queries/staff_with_same_sage_id_query.rb
@@ -1,26 +1,17 @@
 class StaffWithSameSageIdQuery
-  Result = Struct.new(:same_sage_id, :all_staff_members)
-
   def all
     staff_members = StaffMember
       .where.not(sage_id: nil)
       .where.not(master_venue_id: nil)
 
-    same_sage_id = []
-    all_staff_members_ids = []
+    all_staff_members = []
 
-    staff_members.group_by { |s| [s.sage_id] }.each_pair do |sage, staff_members|
-      sage_id = sage
+    staff_members.group_by { |s| [s.master_venue_id, s.sage_id] }.each_pair do |venue_sage_id, staff_members|
       if staff_members.count > 1
-        staff_members_ids = staff_members.map(&:id)
-        same_sage_id << {
-          sageId: sage_id[0],
-          staffMembersIds: staff_members_ids,
-        }
-        all_staff_members_ids.concat(staff_members_ids)
+        all_staff_members.concat(staff_members)
       end
     end
 
-    Result.new(same_sage_id, StaffMember.where(id: all_staff_members_ids))
+    all_staff_members
   end
 end
