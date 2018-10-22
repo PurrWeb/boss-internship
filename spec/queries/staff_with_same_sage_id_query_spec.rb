b7e911009bd1d01d60e9ded74cbe5fcce8352a64
Igor Pugachev <ipugachev84@gmail.com>
2018-10-22 12:28:56 +0100

    Update StaffWithSameSageIdQuery spec

--- spec/queries/staff_with_same_sage_id_query_spec.rb
+++ spec/queries/staff_with_same_sage_id_query_spec.rb
@@ -4,25 +4,19 @@ describe StaffWithSameSageIdQuery do
   let(:query) { StaffWithSameSageIdQuery.new.all }
   let(:sage_id_1) { "SAGE_ONE" }
   let(:sage_id_2) { "SAGE_TWO" }
-
-  let!(:staff_members_with_duplicated_1) { FactoryGirl.create_list(:staff_member, 10, sage_id: sage_id_1) }
-  let!(:staff_members_with_duplicated_2) { FactoryGirl.create_list(:staff_member, 10, sage_id: sage_id_2) }
+  let(:master_venue) { FactoryGirl.create(:venue) }
+  let!(:staff_members_with_duplicated_1) { FactoryGirl.create_list(:staff_member, 10, master_venue: master_venue, sage_id: sage_id_1) }
+  let!(:staff_members_with_duplicated_2) { FactoryGirl.create_list(:staff_member, 10, master_venue: master_venue, sage_id: sage_id_2) }
   let!(:staff_members_without_duplication) { (1..10).map { |number| FactoryGirl.create(:staff_member, sage_id: SecureRandom.hex) } }
 
-  context "Should work" do
-    specify "its should return groupped by duplicated sage id staff members" do
-      result = query
-      expect(result.same_sage_id).to eq(expected_query_result)
+  context "Query" do
+    specify "should return staff members with duplicated sage id" do
+      staff_members = query
+      expect(staff_members).to eq(expected_query_result)
     end
   end
 
   def expected_query_result
-    [{
-      sageId: sage_id_1,
-      staffMembersIds: staff_members_with_duplicated_1.map(&:id),
-    }, {
-      sageId: sage_id_2,
-      staffMembersIds: staff_members_with_duplicated_2.map(&:id),
-    }]
+    staff_members_with_duplicated_1.concat(staff_members_with_duplicated_2)
   end
 end
