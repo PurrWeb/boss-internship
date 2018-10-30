be6142d50386f0313dfd6a07ca97dcf365dd2ddd
Igor Pugachev <ipugachev84@gmail.com>
2018-10-30 09:56:05 +0000

    Run clock_in_rollover_job code in all environments

--- app/jobs/clock_in_rollover_job.rb
+++ app/jobs/clock_in_rollover_job.rb
@@ -1,7 +1,5 @@
 class ClockInRolloverJob < ActiveJob::Base
   def perform(env: Rails.env, service: ReClockInStaffMemberToNextDay)
-    return unless env == "production"
-
     service.new.call
   end
 end
