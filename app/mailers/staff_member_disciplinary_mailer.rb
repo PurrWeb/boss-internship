d0ef7f696e873e28a8e851f14dce458956cde60b
William Billington <william.billington@gmail.com>
2018-10-31 14:54:13 +0000

    Bcc all disciplinary emails to sina

--- app/mailers/staff_member_disciplinary_mailer.rb
+++ app/mailers/staff_member_disciplinary_mailer.rb
@@ -4,7 +4,11 @@ class StaffMemberDisciplinaryMailer < ApplicationMailer
   APPEAL_TO_NAME = "Sina (sina@jsmbars.co.uk)"
 
   def send_disciplinary_email(disciplinary:)
-    mail(to: disciplinary.staff_member.email, subject: "Notice of #{disciplinary.warning_level_text}") do |format|
+    mail(
+      to: disciplinary.staff_member.email,
+      bcc: ["sina@jsmbars.co.uk"],
+      subject: "Notice of #{disciplinary.warning_level_text}",
+    ) do |format|
       format.html do
         render locals: {
           disciplinary: disciplinary,
