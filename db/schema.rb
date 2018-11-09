0a5a68c8d14180e148f263ec820ea95c7520aa0f
William Billington <william.billington@gmail.com>
2018-11-09 13:42:34 +0000

    Mark legacy hap records with seconds for validation exception

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181109132743) do
+ActiveRecord::Schema.define(version: 20181109133907) do
 
   create_table "accessories", force: :cascade do |t|
     t.integer  "venue_id",         limit: 4
