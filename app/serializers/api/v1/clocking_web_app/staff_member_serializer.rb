7a01d8565817e25fc2964c53d7bfd758547a2ea5
Igor Pugachev <ipugachev84@gmail.com>
2018-11-15 14:44:43 +0000

    Add Please Retake Picture wrapper to clocking web app

--- app/serializers/api/v1/clocking_web_app/staff_member_serializer.rb
+++ app/serializers/api/v1/clocking_web_app/staff_member_serializer.rb
@@ -2,7 +2,7 @@ class Api::V1::ClockingWebApp::StaffMemberSerializer < ActiveModel::Serializer
   include Rails.application.routes.url_helpers
 
   attributes :id, :url, :avatar_url, :staff_type, :first_name, :surname, :preferred_hours,
-             :preferred_days, :venues, :isOnHolidays
+             :preferred_days, :venues, :isOnHolidays, :markedRetakeAvatar
 
   def url
     api_v1_staff_member_url(object)
@@ -34,6 +34,10 @@ class Api::V1::ClockingWebApp::StaffMemberSerializer < ActiveModel::Serializer
   def isOnHolidays
     staff_with_holidays_ids = instance_options.fetch(:scope).fetch(:staff_with_holidays_ids)
     staff_with_holidays_ids.include?(object.id)
+  end
+
+  def markedRetakeAvatar
+    object.marked_retake_avatar?
   end
 
   def venues
