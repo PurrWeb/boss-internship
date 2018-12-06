2f905573a2a7f6e71e044cd5a760ae5bfebca806
Igor Pugachev <ipugachev84@gmail.com>
2018-12-06 13:50:21 +0000

    Update RefreshRepeatOffendersData migration

--- db/migrate/20181204142011_refresh_repeat_offenders_data.rb
+++ db/migrate/20181204142011_refresh_repeat_offenders_data.rb
@@ -3,14 +3,16 @@ class RefreshRepeatOffendersData < ActiveRecord::Migration
     ActiveRecord::Base.transaction do
       now = Time.current
       today = RotaShiftDate.to_rota_date(now)
-      tax_year = TaxYear.new(today)
+      monday_tax_year = MondayTaxYear.new(today)
+      start_date = monday_tax_year.start_date
+      end_date = monday_tax_year.end_date
 
       TimeDodgerOffenceService.new(
-        start_date: tax_year.start_date,
-        end_date: tax_year.end_date,
+        start_date: start_date,
+        end_date: end_date,
       ).call
 
-      RefreshTimeDodgerOffenceLevels.new(tax_year: tax_year).call
+      RefreshTimeDodgerOffenceLevels.new(monday_tax_year: monday_tax_year).call
     end
   end
 end
