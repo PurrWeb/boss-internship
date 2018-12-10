0d3f76c4b862818437af508553288fbe8df7cbbb
Igor Pugachev <ipugachev84@gmail.com>
2018-12-10 12:44:17 +0000

    Add filtering to payroll reports page

--- app/frontend/apps/payroll-reports/components/payroll-reports-filter.js
+++ app/frontend/apps/payroll-reports/components/payroll-reports-filter.js
@@ -1,16 +1,48 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { RadioGroup, Radio } from 'react-radio-group';
-import  { PAYROLL_REPORT_SHOW_ALL_FILTER_TYPE, PAYROLL_REPORT_SALARY_ONLY_FILTER_TYPE } from '../constants';
+import queryString from 'query-string';
+
+import { PAYROLL_REPORT_SHOW_ALL_FILTER_TYPE, FILTER_TITLES, FILTER_TABS } from '../constants';
 
 class PayrollReportsFilter extends Component {
-  state = {
-    payRate: PAYROLL_REPORT_SHOW_ALL_FILTER_TYPE,
+  constructor(props) {
+    super(props);
+    this.state = {
+      activeTab: this.getActiveTabFromQueryString(),
+    };
+  }
+
+  getActiveTabFromQueryString = () => {
+    const activeTab = queryString.parse(window.location.search).tab || PAYROLL_REPORT_SHOW_ALL_FILTER_TYPE;
+    this.filterByActiveTab(activeTab);
+    return activeTab;
+  };
+
+  filterByActiveTab = activeTab => {
+    this.props.onFilterChange(activeTab);
   };
 
   handleFilterChange = value => {
-    this.setState({ payRate: value });
-    this.props.onFilterChange(value);
+    const queryStringObject = queryString.parse(window.location.search);
+    window.history.pushState(
+      'state',
+      'title',
+      `${window.location.pathname}?${queryString.stringify({ ...queryStringObject, tab: value })}`,
+    );
+    this.setState({ activeTab: value });
+    this.filterByActiveTab(value);
+  };
+
+  renderFilterTabs = () => {
+    return FILTER_TABS.map((filterTab, index) => {
+      return (
+        <label key={index} className="boss-form__switcher-label">
+          <Radio value={filterTab} className="boss-form__switcher-radio" />
+          <span className="boss-form__switcher-label-text">{FILTER_TITLES[filterTab]}</span>
+        </label>
+      );
+    });
   };
 
   render() {
@@ -19,18 +51,11 @@ class PayrollReportsFilter extends Component {
         <div className="boss-form__field boss-form__field_layout_fluid">
           <RadioGroup
             name="filterType"
-            selectedValue={this.state.payRate}
+            selectedValue={this.state.activeTab}
             onChange={this.handleFilterChange}
             className="boss-form__switcher"
           >
-            <label className="boss-form__switcher-label">
-              <Radio value={ PAYROLL_REPORT_SALARY_ONLY_FILTER_TYPE } className="boss-form__switcher-radio" />
-              <span className="boss-form__switcher-label-text">Salary Only</span>
-            </label>
-            <label className="boss-form__switcher-label">
-              <Radio value={ PAYROLL_REPORT_SHOW_ALL_FILTER_TYPE } className="boss-form__switcher-radio" />
-              <span className="boss-form__switcher-label-text">Show All</span>
-            </label>
+            {this.renderFilterTabs()}
           </RadioGroup>
         </div>
       </div>
