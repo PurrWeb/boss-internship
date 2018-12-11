738b2095fc4f24cf4506ef9686055b60e9580b67
Igor Pugachev <ipugachev84@gmail.com>
2018-12-11 09:22:19 +0000

    Change `RefreshTimeDodgerOffenceLevels` dodges threshold

--- app/services/refresh_time_dodger_offence_levels.rb
+++ app/services/refresh_time_dodger_offence_levels.rb
@@ -1,5 +1,6 @@
 class RefreshTimeDodgerOffenceLevels
-  DODGES_THRESHOLD = 5
+  DODGES_FIRST_LEVEL_THRESHOLD = 5
+  DODGES_OTHER_LEVELS_THRESHOLD = 3
 
   def initialize(monday_tax_year:)
     @monday_tax_year = monday_tax_year
@@ -17,7 +18,10 @@ class RefreshTimeDodgerOffenceLevels
     ActiveRecord::Base.transaction do
       time_dodger_offence.group_by(&:staff_member_id).each_pair do |id, dodges|
         count = dodges.count
-        offence_level = count / DODGES_THRESHOLD
+        offence_level = 0
+        if count >= DODGES_FIRST_LEVEL_THRESHOLD
+          offence_level = 1 + ((count - DODGES_FIRST_LEVEL_THRESHOLD) / DODGES_OTHER_LEVELS_THRESHOLD)
+        end
         time_dodger_offence_level = TimeDodgerOffenceLevel.find_or_initialize_by(
           tax_year_start: monday_tax_year.start_date,
           staff_member_id: id,
