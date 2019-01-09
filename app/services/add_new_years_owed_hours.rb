718180dd9e04c3a976a4559122f6fbb8855c90fd
William Billington <william.billington@gmail.com>
2019-01-09 16:18:02 +0000

    Return owed hours from add_new_years_owed_hours script

--- app/services/add_new_years_owed_hours.rb
+++ app/services/add_new_years_owed_hours.rb
@@ -1,5 +1,11 @@
 # Generates owed hours from output of GetNewYearHours service
 class AddNewYearsOwedHours
+  Result = Struct.new(:success, :created_count, :owed_hours) do
+    def success?
+      success
+    end
+  end
+
   def initialize(new_years_hour_data:)
     @new_years_hour_data = new_years_hour_data
   end
@@ -7,6 +13,10 @@ class AddNewYearsOwedHours
 
   def call
     system_user = User.first
+    result_data = {
+      created_count: 0,
+      owed_hours: []
+    }
 
     ActiveRecord::Base.transaction do
       new_years_hour_data.each do |new_years_hours_datum|
@@ -27,9 +37,16 @@ class AddNewYearsOwedHours
             }
           ).call
 
-          raise ActiveRecord::Rollback unless result
+          raise ActiveRecord::Rollback unless result.success?
+          result_data.fetch(:owed_hours) << result.owed_hour
         end
       end
     end
+
+    Result.new(
+      true,
+      result_data.fetch(:created_count),
+      result_data.fetch(:owed_hours),
+    )
   end
 end
