ce200d656232d5434b11098ee57ab9a6d02b8536
Igor Pugachev <ipugachev84@gmail.com>
2019-01-23 10:52:49 +0000

    Add regular to staff members index

--- app/controllers/api/v1/staff_members_controller.rb
+++ app/controllers/api/v1/staff_members_controller.rb
@@ -6,7 +6,7 @@ module Api
       skip_before_filter :parse_access_tokens, only: [:set_password, :reset_password]
 
       def index
-        staff_members = StaffMember.enabled.limit(20)
+        staff_members = StaffMember.regular.enabled.limit(100)
 
         render json: {
           staffMembers: ActiveModel::Serializer::CollectionSerializer.new(
